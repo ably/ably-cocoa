3dc7a42a471ceafe5f665ec642e151a7a83e2915
Yavor Georgiev <fealebenpae@gmail.com>
2015-08-01 17:23:06 +0300

    Apply some basic improvements
    Apply Xcode 7's recommended settings
    Add an umbrella header and modulemap for Swift compatibility
    Restore ARCHS and FRAMEWORK_SEARCH_PATHS settings to default
    Reduce ably_common to a single copy command

---
+++ ably-ios/ably.h
@@ -0,0 +1,27 @@
+//
+//  ably.h
+//  ably
+//
+//  Created by vic on 19/04/2015.
+//  Copyright (c) 2015 Ably. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ably-ios.
+FOUNDATION_EXPORT double ablyVersionNumber;
+
+//! Project version string for ably-ios.
+FOUNDATION_EXPORT const unsigned char ablyVersionString[];
+
+#import <ably/ARTRest.h>
+#import <ably/ARTRealtime.h>
+#import <ably/ARTTypes.h>
+#import <ably/ARTStatus.h>
+#import <ably/ARTPaginatedResult.h>
+#import <ably/ARTHttp.h>
+#import <ably/ARTMessage.h>
+#import <ably/ARTAuth.h>
+#import <ably/ARTPresenceMessage.h>
+#import <ably/ARTHttpPaginatedResult.h>
+#import <ably/ARTPayload.h>
