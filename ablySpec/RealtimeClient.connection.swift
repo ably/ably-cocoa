bfa37b2dd3f132b25a8ef4e89484df4615eedbca
Ricardo Pereira <m@ricardopereira.eu>
2015-11-04 15:30:56 +0000

    Check state order

--- ablySpec/RealtimeClient.connection.swift
+++ ablySpec/RealtimeClient.connection.swift
@@ -186,6 +186,14 @@ class RealtimeClientConnection: QuickSpec {
                     }
 
                     expect(events).to(haveCount(8), description: "Missing some states")
+                    expect(events[0].rawValue).to(equal(ARTRealtimeConnectionState.Initialized.rawValue), description: "Should be INITIALIZED state")
+                    expect(events[1].rawValue).to(equal(ARTRealtimeConnectionState.Connecting.rawValue), description: "Should be CONNECTING state")
+                    expect(events[2].rawValue).to(equal(ARTRealtimeConnectionState.Connected.rawValue), description: "Should be CONNECTED state")
+                    expect(events[3].rawValue).to(equal(ARTRealtimeConnectionState.Closing.rawValue), description: "Should be CLOSING state")
+                    expect(events[4].rawValue).to(equal(ARTRealtimeConnectionState.Closed.rawValue), description: "Should be CLOSED state")
+                    expect(events[5].rawValue).to(equal(ARTRealtimeConnectionState.Suspended.rawValue), description: "Should be SUSPENDED state")
+                    expect(events[6].rawValue).to(equal(ARTRealtimeConnectionState.Disconnected.rawValue), description: "Should be DISCONNECTED state")
+                    expect(events[7].rawValue).to(equal(ARTRealtimeConnectionState.Failed.rawValue), description: "Should be FAILED state")
                 }
 
                 // RTN4b
