f93115cc177d18cc25b6c76656bbc4cbb78e793b
Ricardo Pereira <m@ricardopereira.eu>
2015-12-04 10:39:01 +0000

    Trying to solve `Invalid connection: com.apple.coresymbolicationd`. Scripts: run-tests.sh and setup.sh  - Created a script `run-tests` to be sure we run the same commands on ci server and local machine  - It is possible to choose which build tool to perform the tests: env BUILDTOOL      Options: xctool (default), xcodebuild-pretty, xcodebuild-travis, xcodebuild      Example: BUILDTOOL=xcodebuild ./Scripts/run-tests.sh  - It is possible to run only one test class like: `CLASS=ablyTests:ARTRealtimePresenceHistoryTest ./Scripts/run-tests.sh`  - Changed main scheme to run tests on the same build (the old way was runnning Swift tests and then ObjC tests. Now it runs both on the same execution)  - Before testing, does a `clean` command  - Run setup.sh to install tools dependencies

---
+++ Scripts/run-tests.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -o pipefail
+
+: ${BUILDTOOL:=xctool} #Default
+: ${CLASS:=""} #Default: test all classes (only works on xctool)
+
+# Xcode Build Command Line
+# https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man1/xcodebuild.1.html
+: ${WORKSPACE:="ably.xcworkspace"}
+: ${SCHEME:="ably"}
+: ${CONFIGURATION:="Release"}
+: ${SDK:="iphonesimulator9.1"}
+: ${DESTINATION:="platform=iOS Simulator,OS=9.1,name=iPhone 4s"}
+
+init() {
+  # Launch the simulator before running the tests
+  # Avoid "iPhoneSimulator: Timed out waiting"
+  open -b com.apple.iphonesimulator
+}
+
+COMMAND="-workspace \"${WORKSPACE}\" -scheme \"${SCHEME}\" -configuration \"${CONFIGURATION}\" -sdk \"${SDK}\" -destination \"${DESTINATION}\" ONLY_ACTIVE_ARCH=NO"
+
+case "${BUILDTOOL}" in
+  xctool) echo "Selected build tool: xctool"
+  init
+    # Tests (Swift & Objective-C)
+  	case "${CLASS}" in
+  	  "") echo "Testing all classes"
+      COMMAND="xctool clean test "${COMMAND}
+      ;;
+      *) echo "Testing ${CLASS}"
+      COMMAND="xctool clean test -only ${CLASS} "${COMMAND}
+      ;;
+  	esac
+  ;;
+  xcodebuild-travis) echo "Selected tool: xcodebuild + xcpretty (format: travisci)"
+  init
+    # Use xcpretty together with tee to store the raw log in a file, and get the pretty output in the terminal
+    xcodebuild clean test -workspace "${WORKSPACE}" -scheme "${SCHEME}" -configuration "${CONFIGURATION}" -sdk "${SDK}" -destination "${DESTINATION}" ONLY_ACTIVE_ARCH=NO | tee xcodebuild.log | xcpretty -f `xcpretty-travis-formatter`
+  ;;
+  xcodebuild-pretty) echo "Selected tool: xcodebuild + xcpretty"
+  init
+    xcodebuild clean test -workspace "${WORKSPACE}" -scheme "${SCHEME}" -configuration "${CONFIGURATION}" -sdk "${SDK}" -destination "${DESTINATION}" ONLY_ACTIVE_ARCH=NO | tee xcodebuild.log | xcpretty --test
+  ;;
+  xcodebuild) echo "Selected tool: xcodebuild"
+  init
+    xcodebuild clean test -workspace "${WORKSPACE}" -scheme "${SCHEME}" -configuration "${CONFIGURATION}" -sdk "${SDK}" -destination "${DESTINATION}" ONLY_ACTIVE_ARCH=NO
+  ;;
+  *) echo "No build tool especified" && exit 2
+esac
+
+set -x
+eval "${COMMAND}"
