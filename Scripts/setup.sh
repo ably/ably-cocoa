e66c5e9188a23b642fc356b7dafa59bb8bb5f09c
Ricardo Pereira <m@ricardopereira.eu>
2016-01-18 19:46:42 +0000

    Scan: test suite runner with Travis formatter

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -6,12 +6,8 @@ set -e
 # Facebook Replacement for Apple's xcodebuild
 brew update && brew install xctool
 
-# Xcode Build formatter
-gem install xcpretty
-
-# Custom formatter for xcpretty with some syntactic sugar for presentation on TravisCI
-# https://github.com/kattrali/xcpretty-travis-formatter
-gem install xcpretty-travis-formatter --no-rdoc --no-ri --no-document --quiet
+# Install Scan
+gem install scan
 
 # Install CocoaPods
 gem install cocoapods -v '0.39.0'
