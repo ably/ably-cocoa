8fa33f4a66fd0661e7030b0668a50da9bf63ee24
Ricardo Pereira <m@ricardopereira.eu>
2016-01-19 16:07:34 +0000

    RealtimeClientChannel test

---
+++ ablySpec/RealtimeClient.channel.swift
@@ -0,0 +1,23 @@
+//
+//  RealtimeClient.channel.swift
+//  ably
+//
+//  Created by Ricardo Pereira on 18/01/16.
+//  Copyright © 2016 Ably. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import ably
+@testable import ably.Private
+
+class RealtimeClientChannel: QuickSpec {
+    override func spec() {
+        describe("Channel") {
+
+
+
+        }
+    }
+}
