297b4d590cc1b47c7253ee5172ce779b1ec6cfd6
Ricardo Pereira <m@ricardopereira.eu>
2016-02-05 11:33:19 +0000

    RTL6e: pending

--- ablySpec/RealtimeClientChannel.swift
+++ ablySpec/RealtimeClientChannel.swift
@@ -294,7 +294,7 @@ class RealtimeClientChannel: QuickSpec {
                 context("Unidentified clients using Basic Auth") {
 
                     // RTL6e1
-                    it("should have the provided clientId on received message when it was published with clientId") {
+                    pending("should have the provided clientId on received message when it was published with clientId") {
                         let client = ARTRealtime(options: AblyTests.commonAppSetup())
                         defer { client.close() }
 
