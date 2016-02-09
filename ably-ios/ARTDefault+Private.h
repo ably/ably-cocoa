c7bb5a9650a608d9b3d424c310568e2be68f06b1
Ricardo Pereira <m@ricardopereira.eu>
2016-02-09 22:26:03 +0000

    Defaults: setRealtimeRequestTimeout

---
+++ ably-ios/ARTDefault+Private.h
@@ -0,0 +1,15 @@
+//
+//  ARTDefault+Private.h
+//  ably
+//
+//  Created by Ricardo Pereira on 09/02/16.
+//  Copyright © 2016 Ably. All rights reserved.
+//
+
+#import "ARTDefault.h"
+
+@interface ARTDefault (Private)
+
++ (void)setRealtimeRequestTimeout:(NSTimeInterval)value;
+
+@end
