03e5df19a8f6dbf8b32a020eada8d96e699b7241
Toni Cárdenas <toni@tcardenas.me>
2016-02-09 05:06:30 +0100

    Update README, add examples to tests.

---
+++ ably-iosTests/ARTReadmeExamples.m
@@ -0,0 +1,216 @@
+//
+//  ARTReadmeExamples.m
+//  ably
+//
+//  Created by Toni Cárdenas on 9/2/16.
+//  Copyright © 2016 Ably. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <XCTest/XCTest.h>
+#import "ARTTestUtil.h"
+#import "ably.h"
+
+@interface ARTReadmeExamples : XCTestCase
+@end
+
+@implementation ARTReadmeExamples
+
+- (void)testMakeKeyInstance {
+    ARTRealtime* client = [[ARTRealtime alloc] initWithKey:@"xxxx:xxxx"];
+    [client.connection close];
+}
+
+- (void)testMakeTokenInstance {
+    ARTRealtime* client = [[ARTRealtime alloc] initWithToken:@"xxxx"];
+    [client.connection close];
+}
+
+- (void)testListenToConnectionStateChanges {
+    ARTClientOptions *options = [ARTTestUtil clientOptions];
+    [ARTTestUtil testRealtime:options callback:^(ARTRealtime *client) {
+        [client.connection on:^(ARTConnectionStateChange *stateChange) {
+            switch (stateChange.current) {
+                case ARTRealtimeConnected:
+                    NSLog(@"connected!");
+                    break;
+                case ARTRealtimeFailed:
+                    NSLog(@"failed! %@", stateChange.reason);
+                    break;
+                default:
+                    break;
+            }
+        }];
+    }];
+}
+
+- (void)testNoAutoConnect {
+    ARTClientOptions *options = [[ARTClientOptions alloc] initWithKey:@"xxxx:xxxx"];
+    options.autoConnect = false;
+    ARTRealtime *client = [[ARTRealtime alloc] initWithOptions:options];
+    [client.connection connect];
+    [client.connection close];
+}
+
+- (void)testSubscribeAndPublishingToChannel {
+    ARTClientOptions *options = [ARTTestUtil clientOptions];
+    [ARTTestUtil testRealtime:options callback:^(ARTRealtime *client) {
+        ARTRealtimeChannel *channel = [client.channels get:@"test"];
+        
+        [channel subscribe:^(ARTMessage *message) {
+            NSLog(@"%@", message.name);
+            NSLog(@"%@", message.data);
+        }];
+        
+        [channel subscribe:@"myEvent" cb:^(ARTMessage *message) {
+            NSLog(@"%@", message.name);
+            NSLog(@"%@", message.data);
+        }];
+        
+        [channel publish:@"greeting" data:@"Hello World!"];
+
+        [channel history:^(ARTPaginatedResult<ARTMessage *> *messagesPage, NSError *error) {
+            NSLog(@"%@", messagesPage.items);
+        }];
+    }];
+}
+
+- (void) testQueryingTheHistory {
+    ARTClientOptions *options = [ARTTestUtil clientOptions];
+    [ARTTestUtil testRealtime:options callback:^(ARTRealtime *client) {
+        ARTRealtimeChannel *channel = [client.channels get:@"test"];
+
+        [channel history:^(ARTPaginatedResult<ARTMessage *> *messagesPage, NSError *error) {
+            NSLog(@"%@", messagesPage.items);
+            NSLog(@"%@", messagesPage.items.firstObject);
+            NSLog(@"%@", messagesPage.items.firstObject.data); // payload for the message
+            NSLog(@"%lu", (unsigned long)[messagesPage.items count]); // number of messages in the current page of history
+            [messagesPage next:^(ARTPaginatedResult<ARTMessage *> *nextPage, NSError *error) {
+                // retrieved the next page in nextPage
+            }];
+            NSLog(@"%d", messagesPage.hasNext); // true, there are more pages
+        }];
+    }];
+}
+
+- (void)testPresenceOnAChannel {
+    ARTClientOptions *options = [ARTTestUtil clientOptions];
+    options.clientId = @"foo";
+    [ARTTestUtil testRealtime:options callback:^(ARTRealtime *client) {
+        ARTRealtimeChannel *channel = [client.channels get:@"test"];
+        
+        [channel.presence enter:@"john.doe" cb:^(ARTErrorInfo *errorInfo) {
+            [channel.presence get:^(ARTPaginatedResult<ARTPresenceMessage *> *result, NSError *error) {
+                // members is the array of members present
+            }];
+        }];
+    }];
+}
+
+- (void)testQueryingThePresenceHistory {
+    ARTClientOptions *options = [ARTTestUtil clientOptions];
+    [ARTTestUtil testRealtime:options callback:^(ARTRealtime *client) {
+        ARTRealtimeChannel *channel = [client.channels get:@"test"];
+        
+        [channel.presence history:^(ARTPaginatedResult<ARTPresenceMessage *> *presencePage, NSError *error) {
+            ARTPresenceMessage *first = (ARTPresenceMessage *)presencePage.items.firstObject;
+            NSLog(@"%lu", (unsigned long)first.action); // Any of ARTPresenceEnter, ARTPresenceUpdate or ARTPresenceLeave
+            NSLog(@"%@", first.clientId); // client ID of member
+            NSLog(@"%@", first.data); // optional data payload of member
+            [presencePage next:^(ARTPaginatedResult<ARTPresenceMessage *> *nextPage, NSError *error) {
+                // retrieved the next page in nextPage
+            }];
+        }];
+    }];
+}
+
+- (void)testMakeRestClientAndChannel {
+    ARTRest *client = [[ARTRest alloc] initWithKey:@"xxxx:xxxx"];
+    ARTRestChannel *channel = [client.channels get:@"test"];
+    channel = channel;
+}
+
+- (void)testRestPublishMessage {
+    [ARTTestUtil testRest:^(ARTRest *client) {
+        ARTRestChannel *channel = [client.channels get:@"test"];
+        
+        [channel publish:@"myEvent" data:@"Hello!"];
+    }];
+}
+
+- (void)testRestQueryingTheHistory {
+    [ARTTestUtil testRest:^(ARTRest *client) {
+        ARTRestChannel *channel = [client.channels get:@"test"];
+        
+        [channel history:^(ARTPaginatedResult<ARTMessage *> *messagesPage, NSError *error) {
+            NSLog(@"%@", messagesPage.items.firstObject);
+            NSLog(@"%@", messagesPage.items.firstObject.data); // payload for the message
+            NSLog(@"%lu", (unsigned long)[messagesPage.items count]); // number of messages in the current page of history
+            [messagesPage next:^(ARTPaginatedResult<ARTMessage *> *nextPage, NSError *error) {
+                // retrieved the next page in nextPage
+            }];
+            NSLog(@"%d", messagesPage.hasNext); // true, there are more pages
+        }];
+    }];
+}
+
+- (void)testRestPresenceOnAChannel {
+    [ARTTestUtil testRest:^(ARTRest *client) {
+        ARTRestChannel *channel = [client.channels get:@"test"];
+        
+        [channel.presence get:^(ARTPaginatedResult<ARTPresenceMessage *> *membersPage, NSError *error) {
+            NSLog(@"%@", membersPage.items.firstObject);
+            NSLog(@"%@", membersPage.items.firstObject.data); // payload for the message
+            [membersPage next:^(ARTPaginatedResult<ARTMessage *> *nextPage, NSError *error) {
+                // retrieved the next page in nextPage
+            }];
+            NSLog(@"%d", membersPage.hasNext); // true, there are more pages
+        }];
+    }];
+}
+
+- (void)testRestQueryingThePresenceHistory {
+    [ARTTestUtil testRest:^(ARTRest *client) {
+        ARTRestChannel *channel = [client.channels get:@"test"];
+        
+        [channel.presence history:^(ARTPaginatedResult<ARTPresenceMessage *> *presencePage, NSError *error) {
+            ARTPresenceMessage *first = (ARTPresenceMessage *)presencePage.items.firstObject;
+            NSLog(@"%@", first.clientId); // client ID of member
+            NSLog(@"%@", first.data); // optional data payload of member
+            [presencePage next:^(ARTPaginatedResult<ARTPresenceMessage *> *nextPage, NSError *error) {
+                // retrieved the next page in nextPage
+            }];
+        }];
+    }];
+}
+
+- (void)testGenerateToken {
+    [ARTTestUtil testRest:^(ARTRest *client) {
+        [client.auth requestToken:nil withOptions:nil callback:^(ARTTokenDetails *tokenDetails, NSError *error) {
+            NSLog(@"%@", tokenDetails.token); // "xVLyHw.CLchevH3hF....MDh9ZC_Q"
+            ARTRest *client = [[ARTRest alloc] initWithToken:tokenDetails.token];
+            client = client;
+        }];
+    }];
+}
+
+- (void)testFetchingStats {
+    [ARTTestUtil testRest:^(ARTRest *client) {
+        [client stats:^(ARTPaginatedResult<ARTStats *> *statsPage, NSError *error) {
+            NSLog(@"%@", statsPage.items.firstObject);
+            [statsPage next:^(ARTPaginatedResult<ARTStats *> *nextPage, NSError *error) {
+                // retrieved the next page in nextPage
+            }];
+        }];
+    }];
+}
+
+- (void)testFetchingTime {
+    [ARTTestUtil testRest:^(ARTRest *client) {
+        [client time:^(NSDate *time, NSError *error) {
+            NSLog(@"%@", time); // 2016-02-09 03:59:24 +0000
+        }];
+    }];
+}
+
+@end
---
+++ ablySpec/ReadmeExamples.swift
@@ -0,0 +1,224 @@
+//
+//  ReadmeExamples.swift
+//
+//
+//  Created by Toni Cárdenas on 9/2/16.
+//
+//
+
+import Foundation
+import XCTest
+import Nimble
+
+// This file is to be kept in sync with the examples in README.md, to make sure they are kept valid.
+
+class ReadmeExamples : XCTestCase {
+    func testMakeKeyInstance() {
+        let client = ARTRealtime(key: "xxxx:xxxx")
+        client.connection.close()
+    }
+    
+    func testMakeTokenInstance() {
+        let client = ARTRealtime(token: "xxxx")
+        client.connection.close()
+    }
+    
+    func testListenToConnectionStateChanges() {
+        let options = AblyTests.clientOptions(requestToken: true)
+        let client = ARTRealtime(options: options)
+        defer { client.close() }
+        
+        client.connection.on { stateChange in
+            let stateChange = stateChange!
+            switch stateChange.current {
+            case .Connected:
+                print("connected!")
+            case .Failed:
+                print("failed! \(stateChange.reason)")
+            default:
+                break
+            }
+        }
+    }
+    
+    func testNoAutoConnect() {
+        let options = ARTClientOptions(key: "xxxx:xxxx")
+        options.autoConnect = false
+        let client = ARTRealtime(options: options)
+        client.connection.connect()
+        client.connection.close()
+    }
+    
+    func testSubscribeAndPublishingToChannel() {
+        let options = AblyTests.clientOptions(requestToken: true)
+        let client = ARTRealtime(options: options)
+        defer { client.close() }
+        
+        let channel = client.channels.get("test")
+        
+        channel.subscribe { message in
+            print(message.name)
+            print(message.data)
+        }
+        
+        channel.subscribe("myEvent") { message in
+            print(message.name)
+            print(message.data)
+        }
+        
+        channel.publish("greeting", data: "Hello World!")
+    }
+    
+    func testQueryingTheHistory() {
+        let options = AblyTests.clientOptions(requestToken: true)
+        let client = ARTRealtime(options: options)
+        defer { client.close() }
+        
+        let channel = client.channels.get("test")
+        
+        channel.history { messagesPage, error in
+            let messagesPage = messagesPage!
+            print(messagesPage.items)
+            print(messagesPage.items.first)
+            print((messagesPage.items.first as? ARTMessage)?.data) // payload for the message
+            print(messagesPage.items.count) // number of messages in the current page of history
+            messagesPage.next { nextPage, error in
+                // retrieved the next page in nextPage
+            }
+            print(messagesPage.hasNext) // true, there are more pages
+        }
+    }
+    
+    func testPresenceOnAChannel() {
+        let options = AblyTests.clientOptions(requestToken: true)
+        options.clientId = "foo"
+        let client = ARTRealtime(options: options)
+        defer { client.close() }
+
+        client.connection.on { stateChange in
+            if stateChange!.current == .Connected {
+                let channel = client.channels.get("test")
+                
+                channel.presence.enter("john.doe") { errorInfo in
+                    channel.presence.get { members, error in
+                        // members is the array of members present
+                    }
+                }
+            }
+        }
+    }
+    
+    func testQueryingThePresenceHistory() {
+        let options = AblyTests.clientOptions(requestToken: true)
+        let client = ARTRealtime(options: options)
+        defer { client.close() }
+        
+        let channel = client.channels.get("test")
+        
+        channel.presence.history { presencePage, error in
+            let presencePage = presencePage!
+            if let first = presencePage.items.first as? ARTPresenceMessage {
+                print(first.action) // Any of .Enter, .Update or .Leave
+                print(first.clientId) // client ID of member
+                print(first.data) // optional data payload of member
+                presencePage.next { nextPage, error in
+                    // retrieved the next page in nextPage
+                }
+            }
+        }
+    }
+    
+    func testMakeRestClientAndChannel() {
+        let client = ARTRest(key: "xxxx:xxxx")
+        let channel = client.channels.get("test")
+        _ = channel
+    }
+    
+    func testRestPublishMessage() {
+        let options = AblyTests.clientOptions(requestToken: true)
+        let client = ARTRest(options: options)
+        let channel = client.channels.get("test")
+        
+        channel.publish("myEvent", data: "Hello!")
+    }
+    
+    func testRestQueryingTheHistory() {
+        let options = AblyTests.clientOptions(requestToken: true)
+        let client = ARTRest(options: options)
+        let channel = client.channels.get("test")
+        
+        channel.history { messagesPage, error in
+            let messagesPage = messagesPage!
+            print(messagesPage.items.first)
+            print((messagesPage.items.first as? ARTMessage)?.data) // payload for the message
+            messagesPage.next { nextPage, error in
+                // retrieved the next page in nextPage
+            }
+            print(messagesPage.hasNext) // true, there are more pages
+        }
+    }
+    
+    func testRestPresenceOnAChannel() {
+        let options = AblyTests.clientOptions(requestToken: true)
+        let client = ARTRest(options: options)
+        let channel = client.channels.get("test")
+        
+        channel.presence.get { membersPage, error in
+            let membersPage = membersPage!
+            print(membersPage.items.first)
+            print((membersPage.items.first as? ARTPresenceMessage)?.data) // payload for the message
+            membersPage.next { nextPage, error in
+                // retrieved the next page in nextPage
+            }
+            print(membersPage.hasNext) // true, there are more pages
+        }
+    }
+    
+    func testRestQueryingThePresenceHistory() {
+        let options = AblyTests.clientOptions(requestToken: true)
+        let client = ARTRest(options: options)
+        let channel = client.channels.get("test")
+        
+        channel.presence.history { presencePage, error in
+            let presencePage = presencePage!
+            if let first = presencePage.items.first as? ARTPresenceMessage {
+                print(first.clientId) // client ID of member
+                presencePage.next { nextPage, error in
+                    // retrieved the next page in nextPage
+                }
+            }
+        }
+    }
+    
+    func testGenerateToken() {
+        let client = ARTRest(options: AblyTests.commonAppSetup())
+        
+        client.auth.requestToken(nil, withOptions: nil) { tokenDetails, error in
+            let tokenDetails = tokenDetails!
+            print(tokenDetails.token) // "xVLyHw.CLchevH3hF....MDh9ZC_Q"
+            let client = ARTRest(token: tokenDetails.token)
+            _ = client
+        }
+    }
+    
+    func testFetchingStats() {
+        let client = ARTRest(options: AblyTests.commonAppSetup())
+        client.channels.get("test").publish("foo", data: "bar") { _ in
+            client.stats { statsPage, error in
+                let statsPage = statsPage!
+                print(statsPage.items.first)
+                statsPage.next { nextPage, error in
+                    // retrieved the next page in nextPage
+                }
+            }
+        }
+    }
+    
+    func testFetchingTime() {
+        let client = ARTRest(options: AblyTests.commonAppSetup())
+        
+        client.time { time, error in
+            print(time) // 2016-02-09 03:59:24 +0000
+        }
+    }
+}
