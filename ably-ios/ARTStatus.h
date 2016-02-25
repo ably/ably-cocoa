287d93fc93bfd1bfa0e8bf9cdee4f3af497d01f1
Toni Cárdenas <toni@tcardenas.me>
2016-02-25 23:49:00 +0100

    Test RTL5*.
    Just adapting `attach` tests.

--- ably-ios/ARTStatus.h
+++ ably-ios/ARTStatus.h
@@ -19,6 +19,7 @@ typedef NS_ENUM(NSUInteger, ARTState) {
     ARTStateNeverConnected,
     ARTStateConnectionTimedOut,
     ARTStateAttachTimedOut,
+    ARTStateDetachTimedOut,
     ARTStateNotAttached,
     ARTStateInvalidArgs,
     ARTStateCryptoBadPadding,
