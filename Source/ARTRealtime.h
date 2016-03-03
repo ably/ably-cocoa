f4df20a795460dea408a46693d1c8b1849f08334
Toni Cárdenas <toni@tcardenas.me>
2016-03-03 18:18:18 +0100

    Remove ARTTimeCallback.

--- Source/ARTRealtime.h
+++ Source/ARTRealtime.h
@@ -58,7 +58,7 @@ Instance the Ably library with the given options.
 */
 - (instancetype)initWithOptions:(ARTClientOptions *)options;
 
-- (void)time:(ARTTimeCallback)cb;
+- (void)time:(void (^)(NSDate *__art_nullable, NSError *__art_nullable))cb;
 
 - (BOOL)stats:(void (^)(__GENERIC(ARTPaginatedResult, ARTStats *) *__art_nullable, NSError *__art_nullable))callback;
 - (BOOL)stats:(art_nullable ARTStatsQuery *)query callback:(void (^)(__GENERIC(ARTPaginatedResult, ARTStats *) *__art_nullable, NSError *__art_nullable))callback error:(NSError *__art_nullable *__art_nullable)errorPtr;
