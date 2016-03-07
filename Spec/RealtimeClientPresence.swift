e8589f40a0e128dbd9604a5034ffeb6d939f407c
Ricardo Pereira <m@ricardopereira.eu>
2016-03-07 15:22:32 +0000

    Add Realtime Presence file to Spec

---
+++ Spec/RealtimeClientPresence.swift
@@ -0,0 +1,20 @@
+//
+//  RealtimeClientPresence.swift
+//  Ably
+//
+//  Created by Ricardo Pereira on 07/03/16.
+//  Copyright © 2016 Ably. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class RealtimeClientPresence: QuickSpec {
+    override func spec() {
+        describe("Presence") {
+
+
+
+        }
+    }
+}
