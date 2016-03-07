e8508f5b05d352838d5243a5b42d7b77fda32b2c
Ricardo Pereira <m@ricardopereira.eu>
2016-03-07 21:21:33 +0000

    Test suite: addRandomMemberToChannel

--- Spec/RealtimeClientPresence.swift
+++ Spec/RealtimeClientPresence.swift
@@ -9,6 +9,16 @@
 import Quick
 import Nimble
 
+func addRandomMemberToChannel(channelName: String, options: ARTClientOptions, done: ()->()) -> [ARTRealtime] {
+    let client = ARTRealtime(options: options)
+    let channel = client.channels.get(channelName)
+    channel.attach() { _ in
+        channel.presence.enterClient(AblyTests.newRandomString(), data: nil)
+        done()
+    }
+    return [client]
+}
+
 class RealtimeClientPresence: QuickSpec {
     override func spec() {
         describe("Presence") {
