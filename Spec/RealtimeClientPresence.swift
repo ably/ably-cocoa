6279526884b224b201d4c69cd3c63f058e03c2b6
Ricardo Pereira <m@ricardopereira.eu>
2016-03-07 21:21:48 +0000

    RTP1

--- Spec/RealtimeClientPresence.swift
+++ Spec/RealtimeClientPresence.swift
@@ -9,12 +9,23 @@
 import Quick
 import Nimble
 
-func addRandomMemberToChannel(channelName: String, options: ARTClientOptions, done: ()->()) -> [ARTRealtime] {
+func addRandomMembersToChannel(channelName: String, members: Int = 1, options: ARTClientOptions, done: ()->()) -> [ARTRealtime] {
     let client = ARTRealtime(options: options)
     let channel = client.channels.get(channelName)
+
+    class Total {
+        static var count: Int = 0
+    }
+
     channel.attach() { _ in
-        channel.presence.enterClient(AblyTests.newRandomString(), data: nil)
-        done()
+        for _ in 1...members {
+            channel.presence.enterClient(AblyTests.newRandomString(), data: nil) { _ in
+                Total.count += 1
+                if Total.count == members {
+                    done()
+                }
+            }
+        }
     }
     return [client]
 }
@@ -23,7 +34,69 @@ class RealtimeClientPresence: QuickSpec {
     override func spec() {
         describe("Presence") {
 
+            // RTP1
+            context("ProtocolMessage bit flag") {
 
+                it("when no members are present") {
+                    let options = AblyTests.commonAppSetup()
+                    options.autoConnect = false
+                    let client = ARTRealtime(options: options)
+                    client.setTransportClass(TestProxyTransport)
+                    client.connect()
+                    defer { client.close() }
+                    let channel = client.channels.get("test")
+                    channel.attach()
+
+                    expect(channel.state).toEventually(equal(ARTRealtimeChannelState.Attached), timeout: testTimeout)
+
+                    let transport = client.transport as! TestProxyTransport
+                    let attached = transport.protocolMessagesReceived.filter({ $0.action == .Attached })[0]
+
+                    expect(attached.flags & 0x1).to(equal(0))
+                    expect(attached.isSyncEnabled()).to(beFalse())
+                    expect(channel.presence.syncComplete).to(beFalse())
+                    expect(channel.presenceMap.isSyncComplete()).to(beFalse())
+                }
+
+                it("when members are present") {
+                    let options = AblyTests.commonAppSetup()
+
+                    var disposable = [ARTRealtime]()
+                    defer {
+                        for clientItem in disposable {
+                            clientItem.close()
+                        }
+                    }
+
+                    waitUntil(timeout: testTimeout) { done in
+                        disposable += addRandomMembersToChannel("test", members: 250, options: options) {
+                            done()
+                        }
+                    }
+
+                    options.autoConnect = false
+                    let client = ARTRealtime(options: options)
+                    client.setTransportClass(TestProxyTransport)
+                    client.connect()
+                    defer { client.close() }
+                    let channel = client.channels.get("test")
+                    channel.attach()
+
+                    expect(channel.presence.syncComplete).to(beFalse())
+                    expect(channel.state).toEventually(equal(ARTRealtimeChannelState.Attached), timeout: testTimeout)
+
+                    let transport = client.transport as! TestProxyTransport
+                    let attached = transport.protocolMessagesReceived.filter({ $0.action == .Attached })[0]
+
+                    // There are members present on the channel
+                    expect(attached.flags & 0x1).to(equal(1))
+                    expect(attached.isSyncEnabled()).to(beTrue())
+                    
+                    expect(channel.presence.syncComplete).toEventually(beTrue(), timeout: testTimeout)
+
+                    expect(transport.protocolMessagesReceived.filter({ $0.action == .Sync })).to(haveCount(3))
+                }
+            }
 
         }
     }
