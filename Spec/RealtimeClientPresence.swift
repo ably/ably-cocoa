283241ddea05c7b15f38e97a0d5884476c66e112
Ricardo Pereira <m@ricardopereira.eu>
2016-03-21 16:04:27 +0000

    RTP10e: pending

--- Spec/RealtimeClientPresence.swift
+++ Spec/RealtimeClientPresence.swift
@@ -440,7 +440,7 @@ class RealtimeClientPresence: QuickSpec {
                 }
 
                 // RTP10e
-                it("should result in an error if the client does not have required presence permission") {
+                pending("should result in an error if the client does not have required presence permission") {
                     let options = AblyTests.clientOptions()
                     options.token = getTestToken(capability: "{ \"cannotpresence:john\":[\"publish\"] }")
                     options.clientId = "john"
