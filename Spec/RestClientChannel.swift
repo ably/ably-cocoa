1e042e9a323c600868ffc0a93f8dd88945fc8dab
Ricardo Pereira <m@ricardopereira.eu>
2016-04-05 22:55:43 +0100

    Fix RSL1g4: should reject the clientId before the message is sent to the server

--- Spec/RestClientChannel.swift
+++ Spec/RestClientChannel.swift
@@ -234,13 +234,21 @@ class RestClientChannel: QuickSpec {
                     let options = AblyTests.commonAppSetup()
                     options.clientId = "john"
                     let client = ARTRest(options: options)
+                    client.httpExecutor = mockExecutor
                     let channel = client.channels.get("test")
+
+                    // Reject before the message is sent to the server
+                    channel.testSuite_injectIntoMethodBefore("publish:callback:") {
+                        mockExecutor.http = nil
+                    }
 
                     waitUntil(timeout: testTimeout) { done in
                         let message = ARTMessage(name: nil, data: "message", clientId: "tester")
                         channel.publish([message]) { error in
                             expect(error!.code).to(equal(40012))
                             expect(error!.message).to(contain("mismatched clientId"))
+
+                            mockExecutor.http = ARTHttp()
                             channel.history { page, error in
                                 expect(error).to(beNil())
                                 expect(page!.items).to(haveCount(0))
