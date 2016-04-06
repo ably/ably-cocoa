639b2f41c3aeea416076ee620b892ccce791d318
Toni Cárdenas <toni@tcardenas.me>
2016-04-06 18:52:07 +0200

    Presence: wait on all the callbacks to avoid "connection broken" errors.

--- Spec/TestUtilities.swift
+++ Spec/TestUtilities.swift
@@ -183,6 +183,18 @@ class AblyTests {
         return [client]
     }
 
+    class func splitDone(howMany: Int, done: () -> ()) -> (() -> ()) {
+        var left = howMany
+        return {
+            left -= 1
+            if left == 0 {
+                done()
+            } else if left < 0 {
+                fail("splitDone called more than the expected \(howMany) times")
+            }
+        }
+    }
+
     // MARK: Crypto
 
     struct CryptoTestItem {
