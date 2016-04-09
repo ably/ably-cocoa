eeb90e989324d3181cb77caa5384244c8aff31c4
Ricardo Pereira <m@ricardopereira.eu>
2016-04-09 15:55:32 +0100

    RSA8f3: misplaced

--- Spec/Auth.swift
+++ Spec/Auth.swift
@@ -923,6 +923,37 @@ class Auth : QuickSpec {
                 expect(rest.auth.clientId).to(beNil())
             }
 
+            // RSA8f3
+            it("ensure the message published with a wildcard '*' does not have a clientId") {
+                let options = AblyTests.commonAppSetup()
+                // Request a token with a wildcard '*' value clientId
+                options.token = getTestToken(clientId: "*")
+                let rest = ARTRest(options: options)
+                rest.httpExecutor = mockExecutor
+                let channel = rest.channels.get("test")
+
+                waitUntil(timeout: testTimeout) { done in
+                    let message = ARTMessage(name: nil, data: "no client")
+                    expect(message.clientId).to(beNil())
+                    channel.publish([message]) { error in
+                        expect(error).to(beNil())
+                        switch extractBodyAsMessages(mockExecutor.requests.first) {
+                        case .Failure(let error):
+                            fail(error)
+                        case .Success(let httpBody):
+                            expect(httpBody.unbox.first!["clientId"]).to(beNil())
+                        }
+                        channel.history { page, error in
+                            expect(error).to(beNil())
+                            expect(page!.items).to(haveCount(1))
+                            expect((page!.items[0] as! ARTMessage).clientId).to(beNil())
+                            done()
+                        }
+                    }
+                }
+                expect(rest.auth.clientId).to(beNil())
+            }
+
             // RSA8f4
             it("ensure the message published with a wildcard '*' has the provided clientId") {
                 let options = AblyTests.commonAppSetup()
@@ -946,37 +977,6 @@ class Auth : QuickSpec {
                 expect(rest.auth.clientId).to(beNil())
             }
 
-        }
-
-        // RSA8f3
-        it("ensure the message published with a wildcard '*' does not have a clientId") {
-            let options = AblyTests.commonAppSetup()
-            // Request a token with a wildcard '*' value clientId
-            options.token = getTestToken(clientId: "*")
-            let rest = ARTRest(options: options)
-            rest.httpExecutor = mockExecutor
-            let channel = rest.channels.get("test")
-
-            waitUntil(timeout: testTimeout) { done in
-                let message = ARTMessage(name: nil, data: "no client")
-                expect(message.clientId).to(beNil())
-                channel.publish([message]) { error in
-                    expect(error).to(beNil())
-                    switch extractBodyAsMessages(mockExecutor.requests.first) {
-                    case .Failure(let error):
-                        fail(error)
-                    case .Success(let httpBody):
-                        expect(httpBody.unbox.first!["clientId"]).to(beNil())
-                    }
-                    channel.history { page, error in
-                        expect(error).to(beNil())
-                        expect(page!.items).to(haveCount(1))
-                        expect((page!.items[0] as! ARTMessage).clientId).to(beNil())
-                        done()
-                    }
-                }
-            }
-            expect(rest.auth.clientId).to(beNil())
         }
 
         struct ExpectedTokenParams {
