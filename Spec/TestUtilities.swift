55f905be9e0a04e838357d352f7450b1d86dadcf
Ricardo Pereira <ricardopereira.eu@gmail.com>
2016-04-29 17:03:51 +0100

    RTP14 (#429)

--- Spec/TestUtilities.swift
+++ Spec/TestUtilities.swift
@@ -670,6 +670,22 @@ extension SequenceType where Generator.Element: NSData {
     
 }
 
+func + <K,V> (left: Dictionary<K,V>, right: Dictionary<K,V>?) -> Dictionary<K,V> {
+    guard let right = right else { return left }
+    return left.reduce(right) {
+        var new = $0 as [K:V]
+        new.updateValue($1.1, forKey: $1.0)
+        return new
+    }
+}
+
+func += <K,V> (inout left: Dictionary<K,V>, right: Dictionary<K,V>?) {
+    guard let right = right else { return }
+    right.forEach { key, value in
+        left.updateValue(value, forKey: key)
+    }
+}
+
 extension ARTMessage {
 
     public override func isEqual(object: AnyObject?) -> Bool {
