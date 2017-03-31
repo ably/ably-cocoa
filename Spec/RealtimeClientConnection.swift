af48508d77884cd6f97f58799eec3991f57ffc8c
Ricardo Pereira <m@ricardopereira.eu>
2017-03-31 15:01:55 +0100

    Bump version to 0.8.11.

--- Spec/RealtimeClientConnection.swift
+++ Spec/RealtimeClientConnection.swift
@@ -213,7 +213,7 @@ class RealtimeClientConnection: QuickSpec {
                             done()
                         case .Connected:
                             if let transport = client.transport as? TestProxyTransport, let query = transport.lastUrl?.query {
-                                expect(query).to(haveParam("lib", withValue: "ios-0.8.10"))
+                                expect(query).to(haveParam("lib", withValue: "ios-0.8.11"))
                             }
                             else {
                                 XCTFail("MockTransport isn't working")
