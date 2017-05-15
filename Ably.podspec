e3ca11e519213c2100f7c24f93e0b83c95f18bd9
Toni Cárdenas <toni@tcardenas.me>
2017-05-15 06:30:10 +0200

    Bump version to 1.0.4.

--- Ably.podspec
+++ Ably.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name              = "Ably"
-  s.version           = "1.0.3"
+  s.version           = "1.0.4"
   s.summary           = "iOS client for Ably"
   s.description       = <<-DESC
                         iOS client library for ably.io, the realtime messaging service, written in Objective-C and ready for Swift 2.0.
--- Spec/RealtimeClientConnection.swift
+++ Spec/RealtimeClientConnection.swift
@@ -213,7 +213,7 @@ class RealtimeClientConnection: QuickSpec {
                             done()
                         case .Connected:
                             if let transport = client.transport as? TestProxyTransport, let query = transport.lastUrl?.query {
-                                expect(query).to(haveParam("lib", withValue: "ios-1.0.3"))
+                                expect(query).to(haveParam("lib", withValue: "ios-1.0.4"))
                             }
                             else {
                                 XCTFail("MockTransport isn't working")
