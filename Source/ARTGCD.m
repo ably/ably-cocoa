d1fea9cb99868934d8e287fa1e54f67f28948e50
Toni Cárdenas <toni@tcardenas.me>
2017-06-15 14:47:06 +0200

    Make artDispatchSync avoid transitive deadlocks.

--- Source/ARTGCD.m
+++ Source/ARTGCD.m
@@ -48,10 +48,53 @@ void artDispatchCancel(dispatch_block_t block) {
     if (block) dispatch_block_cancel(block);
 }
 
+// artDispatchSync is like dispatch_sync, but if the a thread attempts to
+// dispatch_sync to a queue to which it already is dispatch_syncinc, it calls
+// the block right away.
 void artDispatchSync(dispatch_queue_t queue, dispatch_block_t block) {
-    if (dispatch_get_current_queue() == queue) {
+    NSString *queueName = [NSString stringWithUTF8String:dispatch_queue_get_label(queue)];
+    
+    static NSMutableDictionary<NSString *, NSMutableSet<NSThread *> *> *threadsUsingQueues;
+    // threadsUsingQueuesOps is the serial queue we use to operate on threadsUsingQueues,
+    // to avoid concurrent operations on it.
+    static dispatch_queue_t threadsUsingQueuesOps;
+
+    // Initialize locks set and its queue.
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        threadsUsingQueues = [[NSMutableDictionary alloc] init];
+        threadsUsingQueuesOps = dispatch_queue_create("io.ably.artDispatchSync.threadsUsingQueuesOps", DISPATCH_QUEUE_SERIAL);
+    });
+
+    bool __block queueIsUsedByThisThread = false;
+
+    dispatch_sync(threadsUsingQueuesOps, ^{
+        NSMutableSet<NSThread *> *threads = threadsUsingQueues[queueName];
+        if (!threads) {
+            threads = [[NSMutableSet alloc] init];
+            threadsUsingQueues[queueName] = threads;
+        } else if ([threads containsObject:[NSThread currentThread]]) {
+            queueIsUsedByThisThread = true;
+            return;
+        }
+        [threads addObject:[NSThread currentThread]];
+    });
+
+    if (queueIsUsedByThisThread) {
         block();
     } else {
-        dispatch_sync(queue, block);
+        dispatch_sync(queue, ^{
+            @try {
+                block();
+            } @finally {
+                dispatch_sync(threadsUsingQueuesOps, ^{
+                    NSMutableSet<NSThread *> *threads = threadsUsingQueues[queueName];
+                    [threads removeObject:[NSThread currentThread]];
+                    if ([threads count] == 0) {
+                        [threadsUsingQueues removeObjectForKey:queueName];
+                    }
+                });
+            }
+        });
     }
 }
