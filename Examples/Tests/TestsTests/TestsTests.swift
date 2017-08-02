15aec59395c6cb5e5f6b00b3c232f658e5a936a1
Toni Cárdenas <toni@tcardenas.me>
2017-08-02 17:19:31 +0200

    Convert Examples project to Swift 3.

--- Examples/Tests/TestsTests/TestsTests.swift
+++ Examples/Tests/TestsTests/TestsTests.swift
@@ -16,28 +16,28 @@ class TestsTests: XCTestCase {
     let options: ARTClientOptions! = nil
 
     func testAblyWorks() {
-        var responseData: NSData?
+        var responseData: Data?
 
-        let postAppExpectation = self.expectationWithDescription("POST app to sandbox")
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://sandbox-rest.ably.io:443/apps")!)
-        request.HTTPMethod = "POST"
-        request.HTTPBody = "{\"keys\":[{}]}".dataUsingEncoding(NSUTF8StringEncoding)
+        let postAppExpectation = self.expectation(description: "POST app to sandbox")
+        let request = NSMutableURLRequest(url: URL(string: "https://sandbox-rest.ably.io:443/apps")!)
+        request.httpMethod = "POST"
+        request.httpBody = "{\"keys\":[{}]}".data(using: String.Encoding.utf8)
         request.allHTTPHeaderFields = [
             "Accept" : "application/json",
             "Content-Type" : "application/json"
         ]
-        NSURLSession.sharedSession().dataTaskWithRequest(request) { data, _, error in
+        URLSession.shared.dataTask(with: request, completionHandler: { data, _, error in
             defer { postAppExpectation.fulfill() }
             if let e = error {
                 XCTFail("Error setting up sandbox app: \(e)")
                 return
             }
             responseData = data
-        }.resume()
-        self.waitForExpectationsWithTimeout(10, handler: nil)
+        }) .resume()
+        self.waitForExpectations(timeout: 10, handler: nil)
 
         guard let key = responseData
-            .flatMap({ try? NSJSONSerialization.JSONObjectWithData($0, options: NSJSONReadingOptions(rawValue: 0)) })
+            .flatMap({ try? JSONSerialization.jsonObject(with: $0, options: JSONSerialization.ReadingOptions(rawValue: 0)) })
             .flatMap({ $0 as? NSDictionary })
             .flatMap({ $0["keys"] as? NSArray })
             .flatMap({ $0[0] as? NSDictionary })
@@ -51,7 +51,7 @@ class TestsTests: XCTestCase {
         options.environment = "sandbox"
         let client = ARTRealtime(options: options)
 
-        let receiveExpectation = self.expectationWithDescription("message received")
+        let receiveExpectation = self.expectation(description: "message received")
 
         client.channels.get("test").subscribe { message in
             XCTAssertEqual(message.data as? NSString, "Get this!")
@@ -61,26 +61,26 @@ class TestsTests: XCTestCase {
         
         client.channels.get("test").publish(nil, data: "Get this!")
 
-        self.waitForExpectationsWithTimeout(10, handler: nil)
+        self.waitForExpectations(timeout: 10, handler: nil)
 
-        let backgroundRealtimeExpectation = self.expectationWithDescription("Realtime in a Background Queue")
+        let backgroundRealtimeExpectation = self.expectation(description: "Realtime in a Background Queue")
         var realtime: ARTRealtime! //strong reference
-        NSURLSession.sharedSession().dataTaskWithURL(NSURL(string:"https://ably.io")!) { _ in
+        URLSession.shared.dataTask(with: URL(string:"https://ably.io")!, completionHandler: { _ in
             realtime = ARTRealtime(key: key as String)
             realtime.channels.get("foo").attach { _ in
                 defer { backgroundRealtimeExpectation.fulfill() }
             }
-        }.resume()
-        self.waitForExpectationsWithTimeout(10, handler: nil)
+        }) .resume()
+        self.waitForExpectations(timeout: 10, handler: nil)
 
-        let backgroundRestExpectation = self.expectationWithDescription("Rest in a Background Queue")
+        let backgroundRestExpectation = self.expectation(description: "Rest in a Background Queue")
         var rest: ARTRest! //strong reference
-        NSURLSession.sharedSession().dataTaskWithURL(NSURL(string:"https://ably.io")!) { _ in
+        URLSession.shared.dataTask(with: URL(string:"https://ably.io")!, completionHandler: { _ in
             rest = ARTRest(key: key as String)
             rest.channels.get("foo").history { _ in
                 defer { backgroundRestExpectation.fulfill() }
             }
-        }.resume()
-        self.waitForExpectationsWithTimeout(10, handler: nil)
+        }) .resume()
+        self.waitForExpectations(timeout: 10, handler: nil)
     }
 }
