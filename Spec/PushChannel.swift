dcdebf76bbd4cf1725ee57d4b7c1337c5ffa77ca
Ricardo Pereira <ricardopereira@whitesmith.co>
2018-03-23 11:17:53 +0000

    PushChannel

---
+++ Spec/PushChannel.swift
@@ -0,0 +1,18 @@
+//
+//  PushChannel.swift
+//  Ably
+//
+//  Created by Ricardo Pereira on 23/03/2018.
+//  Copyright © 2018 Ably. All rights reserved.
+//
+
+import Ably
+import Nimble
+import Quick
+
+class PushChannel : QuickSpec {
+    override func spec() {
+
+    }
+
+}
