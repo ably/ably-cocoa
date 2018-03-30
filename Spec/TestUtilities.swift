fbdd09ee4f85f8ab46f8dfc1c787bd880dc9ed2e
Ricardo Pereira <ricardopereira@whitesmith.co>
2018-03-30 11:42:30 +0100

    MockHTTPExecutor should return a valid and successfull response

--- Spec/TestUtilities.swift
+++ Spec/TestUtilities.swift
@@ -727,7 +727,7 @@ class MockHTTPExecutor: NSObject, ARTHTTPAuthenticatedExecutor {
             return
         }
 
-        callback(nil, nil, nil)
+        callback(HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: ["X-Ably-HTTPExecutor": "MockHTTPExecutor"]), nil, nil)
     }
 
     func execute(_ request: URLRequest, completion callback: ((HTTPURLResponse?, Data?, Error?) -> Void)? = nil) {
@@ -739,7 +739,7 @@ class MockHTTPExecutor: NSObject, ARTHTTPAuthenticatedExecutor {
             return
         }
 
-        callback?(nil, nil, nil)
+        callback?(HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: ["X-Ably-HTTPExecutor": "MockHTTPExecutor"]), nil, nil)
     }
 
     func simulateIncomingErrorOnNextRequest(_ error: NSError) {
