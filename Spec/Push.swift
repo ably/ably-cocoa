9acb5d199ce40f5ed1db2bb922c0a68a865e7ae0
Ricardo Pereira <ricardopereira@whitesmith.co>
2018-05-04 20:43:37 +0100

    Add Push spec file

---
+++ Spec/Push.swift
@@ -0,0 +1,17 @@
+//
+//  Push.swift
+//  AblySpec
+//
+//  Created by Ricardo Pereira on 04/05/2018.
+//  Copyright © 2018 Ably. All rights reserved.
+//
+
+import Ably
+import Nimble
+import Quick
+
+class Push : QuickSpec {
+    override func spec() {
+
+    }
+}
