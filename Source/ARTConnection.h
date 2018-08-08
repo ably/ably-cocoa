369d6f95baa579e3a2a11cd2544b37aaa64b1482
Cesare Rocchi <cesarerocchi@gmail.com>
2018-08-08 11:28:47 +0200

    Expose maxMessageSize property in connection

--- Source/ARTConnection.h
+++ Source/ARTConnection.h
@@ -21,6 +21,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nullable, readonly, strong, nonatomic) NSString *key;
 @property (nullable, readonly, getter=getRecoveryKey) NSString *recoveryKey;
 @property (readonly, assign, nonatomic) int64_t serial;
+@property (readonly, assign, nonatomic) NSInteger maxMessageSize;
 @property (readonly, assign, nonatomic) ARTRealtimeConnectionState state;
 @property (nullable, readonly, strong, nonatomic) ARTErrorInfo *errorReason;
 
