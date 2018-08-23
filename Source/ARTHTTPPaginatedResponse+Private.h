a5060862cb6ebde6ac6960b6cab65f1028371481
Ricardo Pereira <ricardopereira@whitesmith.co>
2018-08-23 16:42:02 +0100

    ARTHTTPPaginatedResponse

---
+++ Source/ARTHTTPPaginatedResponse+Private.h
@@ -0,0 +1,19 @@
+//
+//  ARTHTTPPaginatedResponse+Private.h
+//  Ably
+//
+//  Created by Yavor Georgiev on 28.08.15.
+//  Copyright (c) 2015 г. Ably. All rights reserved.
+//
+
+#import <Ably/ARTHTTPPaginatedResponse.h>
+
+@class ARTRest;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ARTHTTPPaginatedResponse ()
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/ARTHTTPPaginatedResponse.m
@@ -0,0 +1,23 @@
+//
+//  ARTHTTPPaginatedResponse.m
+//  Ably
+//
+//  Created by Ricardo Pereira on 17/08/2018.
+//  Copyright © 2018 Ably. All rights reserved.
+//
+
+#import "ARTHTTPPaginatedResponse.h"
+
+#import "ARTHttp.h"
+#import "ARTAuth.h"
+#import "ARTRest+Private.h"
+#import "ARTPaginatedResult+Private.h"
+#import "ARTNSMutableURLRequest+ARTPaginated.h"
+#import "ARTNSHTTPURLResponse+ARTPaginated.h"
+#import "ARTEncoder.h"
+#import "ARTConstants.h"
+
+@implementation ARTHTTPPaginatedResponse {
+}
+
+@end
