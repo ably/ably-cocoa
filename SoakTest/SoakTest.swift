d5c6fc089704685a17cc06b5da086f50af8fbad3
Toni Cárdenas <toni@tcardenas.me>
2019-11-09 08:56:10 +0100

    Create Xcode artifacts to run soak test.

---
+++ SoakTest/SoakTest.swift
@@ -0,0 +1,18 @@
+//
+//  SoakTest.swift
+//  Ably-iOS-SoakTest
+//
+//  Created by Toni Cárdenas on 09/11/2019.
+//  Copyright © 2019 Ably. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Ably
+
+class SoakTest: XCTestCase {
+    func testSoak() {
+        var _: ARTRealtime?
+        XCTAssertTrue(true)
+    }
+}
