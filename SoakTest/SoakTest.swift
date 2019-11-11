57a879ed764880e3ca4f5b21b40d6a4d9c915836
Toni Cárdenas <toni@tcardenas.me>
2019-11-11 11:18:24 +0100

    Get basic path working: send and receive messages.

--- SoakTest/SoakTest.swift
+++ SoakTest/SoakTest.swift
@@ -10,14 +10,121 @@ import Foundation
 import XCTest
 import Ably.Private
 
+let randomSeed: Int = 13
+let concurrentConnections: Int = 1000
+let runTime: TimeInterval = 60 * 30
+
 class SoakTest: XCTestCase {
     func testSoak() {
         ARTWebSocketTransport.setWebSocketClass(SoakTestWebSocket.self)
         ARTHttp.setURLSessionClass(SoakTestURLSession.self)
-        if let realtime = (nil as ARTRealtime?) {
-            realtime.internal.setReachabilityClass(SoakTestReachability.self)
+
+        var queues: [DispatchQueue] = []
+        
+        for i in (0 ..< concurrentConnections) {
+            let queue = DispatchQueue(label: "io.ably.soakTest.\(i)")
+            let internalQueue = DispatchQueue(label: "io.ably.soakTest.internal.\(i)")
+            queues.append(queue)
+            queues.append(internalQueue)
+
+            queue.async {
+                let options = ARTClientOptions(key: "fake:key")
+                options.autoConnect = false
+                options.logLevel = .debug
+                options.dispatchQueue = queue
+                options.internalDispatchQueue = internalQueue
+                let realtime = ARTRealtime(options: options)
+                realtime.internal.setReachabilityClass(SoakTestReachability.self)
+                
+                queue.afterSeconds(between: 0.0 ... 2.0) {
+                    realtime.connection.on { state in
+                        print("got connection notification; error: \(String(describing: state?.reason))")
+                        queue.afterSeconds(between: 0.0 ... 120.0) {
+                            realtime.close()
+                        }
+                    }
+                    realtime.connect()
+                }
+                
+                sendMessages(realtime: realtime, queue: queue)
+            }
         }
         
-        XCTAssertTrue(true)
+        Thread.sleep(forTimeInterval: runTime)
+        
+        for queue in queues {
+            queue.suspend()
+        }
     }
 }
+
+func sendMessages(realtime: ARTRealtime, queue: DispatchQueue) {
+    queue.afterSeconds(between: 0.1 ... 1.0) {
+        if realtime.connection.state == .closed {
+            return
+        }
+        sendMessages(realtime: realtime, queue: queue)
+
+        let channel = realtime.channels.get("channel.\(Int((0 ... 100).randomWithin()))")
+        channel.subscribe { message in
+            print("got message: \(message)")
+        }
+        channel.publish("fakeMessage", data: messageFixtures.randomElement(using: &seededRandomNumberGenerator) ?? nil) { error in
+                print("got message ack; error: \(String(describing: error))")
+        }
+    }
+}
+
+let messageFixtures: [Any?] = [
+    "a string",
+    ["some", ["values": 456]],
+    nil,
+]
+
+var randDouble : () -> Double = {
+    srand48(randomSeed)
+    return { drand48() }
+}()
+
+class SeededRandomNumberGenerator : RandomNumberGenerator {
+    func next() -> UInt64 {
+        return UInt64((0 ... UInt64.max).randomWithin())
+    }
+}
+
+var seededRandomNumberGenerator = SeededRandomNumberGenerator()
+
+extension ClosedRange where Bound : BinaryFloatingPoint {
+    func randomWithin() -> Double {
+        return Double(lowerBound) + Double(upperBound - lowerBound) * randDouble()
+    }
+}
+
+extension ClosedRange where Bound : BinaryInteger {
+    func randomWithin() -> Double {
+        return (Double(self.lowerBound) ... Double(self.upperBound)).randomWithin()
+    }
+}
+
+extension Double {
+    func secondsFromNow() -> DispatchTime {
+        return DispatchTime.now() + .milliseconds(Int(self * 1000))
+    }
+}
+
+extension DispatchQueue {
+    func afterSeconds(between secondsRange: ClosedRange<Double>, execute: @escaping () -> Void) {
+        self.asyncAfter(deadline: secondsRange.randomWithin().secondsFromNow(), execute: execute)
+    }
+}
+
+extension Bool {
+    func times(_ times: Int, outOf: Int) -> Bool {
+        if (0 ... outOf).randomWithin() < Double(times) {
+            return self
+        } else {
+            return !self
+        }
+    }
+}
+
--- SoakTest/SoakTestWebSocket.swift
+++ SoakTest/SoakTestWebSocket.swift
@@ -10,23 +10,207 @@ import Ably.Private
 import SocketRocketAblyFork
 
 class SoakTestWebSocket: NSObject, ARTWebSocket {
-    var delegate: ARTWebSocketDelegate?
-    
     var readyState: SRReadyState
-    
+    var queue: DispatchQueue!
+    var delegate: ARTWebSocketDelegate?
+
+    let id = nextGlobalSerial()
+    let nextConnectionSerial: () -> Int64
+
     required init(urlRequest request: URLRequest) {
         readyState = .CLOSED
+        // TODO (maybe?): Extract connectionKey from params, resume conn state if
+        // connectionStateTtl hasn't passed yet.
+        nextConnectionSerial = serialSequence(label: "fakeConnection.\(id)", first: -1)
     }
     
     func setDelegateDispatchQueue(_ queue: DispatchQueue) {
+        self.queue = queue
     }
     
     func open() {
+        readyState = .CONNECTING
+        queue.afterSeconds(between: 0.1 ... 3.0) {
+            if true.times(9, outOf: 10) {
+                self.readyState = .OPEN
+                self.delegate?.webSocketDidOpen(self)
+                
+                self.doIfStillOpen(afterSecondsBetween: 0.1 ... 3.0) {
+                    if true.times(9, outOf: 10) {
+                        self.messageToClient(action: .connected) { m in
+                            m.connectionId = "fakeConnection.\(self.id)"
+                            m.connectionDetails = ARTConnectionDetails(
+                                clientId: "*",
+                                connectionKey: "fakeConnectionKey.\(self.id))",
+                                maxMessageSize: 999999999,
+                                maxFrameSize: 999999999,
+                                maxInboundRate: 999999999,
+                                connectionStateTtl: 2.0,
+                                serverId: "fakeServer",
+                                maxIdleInterval: 0
+                            )
+                        }
+                        self.ackMessages()
+                        self.sendHeartbeats()
+                    } else {
+                        self.messageToClient(action: .error) { m in
+                            m.error = ARTErrorInfo.create(from: fakeError)
+                        }
+                    }
+                }
+                
+                self.doIfStillOpen(afterSecondsBetween: 3.0 ... 300.0) {
+                    self.delegate?.webSocket(self, didCloseWithCode: SRStatusCode.codeAbnormal.rawValue, reason: "fake abrupt close", wasClean: false)
+                }
+            } else {
+                self.delegate?.webSocket(self, didFailWithError: fakeError)
+            }
+        }
+    }
+    
+    func sendHeartbeats() {
+        doIfStillOpen(afterSecondsBetween: (2.0 ... 30.0)) {
+            self.messageToClient(action: .heartbeat)
+            self.sendHeartbeats()
+        }
+    }
+    
+    func protocolMessage(action: ARTProtocolMessageAction, setUp: (ARTProtocolMessage) -> Void = { _ in }) -> ARTProtocolMessage {
+        return ARTProtocolMessage.build(action: action) { m in
+            m.connectionSerial = self.nextConnectionSerial()
+            m.timestamp = Date()
+            setUp(m)
+        }
+    }
+    
+    func messageToClient(action: ARTProtocolMessageAction, setUp: (ARTProtocolMessage) -> Void = { _ in }) {
+        let message = protocolMessage(action: action, setUp: setUp)
+        self.delegate?.webSocket(self, didReceiveMessage: message)
     }
     
     func close(withCode code: Int, reason: String?) {
+        readyState = .CLOSING
+        queue.afterSeconds(between: 0.1 ... 3.0) {
+            if self.readyState != .CLOSING {
+                return
+            }
+            self.readyState = .CLOSED
+        }
     }
     
-    func send(_ message: Any?) {
+    var pendingSerials: [NSNumber] = []
+    
+    func ackMessages() {
+        doIfStillOpen(afterSecondsBetween: 0.1 ... 3.0) {
+            self.ackMessages()
+
+            if self.pendingSerials.count == 0 {
+                return
+            }
+            self.messageToClient(action: .ack) { m in
+                m.msgSerial = self.pendingSerials.first!
+                m.count = Int32(self.pendingSerials.count)
+            }
+            self.pendingSerials.removeAll()
+        }
+    }
+    
+    func send(_ msgPackEncodedMessage: Any?) {
+        let message = try! msgPackEncoder.decodeProtocolMessage(msgPackEncodedMessage as! Data)
+        
+        switch message.action {
+        case .close:
+            doIfStillOpen(afterSecondsBetween: 0.1 ... 3.0) {
+                self.messageToClient(action: .closed)
+                self.doIfStillOpen(afterSecondsBetween: 0.1 ... 0.5) {
+                    self.delegate?.webSocket(self, didCloseWithCode: 1000, reason: nil, wasClean: true)
+                }
+            }
+        case .message:
+            queue.async {
+                self.pendingSerials.append(message.msgSerial!)
+            }
+        case .attach:
+            doIfStillOpen(afterSecondsBetween: 0.1 ... 3.0) {
+                let serial: Int64
+                if let s = self.serialForAttachedChannel[message.channel!] {
+                    serial = s
+                } else {
+                    serial = -1
+                    self.serialForAttachedChannel[message.channel!] = -1
+                }
+                self.messageToClient(action: .attached) { m in
+                    m.channel = message.channel
+                    m.channelSerial = "somethingsomething:\(serial)"
+                }
+
+                self.sendMessages(channel: message.channel!)
+            }
+        default:
+            break
+        }
+    }
+    
+    var serialForAttachedChannel: [String: Int64] = [:]
+    
+    func sendMessages(channel: String) {
+        doIfStillOpen(afterSecondsBetween: 0.1 ... 2.0) {
+            guard var channelSerial = self.serialForAttachedChannel[channel] else {
+                return
+            }
+            channelSerial += 1
+            self.serialForAttachedChannel[channel] = channelSerial
+            
+            self.messageToClient(action: .message) { m in
+                m.id = "message.\(nextGlobalSerial())"
+                m.channel = channel
+                m.channelSerial = "somethingsomething:\(channelSerial)"
+                m.messages = [ARTMessage(
+                    name: "fakeMessage",
+                    data: messageFixtures.randomElement(using: &seededRandomNumberGenerator) as Any
+                )]
+            }
+            
+            self.sendMessages(channel: channel)
+        }
+    }
+    
+    func doIfStillOpen(afterSecondsBetween between: ClosedRange<TimeInterval>, execute: @escaping () -> Void) {
+        queue.afterSeconds(between: between) {
+            if self.readyState != .OPEN {
+                return
+            }
+            execute()
+        }
+    }
+}
+
+let msgPackEncoder = ARTJsonLikeEncoder(delegate: ARTMsgPackEncoder())
+
+let fakeError = NSError(domain: "io.ably", code: 1, userInfo: [NSLocalizedDescriptionKey: "fake error for soak test"])
+
+func serialSequence(label: String, first: Int64 = 0) -> (() -> Int64) {
+    let queue = DispatchQueue(label: "io.ably.soakTest.\(label)")
+    var serial: Int64 = first
+    
+    return {
+        var assigned: Int64!
+        queue.sync {
+            assigned = serial
+            serial += 1
+        }
+        return assigned
+    }
+}
+
+let nextGlobalSerial = serialSequence(label: "globalSerial")
+
+extension ARTProtocolMessage {
+    class func build(action: ARTProtocolMessageAction, setUp: (ARTProtocolMessage) -> Void) -> ARTProtocolMessage {
+        let m = ARTProtocolMessage()
+        m.action = action
+        m.id = "fakeProtocolMessage.\(nextGlobalSerial())"
+        setUp(m)
+        return m
     }
 }
