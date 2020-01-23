d0fa780683c076095dadce3c847fd7c9b2b3dd9b
Ricardo Pereira <ricardopereira@whitesmith.co>
2020-01-23 11:50:06 +0000

    Expose 'setOptions' (special case for Realtime because of the callback)

--- Source/ARTRealtimeChannel.m
+++ Source/ARTRealtimeChannel.m
@@ -196,6 +196,10 @@
     return [_internal on:event callback:cb];
 }
 
+- (void)setOptions:(ARTChannelOptions *_Nullable)options callback:(nullable void (^)(ARTErrorInfo *_Nullable))cb {
+    return [_internal setOptions:options callback:cb];
+}
+
 @end
 
 @interface ARTRealtimeChannelInternal () {
@@ -1435,6 +1439,10 @@ ART_TRY_OR_MOVE_TO_FAILED_START(_realtime) {
         maxSize = self.realtime.connection.maxMessageSize;
     }
     return size > maxSize;
+}
+
+- (void)setOptions:(ARTChannelOptions *_Nullable)options callback:(nullable void (^)(ARTErrorInfo *_Nullable))callback {
+
 }
 
 @end
