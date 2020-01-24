94ab33c366475af271942a09c020729b1792025a
Ricardo Pereira <ricardopereira@whitesmith.co>
2020-01-24 11:32:58 +0000

    Create ARTRealtimeChannelOptions class + ChannelOptions enum

---
+++ Source/ARTRealtimeChannelOptions.m
@@ -0,0 +1,13 @@
+//
+//  ARTRealtimeChannelOptions.m
+//  Ably-iOS
+//
+//  Created by Ricardo Pereira on 24/01/2020.
+//  Copyright © 2020 Ably. All rights reserved.
+//
+
+#import "ARTRealtimeChannelOptions.h"
+
+@implementation ARTRealtimeChannelOptions
+
+@end
