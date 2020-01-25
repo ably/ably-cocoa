d788614b2a7f3c5ba602cd9ac311452dad2cf8d9
Ricardo Pereira <ricardopereira@whitesmith.co>
2020-01-25 12:59:57 +0000

    Realtime setOptions implementation per RTL16a

--- Source/ARTProtocolMessage.h
+++ Source/ARTProtocolMessage.h
@@ -65,6 +65,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (readwrite, assign, nonatomic) int64_t flags;
 @property (nullable, readwrite, nonatomic) ARTConnectionDetails *connectionDetails;
 @property (nullable, nonatomic) ARTAuthDetails *auth;
+@property (nonatomic, strong, nullable) NSDictionary<NSString *, NSString *> *params;
 
 @end
 
