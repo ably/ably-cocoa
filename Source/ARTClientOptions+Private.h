b0895779f45e136e5c57a715ee8866925d48adaa
Ricardo Pereira <ricardopereira@whitesmith.co>
2020-02-09 00:00:03 +0000

    Put ClientOptions.plugins as private

--- Source/ARTClientOptions+Private.h
+++ Source/ARTClientOptions+Private.h
@@ -12,6 +12,11 @@
 
 @property (nullable, strong, nonatomic) NSString *channelNamePrefix;
 
+/**
+ Specific client library features that are not commonly used may be supplied as independent libraries.
+ */
+@property (nullable, nonatomic, copy) NSSet<ARTPlugin *> *plugins;
+
 + (void)setDefaultEnvironment:(NSString *_Nullable)environment;
 + (BOOL)getDefaultIdempotentRestPublishingForVersion:(NSString *_Nonnull)version;
 - (NSURLComponents *_Nonnull)restUrlComponents;
