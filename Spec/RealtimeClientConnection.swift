31265baf90f6adc3fd27213d4118f4c330c03eb6
Quintin Willison <quintin@ably.io>
2020-04-29 23:00:58 +0100

    Bump version to 1.1.23.

--- Spec/RealtimeClientConnection.swift
+++ Spec/RealtimeClientConnection.swift
@@ -244,7 +244,7 @@ class RealtimeClientConnection: QuickSpec {
                             done()
                         case .connected:
                             if let transport = client.internal.transport as? TestProxyTransport, let query = transport.lastUrl?.query {
-                                expect(query).to(haveParam("lib", withValue: "cocoa\(ARTDefault_variant)-1.1.22"))
+                                expect(query).to(haveParam("lib", withValue: "cocoa\(ARTDefault_variant)-1.1.23"))
                             }
                             else {
                                 XCTFail("MockTransport isn't working")
