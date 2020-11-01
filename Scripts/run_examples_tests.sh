cb24b46f94cfaf60f701d9c9127ce95a3ba609f0
Lawrence Forooghian <lawrence@forooghian.com>
2020-11-01 19:26:07 +0000

    Fix CocoaPods build
    Similarly to 7c18fe2, SocketRocket’s angle-bracket #imports are a thorn
    in our side. We use a prepare_command in the podspec to replace them
    with double-quoted #imports.

--- Scripts/run_examples_tests.sh
+++ Scripts/run_examples_tests.sh
@@ -3,6 +3,12 @@
 # exit if a command fails
 set -e
 
+# This is the script specified as the pod’s prepare_command in its Podspec.
+# It would be run automatically for a normal CocoaPods install, but it doesn’t
+# get run when the dependency is specified with the :path option. So we run it
+# manually.
+Scripts/prepare-pod.sh
+
 cd "Examples/Tests"
 pod repo update
 pod install
