1db8c400a9dcce0c6e25e1860dd2888f562097ab
Ricardo Pereira <ricardopereira@whitesmith.co>
2021-03-18 17:00:52 +0000

    Fix implementation

--- Source/ARTAuth.m
+++ Source/ARTAuth.m
@@ -783,14 +783,15 @@ dispatch_sync(_queue, ^{
 
 #if TARGET_OS_IOS
 - (void)setLocalDeviceClientId_nosync:(NSString *)clientId {
-    if (clientId && ![clientId isEqualToString:@"*"]) {
-        [_rest.device_nosync setClientId:clientId];
-        [_rest.push getActivationMachine:^(ARTPushActivationStateMachine *stateMachine) {
-            if (![stateMachine.current_nosync isKindOfClass:[ARTPushActivationStateNotActivated class]]) {
-                [stateMachine sendEvent:[[ARTPushActivationEventGotPushDeviceDetails alloc] init]];
-            }
-        }];
+    if (clientId == nil || [clientId isEqualToString:@"*"] || [clientId isEqualToString:_rest.device_nosync.clientId]) {
+        return;
     }
+    [_rest.device_nosync setClientId:clientId];
+    [_rest.push getActivationMachine:^(ARTPushActivationStateMachine *stateMachine) {
+        if (![stateMachine.current_nosync isKindOfClass:[ARTPushActivationStateNotActivated class]]) {
+            [stateMachine sendEvent:[[ARTPushActivationEventGotPushDeviceDetails alloc] init]];
+        }
+    }];
 }
 #endif
 
