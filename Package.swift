311b50427990607aa3ca644828ec3a5c3d447af5
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2021-06-08 08:07:52 +0200

    [819] Add swift package manager config file

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "ably-cocoa",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v9),
+        .tvOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "Ably",
+            targets: ["Ably"]
+        ),
+    ],
+    dependencies: [
+        .package(name: "ULID", url: "https://github.com/whitesmith/ulid", from: "1.2.0"),
+        .package(name: "msgpack", url: "https://github.com/rvi/msgpack-objective-C", from: "0.4.0"),
+        .package(name: "AblyDeltaCodec", url: "https://github.com/ably/delta-codec-cocoa", .branch("main"))
+    ],
+    targets: [
+        .target(
+            name: "Ably",
+            dependencies: [
+                .byName(name: "ULID"),
+                .byName(name: "msgpack"),
+                .byName(name: "AblyDeltaCodec")
+            ],
+            path: "Source"
+        )
+    ]
+)
