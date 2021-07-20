ae6f951b5ff75f7f55fcc14a85af511a5480edb0
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2021-07-20 10:38:36 +0200

    Add file to private headers in module map

--- Source/Ably.h
+++ Source/Ably.h
@@ -71,5 +71,4 @@ FOUNDATION_EXPORT const unsigned char ablyVersionString[];
 #import <Ably/ARTDeltaCodec.h>
 #import <Ably/CompatibilityMacros.h>
 #import <Ably/ARTPendingMessage.h>
-#import <Ably/NSRunLoop+ARTSRWebSocket.h>
 
