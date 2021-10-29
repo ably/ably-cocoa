680c45ab40fd7c38280689f5af84d963c96801d0
Marat Al <m.alekperov@gmail.com>
2021-10-29 23:03:13 +0200

    Xcode static analyser issue fix: - nullable for getters for corresponding nullable properties - removed unnecessary nil check

--- Source/ARTConnection.m
+++ Source/ARTConnection.m
@@ -245,12 +245,7 @@ dispatch_sync(_queue, ^{
         case ARTRealtimeConnected:
         case ARTRealtimeDisconnected:
         case ARTRealtimeSuspended: {
-            NSString *recStr = self.key_nosync;
-            if (recStr == nil) {
-                return nil;
-            }
-            NSString *str = [recStr stringByAppendingString:[NSString stringWithFormat:@":%ld:%ld", (long)self.serial_nosync, (long)_realtime.msgSerial]];
-            return str;
+            return [self.key_nosync stringByAppendingString:[NSString stringWithFormat:@":%ld:%ld", (long)self.serial_nosync, (long)_realtime.msgSerial]];
         } default:
             return nil;
     }
