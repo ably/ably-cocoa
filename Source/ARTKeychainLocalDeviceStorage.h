4f2ba99d9aff52c8a9fddc8920cc02525d615a83
Marat Al <m.alekperov@gmail.com>
2021-11-04 16:11:32 +0100

    Added description for ARTKeychainLocalDeviceStorage

--- Source/ARTKeychainLocalDeviceStorage.h
+++ Source/ARTKeychainLocalDeviceStorage.h
@@ -4,6 +4,10 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ Keychain storage for the device secret. It uses your device's keychain as a storage mechanism.
+ See ``ARTClientOptions/storage`` for further details.
+ */
 @interface ARTKeychainLocalDeviceStorage : ARTUserDefaultsLocalDeviceStorage
 
 - (instancetype)initWithLogger:(ARTLog *)logger;
