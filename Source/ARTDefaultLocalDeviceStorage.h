7ee2aa41a90eb74d3506125c97b029445a691df4
Marat Al <m.alekperov@gmail.com>
2021-11-04 16:05:51 +0100

    Added documentation to ARTDefaultLocalDeviceStorage

--- Source/ARTDefaultLocalDeviceStorage.h
+++ Source/ARTDefaultLocalDeviceStorage.h
@@ -2,6 +2,10 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ Default storage for the device secret. It uses ``NSUserDefaults`` as a storage mechanism. If you want to store your device secret securely, consider to use your own implementation of the ``ARTDeviceStorage`` protocol or use our `ARTKeychainLocalDeviceStorage` implementation.
+ See ``ARTClientOptions/storage`` for further details.
+ */
 @interface ARTDefaultLocalDeviceStorage : NSObject<ARTDeviceStorage>
 @end
 
