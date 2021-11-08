323b2304ef69616a12d7a0400eb5a800560f720f
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-11-08 15:35:13 +0000

    Update Ably Cocoa version to 1.2.7 in source code

--- Spec/ARTDefaultTests.swift
+++ Spec/ARTDefaultTests.swift
@@ -6,6 +6,6 @@ class ARTDefaultTests: XCTestCase {
     
     func testVersions() {
         XCTAssertEqual(ARTDefault.apiVersion(), "1.2")
-        XCTAssertEqual(ARTDefault.libraryVersion(), "1.2.6")
+        XCTAssertEqual(ARTDefault.libraryVersion(), "1.2.7")
     }
 }
