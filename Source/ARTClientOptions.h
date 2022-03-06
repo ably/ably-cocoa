b9b426b69c6e0d5741e235a083284ede6fbaa964
Marat Al <m.alekperov@gmail.com>
2022-03-06 19:31:22 +0100

    Doc comment for `fallbackRetryTimeout` property updated

--- Source/ARTClientOptions.h
+++ Source/ARTClientOptions.h
@@ -68,7 +68,8 @@ NS_ASSUME_NONNULL_BEGIN
 @property (readwrite, assign, nonatomic) NSTimeInterval httpRequestTimeout;
 
 /**
- The period in seconds before HTTP requests are retried against the default endpoint
+ The period in seconds before HTTP requests are retried against the default endpoint.
+ (After a failed request to the default endpoint, followed by a successful request to a fallback endpoint)
  */
 @property (readwrite, assign, nonatomic) NSTimeInterval fallbackRetryTimeout;
 
