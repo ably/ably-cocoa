e66b584c7eeec93811922bc4e1447ca108172508
Lawrence Forooghian <lawrence@forooghian.com>
2022-03-22 16:06:34 -0300

    Increase logging around Presence presence map
    In particular, I want to know every time the _members array of
    ARTPresenceMap is modified, and I want better info about the messages
    involved

--- Source/ARTPresenceMap.m
+++ Source/ARTPresenceMap.m
@@ -24,6 +24,10 @@ NSString *ARTPresenceSyncStateToStr(ARTPresenceSyncState state) {
     }
 }
 
+static NSString *_logMessage(ARTPresenceMessage *message) {
+    return [message description];
+}
+
 #pragma mark - ARTEvent
 
 @interface ARTEvent (PresenceSyncState)
@@ -52,7 +56,7 @@ NSString *ARTPresenceSyncStateToStr(ARTPresenceSyncState state) {
     self = [super init];
     if(self) {
         _logger = logger;
-        [self reset];
+        [self reset:@"ARTPresenceMap init"];
         _syncSessionId = 0;
         _syncState = ARTPresenceSyncInitialized;
         _syncEventEmitter = [[ARTInternalEventEmitter alloc] initWithQueue:queue];
@@ -68,8 +72,9 @@ NSString *ARTPresenceSyncStateToStr(ARTPresenceSyncState state) {
     return _localMembers;
 }
 
-- (BOOL)add:(ARTPresenceMessage *)message {
+- (BOOL)add:(ARTPresenceMessage *)message reason:(NSString *)reason {
     ARTPresenceMessage *latest = [_members objectForKey:message.memberKey];
+    [_logger verbose:__FILE__ line:__LINE__ message:@"1279-logging (reason %@): add: %@. Comparing to latest %@", reason, _logMessage(message), _logMessage(latest)];
     if ([message isNewerThan:latest]) {
         ARTPresenceMessage *messageCopy = [message copy];
         switch (message.action) {
@@ -78,10 +83,10 @@ NSString *ARTPresenceSyncStateToStr(ARTPresenceSyncState state) {
                 messageCopy.action = ARTPresencePresent;
                 // intentional fallthrough
             case ARTPresencePresent:
-                [self internalAdd:messageCopy];
+                [self internalAdd:messageCopy reason:reason];
                 break;
             case ARTPresenceLeave:
-                [self internalRemove:messageCopy];
+                [self internalRemove:messageCopy reason:reason];
                 break;
             default:
                 break;
@@ -93,12 +98,13 @@ NSString *ARTPresenceSyncStateToStr(ARTPresenceSyncState state) {
     return NO;
 }
 
-- (void)internalAdd:(ARTPresenceMessage *)message {
-    [self internalAdd:message withSessionId:_syncSessionId];
+- (void)internalAdd:(ARTPresenceMessage *)message reason:(NSString *)reason {
+    [self internalAdd:message withSessionId:_syncSessionId reason:reason];
 }
 
-- (void)internalAdd:(ARTPresenceMessage *)message withSessionId:(NSUInteger)sessionId {
+- (void)internalAdd:(ARTPresenceMessage *)message withSessionId:(NSUInteger)sessionId reason:(NSString *)reason {
     message.syncSessionId = sessionId;
+    [_logger verbose:__FILE__ line:__LINE__ message:@"1279-logging (reason %@): internalAdd: %@", reason, _logMessage(message)];
     [_members setObject:message forKey:message.memberKey];
     // Local member
     if ([message.connectionId isEqualToString:self.delegate.connectionId]) {
@@ -107,11 +113,11 @@ NSString *ARTPresenceSyncStateToStr(ARTPresenceSyncState state) {
     }
 }
 
-- (void)internalRemove:(ARTPresenceMessage *)message {
-    [self internalRemove:message force:false];
+- (void)internalRemove:(ARTPresenceMessage *)message reason:(NSString *)reason {
+    [self internalRemove:message force:false reason:reason];
 }
 
-- (void)internalRemove:(ARTPresenceMessage *)message force:(BOOL)force {
+- (void)internalRemove:(ARTPresenceMessage *)message force:(BOOL)force reason:(NSString *)reason {
     if ([message.connectionId isEqualToString:self.delegate.connectionId] && !message.isSynthesized) {
         [_localMembers removeObject:message];
     }
@@ -121,47 +127,49 @@ NSString *ARTPresenceSyncStateToStr(ARTPresenceSyncState state) {
         [_logger debug:__FILE__ line:__LINE__ message:@"%p \"%@\" should be removed after sync ends (syncInProgress=%d)", self, message.clientId, syncInProgress];
         message.action = ARTPresenceAbsent;
         // Should be removed after Sync ends
-        [self internalAdd:message withSessionId:message.syncSessionId];
+        [self internalAdd:message withSessionId:message.syncSessionId reason:[NSString stringWithFormat:@"internalRemove (reason %@)", reason]];
     }
     else {
+        [_logger verbose:__FILE__ line:__LINE__ message:@"1279-logging (reason %@): internalRemove: %@ force: %@", reason, _logMessage(message), force ? @"YES" : @"NO"];
         [_members removeObjectForKey:message.memberKey];
     }
 }
 
-- (void)cleanUpAbsentMembers {
+- (void)cleanUpAbsentMembers:(NSString *)reason {
     [_logger debug:__FILE__ line:__LINE__ message:@"%p cleaning up absent members (syncSessionId=%lu)", self, (unsigned long)_syncSessionId];
     NSSet<NSString *> *filteredMembers = [_members keysOfEntriesPassingTest:^BOOL(NSString *key, ARTPresenceMessage *message, BOOL *stop) {
         return message.action == ARTPresenceAbsent;
     }];
     for (NSString *key in filteredMembers) {
-        [self internalRemove:[_members objectForKey:key] force:true];
+        [self internalRemove:[_members objectForKey:key] force:true reason:reason];
     }
 }
 
-- (void)leaveMembersNotPresentInSync {
+- (void)leaveMembersNotPresentInSync:(NSString *)reason {
     [_logger debug:__FILE__ line:__LINE__ message:@"%p leaving members not present in sync (syncSessionId=%lu)", self, (unsigned long)_syncSessionId];
     for (ARTPresenceMessage *member in [_members allValues]) {
         if (member.syncSessionId != _syncSessionId) {
             // Handle members that have not been added or updated in the PresenceMap during the sync process
             ARTPresenceMessage *leave = [member copy];
-            [self internalRemove:member];
+            [self internalRemove:member reason:reason];
             [self.delegate map:self didRemovedMemberNoLongerPresent:leave];
         }
     }
 }
 
-- (void)reenterLocalMembersMissingFromSync {
+- (void)reenterLocalMembersMissingFromSync:(NSString *)reason {
     [_logger debug:__FILE__ line:__LINE__ message:@"%p reentering local members missed from sync (syncSessionId=%lu)", self, (unsigned long)_syncSessionId];
     NSSet *filteredLocalMembers = [_localMembers filteredSetUsingPredicate:[NSPredicate predicateWithFormat:@"syncSessionId != %lu", (unsigned long)_syncSessionId]];
     for (ARTPresenceMessage *localMember in filteredLocalMembers) {
         ARTPresenceMessage *reenter = [localMember copy];
-        [self internalRemove:localMember];
+        [self internalRemove:localMember reason:reason];
         [self.delegate map:self shouldReenterLocalMember:reenter];
     }
-    [self cleanUpAbsentMembers];
+    [self cleanUpAbsentMembers:reason];
 }
 
-- (void)reset {
+- (void)reset:(NSString *)reason {
+    [_logger verbose:__FILE__ line:__LINE__ message:@"1279-logging (reason %@): reset", reason];
     _members = [NSMutableDictionary dictionary];
     _localMembers = [NSMutableSet set];
 }
@@ -173,19 +181,19 @@ NSString *ARTPresenceSyncStateToStr(ARTPresenceSyncState state) {
     [_syncEventEmitter emit:[ARTEvent newWithPresenceSyncState:_syncState] with:nil];
 }
 
-- (void)endSync {
+- (void)endSync:(NSString *)reason {
     [_logger verbose:__FILE__ line:__LINE__ message:@"%p PresenceMap sync ending", self];
-    [self cleanUpAbsentMembers];
-    [self leaveMembersNotPresentInSync];
+    [self cleanUpAbsentMembers:reason];
+    [self leaveMembersNotPresentInSync:reason];
     _syncState = ARTPresenceSyncEnded;
-    [self reenterLocalMembersMissingFromSync];
+    [self reenterLocalMembersMissingFromSync:reason];
     [_syncEventEmitter emit:[ARTEvent newWithPresenceSyncState:ARTPresenceSyncEnded] with:[_members allValues]];
     [_syncEventEmitter off];
     [_logger debug:__FILE__ line:__LINE__ message:@"%p PresenceMap sync ended", self];
 }
 
 - (void)failsSync:(ARTErrorInfo *)error {
-    [self reset];
+    [self reset:@"failsSync"];
     _syncState = ARTPresenceSyncFailed;
     [_syncEventEmitter emit:[ARTEvent newWithPresenceSyncState:ARTPresenceSyncFailed] with:error];
     [_syncEventEmitter off];
