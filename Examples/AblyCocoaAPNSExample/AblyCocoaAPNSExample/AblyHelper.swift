24eb5fb98dfb2c100da360652eef4f3970975ff0
Marat Al <m.alekperov@gmail.com>
2022-03-27 21:03:04 +0200

    Added files for basic APNS example

---
+++ Examples/AblyCocoaAPNSExample/AblyCocoaAPNSExample/AblyHelper.swift
@@ -0,0 +1,77 @@
+import Ably
+import UIKit
+
+class AblyHelper: NSObject {
+    
+    static let shared = AblyHelper()
+    
+    private(set) var realtime: ARTRealtime!
+    
+    private let key = "<YOUR ABLY API KEY>"
+    
+    private override init() {
+        super.init()
+        let options = ARTClientOptions(key: key)
+        options.clientId = "basic-apns-example"
+        self.realtime = ARTRealtime(options: options)
+        UNUserNotificationCenter.current().delegate = self
+    }
+}
+
+extension AblyHelper: UNUserNotificationCenterDelegate {
+    
+    static func requestAPNSAuthorization() {
+        UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound]) { granted, error in
+            DispatchQueue.main.async() {
+                UIApplication.shared.registerForRemoteNotifications()
+                print("Push auth: \(error?.localizedDescription ?? (granted ? "Granted" : "Not Granted"))")
+            }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("Got push notification!")
+        completionHandler([.banner, .sound])
+    }
+}
+
+extension AblyHelper {
+    
+    func printIdentityToken() {
+        guard
+            let tokenData = UserDefaults.standard.value(forKey: "ARTDeviceIdentityToken") as? Data,
+            let tokenInfo = ARTDeviceIdentityTokenDetails.unarchive(tokenData)
+        else {
+            print("INDENTITY TOKEN: doesn't exist")
+            return
+        }
+        print("INDENTITY TOKEN:\n\(tokenInfo.token)")
+    }
+    
+    func getDeviceDetails(_ callback: @escaping (ARTDeviceDetails?, ARTErrorInfo?) -> ()) {
+        realtime.push.admin.deviceRegistrations.get(realtime.device.id, callback: callback)
+    }
+    
+    func sendAdminPush() {
+        let recipient = [
+            "deviceId": realtime.device.id
+        ]
+        let data = [
+            "notification": [
+                "title": "Hello",
+                "body": "This push was sent with deviceId"
+            ],
+            "data": [
+                "foo": "bar",
+                "baz": "qux"
+            ]
+        ]
+        realtime.push.admin.publish(recipient, data: data) { error in
+            print("Result: \(error?.localizedDescription ?? "Success")")
+        }
+    }
+}
