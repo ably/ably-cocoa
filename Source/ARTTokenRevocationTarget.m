53f0af57dff503dc903fbf1084f90eb947a044e8
ikbalkaya <ceikbal@gmail.com>
2022-06-22 20:20:36 +0100

    Implement isEqual and hash for ARTTokenRevocationTarget

--- Source/ARTTokenRevocationTarget.m
+++ Source/ARTTokenRevocationTarget.m
@@ -9,4 +9,22 @@
     return self;
 }
 
+- (BOOL)isEqual:(id)object {
+    if ([object isMemberOfClass:[ARTTokenRevocationTarget class]]) {
+        ARTTokenRevocationTarget *otherTarget = object;
+        return [otherTarget.type isEqualToString:self.type] && [otherTarget.value isEqualToString:self.value];
+    }
+    return NO;
+}
+
+- (NSUInteger)hash {
+    NSUInteger prime = 31;
+    NSUInteger result = 1;
+
+    result = prime * result + [self.type hash];
+    result = prime * result + [self.value hash];
+
+    return result;
+}
+
 @end
