3dccfda44bcdf2af2408a1ad0769f39d2191c477
Marat Al <m.alekperov@gmail.com>
2022-08-12 20:15:14 +0200

    Add canonical docstring for ARTRealtime.clientId This is taken verbatim from the repo ably/canonical-api-reference-prototyping at commit 48e2cd2.

--- Source/ARTRealtime.h
+++ Source/ARTRealtime.h
@@ -44,6 +44,12 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @property (readonly) ARTLocalDevice *device;
 #endif
+
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * A client ID, used for identifying this client when publishing messages or for presence purposes. The `clientId` can be any non-empty string, except it cannot contain a `*`. This option is primarily intended to be used in situations where the library is instantiated with a key. A `clientId` may also be implicit in a token used to instantiate the library; an error will be raised if a `clientId` specified here conflicts with the `clientId` implicit in the token.
+ * END CANONICAL DOCSTRING
+ */
 @property (readonly, nullable) NSString *clientId;
 
 - (instancetype)init NS_UNAVAILABLE;
