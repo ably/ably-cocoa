9112ecd2a23c9a4b922bd0897c8072944d43085a
Marat Al <m.alekperov@gmail.com>
2022-08-13 23:15:54 +0200

    Add canonical docstring for ARTClientOptions#httpMaxRetryDuration property This is taken verbatim from the repo ably/canonical-api-reference-prototyping at commit 711f45c.

--- Source/ARTClientOptions.h
+++ Source/ARTClientOptions.h
@@ -207,7 +207,13 @@ NS_ASSUME_NONNULL_BEGIN
 @property (readwrite, assign, nonatomic) NSUInteger httpMaxRetryCount;
 
 /**
- Max elapsed time in which fallback host retries for HTTP requests will be attempted i.e. if the first default host attempt takes 5s, and then the subsequent fallback retry attempt takes 7s, no further fallback host attempts will be made as the total elapsed time of 12s exceeds the default 10s limit.
+ * BEGIN CANONICAL DOCSTRING
+ * The maximum elapsed time in which fallback host retries for HTTP requests will be attempted. The default is 15 seconds.
+ * END CANONICAL DOCSTRING
+ *
+ * BEGIN LEGACY DOCSTRING
+ * Max elapsed time in which fallback host retries for HTTP requests will be attempted i.e. if the first default host attempt takes 5s, and then the subsequent fallback retry attempt takes 7s, no further fallback host attempts will be made as the total elapsed time of 12s exceeds the default 10s limit.
+ * END LEGACY DOCSTRING
  */
 @property (readwrite, assign, nonatomic) NSTimeInterval httpMaxRetryDuration;
 
