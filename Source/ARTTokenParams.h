37d6c7dd45da376fb0943202e6ef40c139dacc54
Marat Al <m.alekperov@gmail.com>
2022-08-14 16:30:07 +0200

    Add canonical docstring for ARTTokenParams#timestamp property This is taken verbatim from the repo ably/canonical-api-reference-prototyping at commit 1764b33.

--- Source/ARTTokenParams.h
+++ Source/ARTTokenParams.h
@@ -46,7 +46,13 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nullable, nonatomic, copy, readwrite) NSString *clientId;
 
 /**
- Timestamp (in millis since the epoch) of this request. Timestamps, in conjunction with the nonce, are used to prevent n requests from being replayed.
+ * BEGIN CANONICAL DOCSTRING
+ * The timestamp of this request as milliseconds since the Unix epoch. Timestamps, in conjunction with the `nonce`, are used to prevent requests from being replayed. `timestamp` is a "one-time" value, and is valid in a request, but is not validly a member of any default token params such as `ClientOptions.defaultTokenParams`.
+ * END CANONICAL DOCSTRING
+ *
+ * BEGIN LEGACY DOCSTRING
+ * Timestamp (in millis since the epoch) of this request. Timestamps, in conjunction with the nonce, are used to prevent n requests from being replayed.
+ * END LEGACY DOCSTRING
  */
 @property (nullable, nonatomic, copy, readwrite) NSDate *timestamp;
 
