e532499b6665b434474abb40eaa9f273de906455
Marat Al <m.alekperov@gmail.com>
2022-08-16 23:13:43 +0200

    Add canonical docstring for ARTRealtimeChannelState This is taken verbatim from the repo ably/canonical-api-reference-prototyping at commits f357c0e, 95e960e, 35caac7, f6b9b4d.

--- Source/ARTTypes.h
+++ Source/ARTTypes.h
@@ -81,15 +81,56 @@ NSString *_Nonnull ARTRealtimeConnectionEventToStr(ARTRealtimeConnectionEvent ev
 #pragma mark - ARTRealtimeChannelState
 
 /**
- ARTRealtimeChannelState is an enum representing all the Realtime Channel states.
+ * BEGIN CANONICAL DOCSTRING
+ * Describes the possible states of a [`RestChannel`]{@link RestChannel} or [`RealtimeChannel`]{@link RealtimeChannel} object.
+ * END CANONICAL DOCSTRING
+ *
+ * BEGIN LEGACY DOCSTRING
+ * ARTRealtimeChannelState is an enum representing all the Realtime Channel states.
+ * END LEGACY DOCSTRING
  */
 typedef NS_ENUM(NSUInteger, ARTRealtimeChannelState) {
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * The channel has been initialized but no attach has yet been attempted.
+     * END CANONICAL DOCSTRING
+     */
     ARTRealtimeChannelInitialized,
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * An attach has been initiated by sending a request to Ably. This is a transient state, followed either by a transition to `ATTACHED`, `SUSPENDED`, or `FAILED`.
+     * END CANONICAL DOCSTRING
+     */
     ARTRealtimeChannelAttaching,
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * The attach has succeeded. In the `ATTACHED` state a client may publish and subscribe to messages, or be present on the channel.
+     * END CANONICAL DOCSTRING
+     */
     ARTRealtimeChannelAttached,
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * A detach has been initiated on an `ATTACHED` channel by sending a request to Ably. This is a transient state, followed either by a transition to `DETACHED` or `FAILED`.
+     * END CANONICAL DOCSTRING
+     */
     ARTRealtimeChannelDetaching,
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * The channel, having previously been `ATTACHED`, has been detached by the user.
+     * END CANONICAL DOCSTRING
+     */
     ARTRealtimeChannelDetached,
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * The channel, having previously been `ATTACHED`, has lost continuity, usually due to the client being disconnected from Ably for longer than two minutes. It will automatically attempt to reattach as soon as connectivity is restored.
+     * END CANONICAL DOCSTRING
+     */
     ARTRealtimeChannelSuspended,
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * An indefinite failure condition. This state is entered if a channel error has been received from the Ably service, such as an attempt to attach without the necessary access rights.
+     * END CANONICAL DOCSTRING
+     */
     ARTRealtimeChannelFailed
 };
 
