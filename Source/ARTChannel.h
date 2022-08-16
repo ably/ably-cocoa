de6b1e8e7c82e3f0caab0a33468354dc96a8a461
Marat Al <m.alekperov@gmail.com>
2022-08-16 15:38:23 +0200

    Add canonical docstring for ARTChannelProtocol#publish This is taken verbatim from the repo ably/canonical-api-reference-prototyping at commits 07e45dd, 593d9fe.
    This is from `RealtimeChannel`, but since ably-cocoa has all `publish` methods declared in the base protocol, I put it here. Will be resolved later during comments editing.

--- Source/ARTChannel.h
+++ Source/ARTChannel.h
@@ -23,6 +23,14 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @property (readonly) NSString *name;
 
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Sends a single message on this channel based on a given event name and payload. A callback may optionally be passed in to this call to be notified of success or failure of the operation. When publish is called with this client library, it won't attempt to implicitly attach to the channel, so long as [transient publishing](https://ably.com/docs/realtime/channels#transient-publish) is available in the library. Otherwise, the client will implicitly attach.
+ *
+ * @param name The name of the message.
+ * @param data The payload of the message.
+ * END CANONICAL DOCSTRING
+ */
 - (void)publish:(nullable NSString *)name data:(nullable id)data;
 
 /**
