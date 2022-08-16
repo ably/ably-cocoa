80d11a24143a156004a90ed033a51c490d2e2337
Marat Al <m.alekperov@gmail.com>
2022-08-17 00:21:29 +0200

    Add canonical docstring for ARTChannelMode This is taken verbatim from the repo ably/canonical-api-reference-prototyping at commits 6a1d25a, 35caac7б bf450a9.

--- Source/ARTRealtimeChannelOptions.h
+++ Source/ARTRealtimeChannelOptions.h
@@ -3,12 +3,38 @@
 #import <Ably/ARTChannelOptions.h>
 
 /**
- ARTChannelMode bitmask matching the ARTProtocolMessageFlag.
+ * BEGIN CANONICAL DOCSTRING
+ * Describes the possible flags used to configure client capabilities, using [`ChannelOptions`]{@link ChannelOptions}.
+ * END CANONICAL DOCSTRING
+ *
+ * BEGIN LEGACY DOCSTRING
+ * ARTChannelMode bitmask matching the ARTProtocolMessageFlag.
+ * END LEGACY DOCSTRING
  */
 typedef NS_OPTIONS(NSUInteger, ARTChannelMode) {
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * The client can enter the presence set.
+     * END CANONICAL DOCSTRING
+     */
     ARTChannelModePresence = 1 << 16,
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * The client can publish messages.
+     * END CANONICAL DOCSTRING
+     */
     ARTChannelModePublish = 1 << 17,
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * The client can subscribe to messages.
+     * END CANONICAL DOCSTRING
+     */
     ARTChannelModeSubscribe = 1 << 18,
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * The client can receive presence messages.
+     * END CANONICAL DOCSTRING
+     */
     ARTChannelModePresenceSubscribe = 1 << 19
 };
 
