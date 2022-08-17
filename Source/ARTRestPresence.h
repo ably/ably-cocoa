813e5705414bdf0017c1e446d08694aa6602fc1c
Marat Al <m.alekperov@gmail.com>
2022-08-17 22:57:38 +0200

    Add canonical docstring for ARTRestPresence#history This is taken verbatim from the repo ably/canonical-api-reference-prototyping at commits 5ead6ad, 1764b33, 711f45c, 887e130.

--- Source/ARTRestPresence.h
+++ Source/ARTRestPresence.h
@@ -37,6 +37,18 @@ NS_ASSUME_NONNULL_BEGIN
  */
 - (BOOL)get:(ARTPresenceQuery *)query callback:(ARTPaginatedPresenceCallback)callback error:(NSError *_Nullable *_Nullable)errorPtr;
 
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Retrieves a [`PaginatedResult`]{@link PaginatedResult} object, containing an array of historical [`PresenceMessage`]{@link PresenceMessage} objects for the channel. If the channel is configured to persist messages, then presence messages can be retrieved from history for up to 72 hours in the past. If not, presence messages can only be retrieved from history for up to two minutes in the past.
+ *
+ * @param start The time from which messages are retrieved, specified as milliseconds since the Unix epoch.
+ * @param end The time until messages are retrieved, specified as milliseconds since the Unix epoch.
+ * @param direction The order for which messages are returned in. Valid values are `backwards` which orders messages from most recent to oldest, or `forwards` which orders messages from oldest to most recent. The default is `backwards`.
+ * @param limit An upper limit on the number of messages returned. The default is 100, and the maximum is 1000.
+ *
+ * @return A [`PaginatedResult`]{@link PaginatedResult} object containing an array of [`PresenceMessage`]{@link PresenceMessage} objects.
+ * END CANONICAL DOCSTRING
+ */
 - (BOOL)history:(nullable ARTDataQuery *)query callback:(ARTPaginatedPresenceCallback)callback error:(NSError *_Nullable *_Nullable)errorPtr;
 
 @end
