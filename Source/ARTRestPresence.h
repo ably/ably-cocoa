e7bd9d94d61c51c9a00cbb1daf63ea97835acc24
Marat Al <m.alekperov@gmail.com>
2022-08-17 22:52:23 +0200

    Add canonical docstring for ARTRestPresence#get This is taken verbatim from the repo ably/canonical-api-reference-prototyping at commits 887e130, 711f45c, b7c1426, 887e130.

--- Source/ARTRestPresence.h
+++ Source/ARTRestPresence.h
@@ -23,6 +23,18 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)get:(ARTPaginatedPresenceCallback)callback;
 - (BOOL)get:(ARTPaginatedPresenceCallback)callback error:(NSError *_Nullable *_Nullable)errorPtr;
+
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Retrieves the current members present on the channel and the metadata for each member, such as their [`PresenceAction`]{@link PresenceAction} and ID. Returns a [`PaginatedResult`]{@link PaginatedResult} object, containing an array of [`PresenceMessage`]{@link PresenceMessage} objects.
+ *
+ * @param limit An upper limit on the number of messages returned. The default is 100, and the maximum is 1000.
+ * @param clientId Filters the list of returned presence members by a specific client using its ID.
+ * @param connectionId Filters the list of returned presence members by a specific connection using its ID.
+ *
+ * @return A [`PaginatedResult`]{@link PaginatedResult} object containing an array of [`PresenceMessage`]{@link PresenceMessage} objects.
+ * END CANONICAL DOCSTRING
+ */
 - (BOOL)get:(ARTPresenceQuery *)query callback:(ARTPaginatedPresenceCallback)callback error:(NSError *_Nullable *_Nullable)errorPtr;
 
 - (BOOL)history:(nullable ARTDataQuery *)query callback:(ARTPaginatedPresenceCallback)callback error:(NSError *_Nullable *_Nullable)errorPtr;
