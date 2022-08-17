db5d20d3df18b792661a11ba9076f499ac088fd3
Marat Al <m.alekperov@gmail.com>
2022-08-18 00:37:52 +0200

    Add canonical docstring for ARTRealtimePresenceProtocol#leaveClient This is taken verbatim from the repo ably/canonical-api-reference-prototyping at commits a6f271c, b7c1426, 6523c2b.

--- Source/ARTRealtimePresence.h
+++ Source/ARTRealtimePresence.h
@@ -94,6 +94,15 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)updateClient:(NSString *)clientId data:(id _Nullable)data;
 - (void)updateClient:(NSString *)clientId data:(id _Nullable)data callback:(nullable ARTCallback)cb;
 
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Leaves the presence set of the channel for a given `clientId`. Enables a single client to update presence on behalf of any number of clients using a single connection. The library must have been instantiated with an API key or a token bound to a wildcard `clientId`. An optional callback may be provided to notify of the success or failure of the operation.
+ *
+ * @param clientId The ID of the client to leave the presence set for.
+ * @param data The payload associated with the presence member.
+ * @param extras A JSON object of arbitrary key-value pairs that may contain metadata, and/or ancillary payloads.
+ * END CANONICAL DOCSTRING
+ */
 - (void)leaveClient:(NSString *)clientId data:(id _Nullable)data;
 - (void)leaveClient:(NSString *)clientId data:(id _Nullable)data callback:(nullable ARTCallback)cb;
 
