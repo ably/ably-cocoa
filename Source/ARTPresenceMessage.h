250b12e73f8fc61fcb02ee1036b82794e7c07b3e
Marat Al <m.alekperov@gmail.com>
2022-08-18 14:49:05 +0200

    Add canonical docstring for ARTPresenceAction This is taken verbatim from the repo ably/canonical-api-reference-prototyping at commits 67d28e0, 35caac7.

--- Source/ARTPresenceMessage.h
+++ Source/ARTPresenceMessage.h
@@ -2,13 +2,44 @@
 #import <Ably/ARTEventEmitter.h>
 
 /**
- ARTPresenceAction represents all actions an ``ARTPresenceMessage`` can indicate.
+ * BEGIN CANONICAL DOCSTRING
+ * Describes the possible actions members in the presence set can emit.
+ * END CANONICAL DOCSTRING
+ *
+ * BEGIN LEGACY DOCSTRING
+ * ARTPresenceAction represents all actions an ``ARTPresenceMessage`` can indicate.
+ * END LEGACY DOCSTRING
  */
 typedef NS_ENUM(NSUInteger, ARTPresenceAction) {
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * A member is not present in the channel.
+     * END CANONICAL DOCSTRING
+     */
     ARTPresenceAbsent,
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * When subscribing to presence events on a channel that already has members present, this event is emitted for every member already present on the channel before the subscribe listener was registered.
+     * END CANONICAL DOCSTRING
+     */
     ARTPresencePresent,
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * A new member has entered the channel.
+     * END CANONICAL DOCSTRING
+     */
     ARTPresenceEnter,
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * A member who was present has now left the channel. This may be a result of an explicit request to leave or implicitly when detaching from the channel. Alternatively, if a member's connection is abruptly disconnected and they do not resume their connection within a minute, Ably treats this as a leave event as the client is no longer present.
+     * END CANONICAL DOCSTRING
+     */
     ARTPresenceLeave,
+    /**
+     * BEGIN CANONICAL DOCSTRING
+     * An already present member has updated their member data. Being notified of member data updates can be very useful, for example, it can be used to update the status of a user when they are typing a message.
+     * END CANONICAL DOCSTRING
+     */
     ARTPresenceUpdate
 };
 
