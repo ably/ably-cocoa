4cc394dc3aeb5e2cd8f5f0738baef8cd9f4f331d
Marat Al <m.alekperov@gmail.com>
2022-08-20 21:28:47 +0200

    Add canonical docstring for ARTPush#admin This is taken verbatim from the repo ably/canonical-api-reference-prototyping at commit 5fe4847.

--- Source/ARTPush.h
+++ Source/ARTPush.h
@@ -86,6 +86,11 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @interface ARTPush : NSObject <ARTPushProtocol>
 
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * A [`PushAdmin`]{@link PushAdmin} object.
+ * END CANONICAL DOCSTRING
+ */
 @property (readonly) ARTPushAdmin *admin;
 
 @end
