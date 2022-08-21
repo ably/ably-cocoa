3771028cbcec6674a0b0722e62595fc1eda50d7b
Marat Al <m.alekperov@gmail.com>
2022-08-21 23:38:21 +0200

    Add canonical docstring for ARTVCDiffDecoder This is taken verbatim from the repo ably/canonical-api-reference-prototyping at commit f357c0e.

--- Source/ARTVCDiffDecoder.h
+++ Source/ARTVCDiffDecoder.h
@@ -2,6 +2,11 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Enables `vcdiff` encoded messages to be decoded.
+ * END CANONICAL DOCSTRING
+ */
 @protocol ARTVCDiffDecoder
 - (nullable NSData *)decode:(NSData *)delta base:(NSData *)base error:(NSError *__autoreleasing _Nullable * _Nullable)error;
 @end
