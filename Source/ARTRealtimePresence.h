e8894ddfeddad1c74353a3f6a45decdcefcc5ef3
Marat Al <m.alekperov@gmail.com>
2022-08-26 17:29:14 +0200

    Edit canonical docstrings for ARTRealtimePresence

--- Source/ARTRealtimePresence.h
+++ Source/ARTRealtimePresence.h
@@ -9,6 +9,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ARTRealtimePresenceQuery : ARTPresenceQuery
 
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Sets whether to wait for a full presence set synchronization between Ably and the clients on the channel to complete before returning the results. Synchronization begins as soon as the channel is `ARTRealtimeChannelAttached`. When set to `true` the results will be returned as soon as the sync is complete. When set to `false` the current list of members will be returned without the sync completing. The default is `true`.
+ * END CANONICAL DOCSTRING
+ */
 @property (readwrite, nonatomic) BOOL waitForSync;
 
 @end
@@ -31,47 +36,81 @@ NS_ASSUME_NONNULL_BEGIN
  * BEGIN CANONICAL DOCSTRING
  * Retrieves the current members present on the channel and the metadata for each member, such as their `ARTPresenceAction` and ID. Returns an array of `ARTPresenceMessage` objects.
  *
- * @param waitForSync Sets whether to wait for a full presence set synchronization between Ably and the clients on the channel to complete before returning the results. Synchronization begins as soon as the channel is `ARTChannelState.ATTACHED`. When set to `true` the results will be returned as soon as the sync is complete. When set to `false` the current list of members will be returned without the sync completing. The default is `true`.
- * @param clientId Filters the array of returned presence members by a specific client using its ID.
- * @param connectionId Filters the array of returned presence members by a specific connection using its ID.
+ * @param query An `ARTRealtimePresenceQuery` object.
+ * @param callback A callback for retriving an `ARTPaginatedResult` object with an array of `ARTPresenceMessage` objects.
+ * @param errorPtr A reference to the `NSError` object where an error information will be saved in case of failure.
  *
- * @return An array of `ARTPresenceMessage` objects.
+ * @return In case of failure returns false and the error information can be retrived via the `error` parameter.
  * END CANONICAL DOCSTRING
  */
 - (void)get:(ARTRealtimePresenceQuery *)query callback:(ARTPresenceMessagesCallback)callback;
 
 /**
  * BEGIN CANONICAL DOCSTRING
- * Enters the presence set for the channel, optionally passing a `data` payload. A `clientId` is required to be present on a channel. An optional callback may be provided to notify of the success or failure of the operation.
+ * Enters the presence set for the channel, optionally passing a `data` payload. A `clientId` is required to be present on a channel.
  *
  * @param data The payload associated with the presence member.
- * @param extras A JSON object of arbitrary key-value pairs that may contain metadata, and/or ancillary payloads.
  * END CANONICAL DOCSTRING
  */
 - (void)enter:(id _Nullable)data;
-- (void)enter:(id _Nullable)data callback:(nullable ARTCallback)cb;
 
 /**
  * BEGIN CANONICAL DOCSTRING
- * Updates the `data` payload for a presence member. If called before entering the presence set, this is treated as an `ARTPresenceAction.ENTER` event. An optional callback may be provided to notify of the success or failure of the operation.
+ * Enters the presence set for the channel, optionally passing a `data` payload. A `clientId` is required to be present on a channel. An optional callback may be provided to notify of the success or failure of the operation.
+ *
+ * @param data The payload associated with the presence member.
+ * @param callback A success or failure callback function.
+ * END CANONICAL DOCSTRING
+ */
+- (void)enter:(id _Nullable)data callback:(nullable ARTCallback)callback;
+
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Updates the `data` payload for a presence member. If called before entering the presence set, this is treated as an `ARTPresenceEnter` event.
  *
  * @param data The payload to update for the presence member.
- * @param extras A JSON object of arbitrary key-value pairs that may contain metadata, and/or ancillary payloads.
  * END CANONICAL DOCSTRING
  */
 - (void)update:(id _Nullable)data;
-- (void)update:(id _Nullable)data callback:(nullable ARTCallback)cb;
+
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Updates the `data` payload for a presence member. If called before entering the presence set, this is treated as an `ARTPresenceEnter` event. An optional callback may be provided to notify of the success or failure of the operation.
+ *
+ * @param data The payload to update for the presence member.
+ * @param callback A success or failure callback function.
+ * END CANONICAL DOCSTRING
+ */
+- (void)update:(id _Nullable)data callback:(nullable ARTCallback)callback;
+
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Leaves the presence set for the channel. A client must have previously entered the presence set before they can leave it.
+ *
+ * @param data The payload associated with the presence member.
+ * END CANONICAL DOCSTRING
+ */
+- (void)leave:(id _Nullable)data;
 
 /**
  * BEGIN CANONICAL DOCSTRING
  * Leaves the presence set for the channel. A client must have previously entered the presence set before they can leave it. An optional callback may be provided to notify of the success or failure of the operation.
  *
  * @param data The payload associated with the presence member.
- * @param extras A JSON object of arbitrary key-value pairs that may contain metadata, and/or ancillary payloads.
+ * @param callback A success or failure callback function.
  * END CANONICAL DOCSTRING
  */
-- (void)leave:(id _Nullable)data;
-- (void)leave:(id _Nullable)data callback:(nullable ARTCallback)cb;
+- (void)leave:(id _Nullable)data callback:(nullable ARTCallback)callback;
+
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Enters the presence set of the channel for a given `clientId`. Enables a single client to update presence on behalf of any number of clients using a single connection. The library must have been instantiated with an API key or a token bound to a wildcard `clientId`.
+ *
+ * @param clientId The ID of the client to enter into the presence set.
+ * @param data The payload associated with the presence member.
+ * END CANONICAL DOCSTRING
+ */
+- (void)enterClient:(NSString *)clientId data:(id _Nullable)data;
 
 /**
  * BEGIN CANONICAL DOCSTRING
@@ -79,11 +118,20 @@ NS_ASSUME_NONNULL_BEGIN
  *
  * @param clientId The ID of the client to enter into the presence set.
  * @param data The payload associated with the presence member.
- * @param extras A JSON object of arbitrary key-value pairs that may contain metadata, and/or ancillary payloads.
+ * @param callback A success or failure callback function.
  * END CANONICAL DOCSTRING
  */
-- (void)enterClient:(NSString *)clientId data:(id _Nullable)data;
-- (void)enterClient:(NSString *)clientId data:(id _Nullable)data callback:(nullable ARTCallback)cb;
+- (void)enterClient:(NSString *)clientId data:(id _Nullable)data callback:(nullable ARTCallback)callback;
+
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Updates the `data` payload for a presence member using a given `clientId`. Enables a single client to update presence on behalf of any number of clients using a single connection. The library must have been instantiated with an API key or a token bound to a wildcard `clientId`.
+ *
+ * @param clientId The ID of the client to update in the presence set.
+ * @param data The payload to update for the presence member.
+ * END CANONICAL DOCSTRING
+ */
+- (void)updateClient:(NSString *)clientId data:(id _Nullable)data;
 
 /**
  * BEGIN CANONICAL DOCSTRING
@@ -91,11 +139,10 @@ NS_ASSUME_NONNULL_BEGIN
  *
  * @param clientId The ID of the client to update in the presence set.
  * @param data The payload to update for the presence member.
- * @param extras A JSON object of arbitrary key-value pairs that may contain metadata, and/or ancillary payloads.
+ * @param callback A success or failure callback function.
  * END CANONICAL DOCSTRING
  */
-- (void)updateClient:(NSString *)clientId data:(id _Nullable)data;
-- (void)updateClient:(NSString *)clientId data:(id _Nullable)data callback:(nullable ARTCallback)cb;
+- (void)updateClient:(NSString *)clientId data:(id _Nullable)data callback:(nullable ARTCallback)callback;
 
 /**
  * BEGIN CANONICAL DOCSTRING
@@ -103,33 +150,68 @@ NS_ASSUME_NONNULL_BEGIN
  *
  * @param clientId The ID of the client to leave the presence set for.
  * @param data The payload associated with the presence member.
- * @param extras A JSON object of arbitrary key-value pairs that may contain metadata, and/or ancillary payloads.
  * END CANONICAL DOCSTRING
  */
 - (void)leaveClient:(NSString *)clientId data:(id _Nullable)data;
-- (void)leaveClient:(NSString *)clientId data:(id _Nullable)data callback:(nullable ARTCallback)cb;
+
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Leaves the presence set of the channel for a given `clientId`. Enables a single client to update presence on behalf of any number of clients using a single connection. The library must have been instantiated with an API key or a token bound to a wildcard `clientId`. An optional callback may be provided to notify of the success or failure of the operation.
+ *
+ * @param clientId The ID of the client to leave the presence set for.
+ * @param data The payload associated with the presence member.
+ * @param callback A success or failure callback function.
+ * END CANONICAL DOCSTRING
+ */
+- (void)leaveClient:(NSString *)clientId data:(id _Nullable)data callback:(nullable ARTCallback)callback;
+
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Registers a listener that is called each time a `ARTPresenceMessage` is received on the channel, such as a new member entering the presence set.
+ *
+ * @param callback An event listener function.
+ *
+ * @return An event listener object.
+ * END CANONICAL DOCSTRING
+ */
+- (ARTEventListener *_Nullable)subscribe:(ARTPresenceMessageCallback)callback;
 
 /**
  * BEGIN CANONICAL DOCSTRING
  * Registers a listener that is called each time a `ARTPresenceMessage` is received on the channel, such as a new member entering the presence set. A callback may optionally be passed in to this call to be notified of success or failure of the channel `-[ARTRealtimeChannel attach]` operation.
  *
- * @return An event listener function.
+ * @param onAttach An attach callback function.
+ * @param callback An event listener function.
+ *
+ * @return An event listener object.
  * END CANONICAL DOCSTRING
  */
-- (ARTEventListener *_Nullable)subscribe:(ARTPresenceMessageCallback)callback;
 - (ARTEventListener *_Nullable)subscribeWithAttachCallback:(nullable ARTCallback)onAttach callback:(ARTPresenceMessageCallback)cb;
 
 /**
  * BEGIN CANONICAL DOCSTRING
- * Registers a listener that is called each time a `ARTPresenceMessage` matching a given `ARTPresenceAction`, or an action within an array of `ARTPresenceAction`s, is received on the channel, such as a new member entering the presence set. A callback may optionally be passed in to this call to be notified of success or failure of the channel `-[ARTRealtimeChannel attach]` operation.
+ * Registers a listener that is called each time a `ARTPresenceMessage` matching a given `ARTPresenceAction` is received on the channel, such as a new member entering the presence set.
  *
- * @param action A `ARTPresenceAction` or an array of `ARTPresenceAction`s to register the listener for.
+ * @param action A `ARTPresenceAction` to register the listener for.
+ * @param callback An event listener function.
  *
- * @return An event listener function.
+ * @return An event listener object.
  * END CANONICAL DOCSTRING
  */
-- (ARTEventListener *_Nullable)subscribe:(ARTPresenceAction)action callback:(ARTPresenceMessageCallback)cb;
-- (ARTEventListener *_Nullable)subscribe:(ARTPresenceAction)action onAttach:(nullable ARTCallback)onAttach callback:(ARTPresenceMessageCallback)cb;
+- (ARTEventListener *_Nullable)subscribe:(ARTPresenceAction)action callback:(ARTPresenceMessageCallback)callback;
+
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Registers a listener that is called each time a `ARTPresenceMessage` matching a given `ARTPresenceAction` is received on the channel, such as a new member entering the presence set. A callback may optionally be passed in to this call to be notified of success or failure of the channel `-[ARTRealtimeChannel attach]` operation.
+ *
+ * @param action A `ARTPresenceAction` to register the listener for.
+ * @param onAttach An attach callback function.
+ * @param callback An event listener function.
+ *
+ * @return An event listener object.
+ * END CANONICAL DOCSTRING
+ */
+- (ARTEventListener *_Nullable)subscribe:(ARTPresenceAction)action onAttach:(nullable ARTCallback)onAttach callback:(ARTPresenceMessageCallback)callback;
 
 /**
  * BEGIN CANONICAL DOCSTRING
@@ -141,6 +223,8 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  * BEGIN CANONICAL DOCSTRING
  * Deregisters a specific listener that is registered to receive `ARTPresenceMessage` on the channel.
+ *
+ * @param An event listener to unsubscribe.
  * END CANONICAL DOCSTRING
  */
 - (void)unsubscribe:(ARTEventListener *)listener;
@@ -150,6 +234,7 @@ NS_ASSUME_NONNULL_BEGIN
  * Deregisters a specific listener that is registered to receive `ARTPresenceMessage` on the channel for a given `ARTPresenceAction`.
  *
  * @param action A specific `ARTPresenceAction` to deregister the listener for.
+ * @param An event listener to unsubscribe.
  * END CANONICAL DOCSTRING
  */
 - (void)unsubscribe:(ARTPresenceAction)action listener:(ARTEventListener *)listener;
@@ -160,12 +245,11 @@ NS_ASSUME_NONNULL_BEGIN
  * BEGIN CANONICAL DOCSTRING
  * Retrieves a `ARTPaginatedResult` object, containing an array of historical `ARTPresenceMessage` objects for the channel. If the channel is configured to persist messages, then presence messages can be retrieved from history for up to 72 hours in the past. If not, presence messages can only be retrieved from history for up to two minutes in the past.
  *
- * @param start The time from which messages are retrieved, specified as milliseconds since the Unix epoch.
- * @param end The time until messages are retrieved, specified as milliseconds since the Unix epoch.
- * @param direction The order for which messages are returned in. Valid values are `backwards` which orders messages from most recent to oldest, or `forwards` which orders messages from oldest to most recent. The default is `backwards`.
- * @param limit An upper limit on the number of messages returned. The default is 100, and the maximum is 1000.
+ * @param query An `ARTRealtimeHistoryQuery` object.
+ * @param callback A callback for retriving an `ARTPaginatedResult` object with an array of `ARTPresenceMessage` objects.
+ * @param errorPtr A reference to the `NSError` object where an error information will be saved in case of failure.
  *
- * @return A `ARTPaginatedResult` object containing an array of `ARTPresenceMessage` objects.
+ * @return In case of failure returns false and the error information can be retrived via the `error` parameter.
  * END CANONICAL DOCSTRING
  */
 - (BOOL)history:(ARTRealtimeHistoryQuery *_Nullable)query callback:(ARTPaginatedPresenceCallback)callback error:(NSError *_Nullable *_Nullable)errorPtr;
