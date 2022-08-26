28bb8a15309b28360a35d52a88c1bfffc7abe48d
Marat Al <m.alekperov@gmail.com>
2022-08-26 16:02:37 +0200

    Mark canonical docstrings for ARTRestChannel as processed

--- Source/ARTRestChannel.h
+++ Source/ARTRestChannel.h
@@ -14,7 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (readonly, nullable) ARTChannelOptions *options;
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * Retrieves a `ARTPaginatedResult` object, containing an array of historical `ARTMessage` objects for the channel. If the channel is configured to persist messages, then messages can be retrieved from history for up to 72 hours in the past. If not, messages can only be retrieved from history for up to two minutes in the past.
  *
  * @param query An `ARTDataQuery` object.
@@ -22,48 +22,48 @@ NS_ASSUME_NONNULL_BEGIN
  * @param errorPtr A reference to the `NSError` object where an error information will be saved in case of failure.
  *
  * @return In case of failure returns false and the error information can be retrived via the `error` parameter.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 - (BOOL)history:(nullable ARTDataQuery *)query callback:(ARTPaginatedMessagesCallback)callback error:(NSError *_Nullable *_Nullable)errorPtr;
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * Retrieves a `ARTChannelDetails` object for the channel, which includes status and occupancy metrics.
  *
  * @param callback A callback for receiving the `ARTChannelDetails` object.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 - (void)status:(ARTChannelDetailsCallback)callback;
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * Sets the `ARTChannelOptions` for the channel.
  *
  * @param options A `ARTChannelOptions` object.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 - (void)setOptions:(ARTChannelOptions *_Nullable)options;
 
 @end
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * Enables messages to be published and historic messages to be retrieved for a channel.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 @interface ARTRestChannel : NSObject <ARTRestChannelProtocol>
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * A `ARTRestPresence` object.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 @property (readonly) ARTRestPresence *presence;
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * A `ARTPushChannel` object.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 @property (readonly) ARTPushChannel *push;
 
