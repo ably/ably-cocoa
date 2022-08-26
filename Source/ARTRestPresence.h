879c685f686b171aa40d912c63c4d5eaf50c230a
Marat Al <m.alekperov@gmail.com>
2022-08-26 16:32:23 +0200

    Edit canonical docstrings for ARTRestPresence

--- Source/ARTRestPresence.h
+++ Source/ARTRestPresence.h
@@ -9,8 +9,25 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ARTPresenceQuery : NSObject
 
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * An upper limit on the number of messages returned. The default is 100, and the maximum is 1000.
+ * END CANONICAL DOCSTRING
+ */
 @property (nonatomic, readwrite) NSUInteger limit;
+
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Filters the array of returned presence members by a specific client using its ID.
+ * END CANONICAL DOCSTRING
+ */
 @property (nullable, nonatomic, strong, readwrite) NSString *clientId;
+
+/**
+ * BEGIN CANONICAL DOCSTRING
+ * Filters the array of returned presence members by a specific connection using its ID.
+ * END CANONICAL DOCSTRING
+ */
 @property (nullable, nonatomic, strong, readwrite) NSString *connectionId;
 
 - (instancetype)init;
@@ -28,11 +45,11 @@ NS_ASSUME_NONNULL_BEGIN
  * BEGIN CANONICAL DOCSTRING
  * Retrieves the current members present on the channel and the metadata for each member, such as their `ARTPresenceAction` and ID. Returns a `ARTPaginatedResult` object, containing an array of `ARTPresenceMessage` objects.
  *
- * @param limit An upper limit on the number of messages returned. The default is 100, and the maximum is 1000.
- * @param clientId Filters the list of returned presence members by a specific client using its ID.
- * @param connectionId Filters the list of returned presence members by a specific connection using its ID.
+ * @param query An `ARTPresenceQuery` object.
+ * @param callback A callback for retriving an `ARTPaginatedResult` object with an array of `ARTPresenceMessage` objects.
+ * @param errorPtr A reference to the `NSError` object where an error information will be saved in case of failure.
  *
- * @return A `ARTPaginatedResult` object containing an array of `ARTPresenceMessage` objects.
+ * @return In case of failure returns false and the error information can be retrived via the `error` parameter.
  * END CANONICAL DOCSTRING
  */
 - (BOOL)get:(ARTPresenceQuery *)query callback:(ARTPaginatedPresenceCallback)callback error:(NSError *_Nullable *_Nullable)errorPtr;
@@ -41,12 +58,11 @@ NS_ASSUME_NONNULL_BEGIN
  * BEGIN CANONICAL DOCSTRING
  * Retrieves a `ARTPaginatedResult` object, containing an array of historical `ARTPresenceMessage` objects for the channel. If the channel is configured to persist messages, then presence messages can be retrieved from history for up to 72 hours in the past. If not, presence messages can only be retrieved from history for up to two minutes in the past.
  *
- * @param start The time from which messages are retrieved, specified as milliseconds since the Unix epoch.
- * @param end The time until messages are retrieved, specified as milliseconds since the Unix epoch.
- * @param direction The order for which messages are returned in. Valid values are `backwards` which orders messages from most recent to oldest, or `forwards` which orders messages from oldest to most recent. The default is `backwards`.
- * @param limit An upper limit on the number of messages returned. The default is 100, and the maximum is 1000.
+ * @param query An `ARTDataQuery` object.
+ * @param callback A callback for retriving an `ARTPaginatedResult` object with an array of `ARTPresenceMessage` objects.
+ * @param errorPtr A reference to the `NSError` object where an error information will be saved in case of failure.
  *
- * @return A `ARTPaginatedResult` object containing an array of `ARTPresenceMessage` objects.
+ * @return In case of failure returns false and the error information can be retrived via the `error` parameter.
  * END CANONICAL DOCSTRING
  */
 - (BOOL)history:(nullable ARTDataQuery *)query callback:(ARTPaginatedPresenceCallback)callback error:(NSError *_Nullable *_Nullable)errorPtr;
