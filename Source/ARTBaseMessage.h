ca517891203f9d54d1618d20755a048f13139d47
Marat Al <m.alekperov@gmail.com>
2022-08-30 00:13:08 +0200

    Mark canonical docstrings for ARTBaseMessage as processed

--- Source/ARTBaseMessage.h
+++ Source/ARTBaseMessage.h
@@ -9,51 +9,51 @@ NS_ASSUME_NONNULL_BEGIN
 @interface ARTBaseMessage : NSObject<NSCopying>
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * A Unique ID assigned by Ably to this message.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 @property (nullable, strong, nonatomic) NSString *id;
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * Timestamp of when the message was received by Ably, as a `NSDate` object.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 @property (strong, nonatomic, nullable) NSDate *timestamp;
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * The client ID of the publisher of this message.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 @property (strong, nonatomic, nullable) NSString *clientId;
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * The connection ID of the publisher of this message.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 @property (strong, nonatomic) NSString *connectionId;
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * This is typically empty, as all messages received from Ably are automatically decoded client-side using this value. However, if the message encoding cannot be processed, this attribute contains the remaining transformations not applied to the `data` payload.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 @property (strong, nonatomic, nullable) NSString *encoding;
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * The message payload, if provided.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 @property (strong, nonatomic, nullable) id data;
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * A JSON object of arbitrary key-value pairs that may contain metadata, and/or ancillary payloads.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 @property (strong, nullable, nonatomic) id<ARTJsonCompatible> extras;
 
