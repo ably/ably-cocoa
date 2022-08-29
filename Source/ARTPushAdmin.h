8334a09033138f21470a584a501733d07ed9d70a
Marat Al <m.alekperov@gmail.com>
2022-08-29 17:43:18 +0200

    Mark canonical docstrings for ARTPushAdmin as processed

--- Source/ARTPushAdmin.h
+++ Source/ARTPushAdmin.h
@@ -10,36 +10,36 @@ NS_ASSUME_NONNULL_BEGIN
 - (instancetype)init NS_UNAVAILABLE;
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * Sends a push notification directly to a device, or a group of devices sharing the same `clientId`.
  *
  * @param recipient A JSON object containing the recipient details using `clientId`, `deviceId` or the underlying notifications service.
  * @param data A JSON object containing the push notification payload.
  * @param callback A success or failure callback function.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 - (void)publish:(ARTPushRecipient *)recipient data:(ARTJsonObject *)data callback:(nullable ARTCallback)callback;
 
 @end
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * Enables the management of device registrations and push notification subscriptions. Also enables the publishing of push notifications to devices.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 @interface ARTPushAdmin : NSObject <ARTPushAdminProtocol>
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * An `ARTPushDeviceRegistrations` object.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 @property (nonatomic, readonly) ARTPushDeviceRegistrations *deviceRegistrations;
 
 /**
- * BEGIN CANONICAL DOCSTRING
+ * BEGIN CANONICAL PROCESSED DOCSTRING
  * An `ARTPushChannelSubscriptions` object.
- * END CANONICAL DOCSTRING
+ * END CANONICAL PROCESSED DOCSTRING
  */
 @property (nonatomic, readonly) ARTPushChannelSubscriptions *channelSubscriptions;
 
