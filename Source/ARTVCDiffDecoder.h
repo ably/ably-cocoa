50cc227261353a1f7447f83eb501431a0c528eaf
Marat Al <m.alekperov@gmail.com>
2022-08-30 13:52:50 +0200

    Edit canonical docstrings for internal ARTVCDiffDecoder

--- Source/ARTVCDiffDecoder.h
+++ Source/ARTVCDiffDecoder.h
@@ -15,11 +15,12 @@ NS_ASSUME_NONNULL_BEGIN
  *
  * @param delta The delta encoded data.
  * @param base The stored base payload of the last message on a channel.
- * 
+ * @param errorPtr A reference to the `NSError` object where an error information will be saved in case of failure.
+ *
  * @return The decoded data.
  * END CANONICAL DOCSTRING
  */
-- (nullable NSData *)decode:(NSData *)delta base:(NSData *)base error:(NSError *__autoreleasing _Nullable * _Nullable)error;
+- (nullable NSData *)decode:(NSData *)delta base:(NSData *)base error:(NSError *__autoreleasing _Nullable * _Nullable)errorPtr;
 @end
 
 NS_ASSUME_NONNULL_END
