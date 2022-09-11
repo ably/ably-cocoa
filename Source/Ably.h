5039df83c648f895af033664901cbf5c0a5c8235
Marat Al <m.alekperov@gmail.com>
2022-09-11 15:35:30 +0200

    Removed "ARTNSMutableDictionary+ARTDictionaryUtil.h" from public headers

--- Source/Ably.h
+++ Source/Ably.h
@@ -61,4 +61,3 @@ FOUNDATION_EXPORT const unsigned char ablyVersionString[];
 #import <Ably/CompatibilityMacros.h>
 #import <Ably/ARTPendingMessage.h>
 #import <Ably/ARTStringifiable.h>
-#import <Ably/ARTNSMutableDictionary+ARTDictionaryUtil.h>
