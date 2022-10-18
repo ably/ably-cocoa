3b3f2861df829eba85f284da59df2f0e61ba7ac0
ikbalkaya <ceikbal@gmail.com>
2022-10-18 19:18:19 +0100

    Fix version check test for RestClientTests

--- Spec/Tests/RestClientTests.swift
+++ Spec/Tests/RestClientTests.swift
@@ -1646,7 +1646,7 @@ class RestClientTests: XCTestCase {
 
                 // This test should not directly validate version against ARTDefault.version(), as
                 // ultimately the version header has been derived from that value.
-                expect(headerAblyVersion).to(equal("1.2"))
+                expect(headerAblyVersion).to(equal("2.0"))
 
                 done()
             }
