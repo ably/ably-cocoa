0eb201495aeeadaa3926dfaf084e5d894523fdfe
ikbalkaya <ceikbal@gmail.com>
2022-11-11 16:13:54 +0000

    Add doc for when the recovery key might be nil

--- Source/ARTConnection.h
+++ Source/ARTConnection.h
@@ -57,6 +57,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  * The recovery key string can be used by another client to recover this connection's state in the recover client options property. See [connection state recover options](https://ably.com/docs/realtime/connection#connection-state-recover-options) for more information.
+ * This wil return nil if connection is in  CLOSED, CLOSING, FAILED, or SUSPENDED states, or when it does not have a connectionKey (for example, it has not yet become connected).
  */
 - (nullable NSString *) createRecoveryKey;
 
