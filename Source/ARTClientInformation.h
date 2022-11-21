d190a62986b2aa156b8ac3e3b49183da574eca76
Lawrence Forooghian <lawrence@forooghian.com>
2022-11-21 10:06:15 -0300

    Add draft canonical docstring for ClientInformation.agentIdentifier
    Taken verbatim from ably/sdk-api-reference@e7bafe9.
    
    This will need updating after the tech writers have made their edits,
    but we can do that as a separate step.

--- Source/ARTClientInformation.h
+++ Source/ARTClientInformation.h
@@ -18,6 +18,14 @@ extern NSString *const ARTClientInformationAgentNotVersioned;
  * Returns the default key-value entries that the Ably client library uses to identify itself, and the environment in which it’s running, to the Ably service. Its keys are the names of the software components, and its values are their optional versions. The full list of keys that this method might return can be found [here](https://github.com/ably/ably-common/tree/main/protocol#agents). For example, users of the `ably-cocoa` client library can find out the library version by fetching the value for the `"ably-cocoa"` key from the return value of this method.
  */
 @property (class, readonly) NSDictionary<NSString *, NSString *> *agents;
+
+/**
+ * Returns the `Agent` library identifier. This method should only be used by Ably-authored SDKs.
+ *
+ * @param additionalAgents A set of additional entries for the `Agent` library identifier. Its keys are the names of the agents, and its values are their optional versions.
+ *
+ * @return The `Agent` library identifier.
+ */
 + (NSString *)agentIdentifierWithAdditionalAgents:(nullable NSDictionary<NSString *, NSString *> *)additionalAgents;
 
 @end
