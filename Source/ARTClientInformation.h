42e737c3c13c511fe4e235ad49a1add0af617617
Lawrence Forooghian <lawrence@forooghian.com>
2022-11-21 10:06:15 -0300

    Add draft canonical docstring for ClientInformation.agents
    Taken verbatim from ably/sdk-api-reference@e7bafe9.
    
    This will need updating after the tech writers have made their edits,
    but we can do that as a separate step.

--- Source/ARTClientInformation.h
+++ Source/ARTClientInformation.h
@@ -14,6 +14,9 @@ extern NSString *ARTClientInformationAgentNotVersioned;
 
 - (instancetype)init NS_UNAVAILABLE;
 
+/**
+ * Returns the default key-value entries that the Ably client library uses to identify itself, and the environment in which it’s running, to the Ably service. Its keys are the names of the software components, and its values are their optional versions. The full list of keys that this method might return can be found [here](https://github.com/ably/ably-common/tree/main/protocol#agents). For example, users of the `ably-cocoa` client library can find out the library version by fetching the value for the `"ably-cocoa"` key from the return value of this method.
+ */
 @property (class, readonly) NSDictionary<NSString *, NSString *> *agents;
 + (NSString *)agentIdentifierWithAdditionalAgents:(nullable NSDictionary<NSString *, NSString *> *)additionalAgents;
 
