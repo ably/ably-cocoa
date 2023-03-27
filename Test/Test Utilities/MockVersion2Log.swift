76e3497748ec9cdc38dc6be5984716ac8c795709
Lawrence Forooghian <lawrence@forooghian.com>
2023-03-27 19:28:15 -0300

    Add ARTInternalLog class
    Resolves #1619.

--- Test/Test Utilities/MockVersion2Log.swift
+++ Test/Test Utilities/MockVersion2Log.swift
@@ -1,13 +1,19 @@
 import Ably.Private
 
-class MockVersion2Log: Version2Log {
+@objc(ARTMockVersion2Log)
+class MockVersion2Log: NSObject, Version2Log {
     var logLevel: ARTLogLevel = .none
 
     var lastReceivedLogMessageArguments: (message: String, level: ARTLogLevel)?
-    var lastReceivedLogErrorArgument: ARTErrorInfo?
+    @objc var lastReceivedLogMessageArgumentMessage: String?
+    @objc var lastReceivedLogMessageArgumentLevel: ARTLogLevel = .none
+
+    @objc var lastReceivedLogErrorArgument: ARTErrorInfo?
 
     func log(_ message: String, with level: ARTLogLevel) {
         lastReceivedLogMessageArguments = (message: message, level: level)
+        lastReceivedLogMessageArgumentMessage = message
+        lastReceivedLogMessageArgumentLevel = level
     }
 
     func logWithError(_ error: ARTErrorInfo) {
