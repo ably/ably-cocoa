f1b2b1769c1782efb7fcff1cb25a19d7cf53fe22
Lawrence Forooghian <lawrence@forooghian.com>
2023-03-27 17:33:50 -0300

    Add a protocol and class for checking errors

---
+++ Test/Tests/DefaultErrorCheckerTests.swift
@@ -0,0 +1,46 @@
+import XCTest
+import Ably.Private
+
+final class DefaultErrorCheckerTests: XCTestCase {
+    private let checker = DefaultErrorChecker()
+
+    func test_isTokenError_statusCode401AndCodeBetween40140And40150() {
+        let errorInfo = ARTErrorInfo.create(
+            withCode: 40145, // arbitrarily chosen in range 40140 ..< 40150
+            status: 401,
+            message: "" // arbitrarily chosen
+        )
+
+        XCTAssertTrue(checker.isTokenError(errorInfo))
+    }
+
+    func test_isTokenError_statusCodeNot401() {
+        let errorInfo = ARTErrorInfo.create(
+            withCode: 40145, // arbitrarily chosen within range 40140 ..< 40150
+            status: 200, // arbitrarily chosen, != 401
+            message: "" // arbitrarily chosen
+        )
+
+        XCTAssertFalse(checker.isTokenError(errorInfo))
+    }
+
+    func test_isTokenError_statusCode401ButCodeLessThan40140() {
+        let errorInfo = ARTErrorInfo.create(
+            withCode: 40139, // arbitrarily chosen < 40140
+            status: 401,
+            message: "" // arbitrarily chosen
+        )
+
+        XCTAssertFalse(checker.isTokenError(errorInfo))
+    }
+
+    func test_isTokenError_statusCode401ButCodeGreaterThanOrEqualTo40150() {
+        let errorInfo = ARTErrorInfo.create(
+            withCode: 40150, // arbitrarily chosen >= 40150
+            status: 401,
+            message: "" // arbitrarily chosen
+        )
+
+        XCTAssertFalse(checker.isTokenError(errorInfo))
+    }
+}
