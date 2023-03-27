702fa38a294ac5387ba6fbe419bcd7493d8ac2e5
Lawrence Forooghian <lawrence@forooghian.com>
2023-03-27 08:01:51 -0300

    Remove prepare-pod script
    We stopped using this in dcb1f2d but didn’t remove the script or its
    usage in our workflows.

--- Scripts/prepare-pod.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-sed -i '' -e 's/#import <SocketRocketAblyFork\/\(.*\)>/#import "\1"/g' \
-  SocketRocket/SocketRocket/Internal/Delegate/ARTSRDelegateController.h \
-  SocketRocket/SocketRocket/Internal/Utilities/ARTSRError.h
