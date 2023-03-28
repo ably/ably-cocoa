90aae13875dd7117f4cc021ea1eec40eb0f6dd63
Lawrence Forooghian <lawrence@forooghian.com>
2023-03-28 13:10:13 -0300

    Add ARTVersion2Log protocol
    Resolves #1620.

---
+++ Test/Test Utilities/MockVersion2Log.swift
@@ -0,0 +1,16 @@
+import Ably.Private
+
+class MockVersion2Log: Version2Log {
+    var logLevel: ARTLogLevel = .none
+
+    var lastReceivedLogMessageArguments: (message: String, level: ARTLogLevel)?
+    var lastReceivedLogErrorArgument: ARTErrorInfo?
+
+    func log(_ message: String, with level: ARTLogLevel) {
+        lastReceivedLogMessageArguments = (message: message, level: level)
+    }
+
+    func logWithError(_ error: ARTErrorInfo) {
+        lastReceivedLogErrorArgument = error
+    }
+}
