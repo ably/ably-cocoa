53b062ad04f7068bb20bdbb19fdff5b9a0664fbd
Lawrence Forooghian <lawrence@forooghian.com>
2023-04-05 14:35:47 -0300

    Add ARTLogAdapter
    We’ll use this in our implementation of #1622 (updating the SDK to use
    ARTInternalLogHandler internally for all of its logging).
    
    Resolves #1621.

---
+++ Source/PrivateHeaders/Ably/ARTLogAdapter.h
@@ -0,0 +1,50 @@
+@import Foundation;
+
+#import "ARTVersion2Log.h"
+
+@class ARTLog;
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ `ARTLogAdapter` provides an implementation of `ARTVersion2Log` that writes all log messages to a given instance of `ARTLog`.
+
+ The intention of this class is to allow us to maintain the public API of the SDK, which allows users to provide an `ARTLog` instance, whilst using `ARTVersion2Log` for logging inside the SDK. Upon the next major release of this library, when `ARTVersion2Log` will be renamed `ARTLog` and the current `ARTLog` will be removed, we can remove this class too.
+ */
+NS_SWIFT_NAME(LogAdapter)
+@interface ARTLogAdapter: NSObject <ARTVersion2Log>
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/**
+ Creates an instance of `ARTLogAdapter` which wraps an underlying `ARTLog` instance.
+
+ - Parameters:
+    - logger: The `ARTLog` instance to wrap.
+ */
+- (instancetype)initWithLogger:(ARTLog *)logger;
+
+/**
+ `ARTLogAdapter` implements this `ARTVersion2Log` protocol requirement by calling the `-log:withMessage:` method on its underlying `ARTLog` instance.
+
+ This implementation will necessarily change (becoming more complex) as we evolve the `ARTVersion2Log` protocol and hence the signature of this method. For example, if we add the ability to attach metadata to a log message, then this implementation will need to choose a way of representing that metadata as a string.
+
+ - Note: `ARTLogAdapter` directly calls the underlying `ARTLog` instance’s `-log:withLevel:` method. It does not call any of the convenience methods in the "`ARTLog (Shorthand)`" category.
+ */
+- (void)log:(NSString *)message withLevel:(ARTLogLevel)level;
+
+/**
+ `ARTLogAdapter` implements this `ARTVersion2Log` protocol requirement by calling the `-logWithError:` method on its underlying `ARTLog` instance.
+
+ As with `-log:withLevel:`, the implementation of this method may change, or the method may disappear entirely, as we evolve the `ARTVersion2Log` protocol.
+ */
+- (void)logWithError:(ARTErrorInfo *)error;
+
+/**
+ `ARTLogAdapter` implements this `ARTVersion2Log` protocol requirement by forwarding the setter and getter calls to its underlying `ARTLog` instance.
+ */
+@property (nonatomic, assign) ARTLogLevel logLevel;
+
+@end
+
+NS_ASSUME_NONNULL_END
