ac3f411f948ef936e9741b5a74eb4c8a61820c32
Lawrence Forooghian <lawrence@forooghian.com>
2023-04-17 20:43:47 -0300

    add factory ability to return created instance

--- Test/Test Utilities/TestProxyTransportFactory.swift
+++ Test/Test Utilities/TestProxyTransportFactory.swift
@@ -1,6 +1,21 @@
 import Ably.Private
 
 class TestProxyTransportFactory: RealtimeTransportFactory {
+    private let semaphore = DispatchSemaphore(value: 1)
+    private var createdTransportCount = 0
+    private var firstCreatedTransport: TestProxyTransport?
+    var onlyCreatedTransport: TestProxyTransport {
+        get {
+            semaphore.wait()
+            guard createdTransportCount == 1 else {
+                preconditionFailure("Expected precisely 1 transport to have already been created, but \(createdTransportCount) have")
+            }
+            let onlyCreatedTransport = self.firstCreatedTransport
+            semaphore.signal()
+            return onlyCreatedTransport!
+        }
+    }
+
     var actionsIgnored: [ARTProtocolMessageAction]
 
     init(
@@ -19,6 +34,13 @@ class TestProxyTransportFactory: RealtimeTransportFactory {
         )
 
         transport.actionsIgnored = actionsIgnored
+
+        semaphore.wait()
+        if (createdTransportCount == 0) {
+            firstCreatedTransport = transport
+        }
+        createdTransportCount += 1
+        semaphore.signal()
 
         return transport
     }
