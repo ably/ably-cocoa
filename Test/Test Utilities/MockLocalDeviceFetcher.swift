c3decc996f9146ef69cc8a9a4fc7a77899f3bedb
Lawrence Forooghian <lawrence@forooghian.com>
2023-04-18 12:12:23 -0300

    Add a mock instance of ARTLocalDeviceFetcher

---
+++ Test/Test Utilities/MockLocalDeviceFetcher.swift
@@ -0,0 +1,20 @@
+import Ably.Private
+
+@objc(ARTMockLocalDeviceFetcher)
+class MockLocalDeviceFetcher: NSObject, LocalDeviceFetcher {
+    private let semaphore = DispatchSemaphore(value: 1)
+    private var device: ARTLocalDevice?
+
+    func fetchLocalDevice(withClientID clientID: String, storage: ARTDeviceStorage, logger: InternalLog?) -> ARTLocalDevice {
+        semaphore.wait()
+        let device: ARTLocalDevice
+        if let existingDevice = self.device {
+            device = existingDevice
+        } else {
+            device = ARTLocalDevice.load(clientID, storage: storage, logger: logger)
+            self.device = device
+        }
+        semaphore.signal()
+        return device
+    }
+}
