657c046f8fb81353310a273902962f2c6bae4644
Lawrence Forooghian <lawrence@forooghian.com>
2023-05-11 18:24:55 -0300

    wip remove use of global state for websocket methods

--- Test/Test Utilities/TestProxyTransportFactory.swift
+++ Test/Test Utilities/TestProxyTransportFactory.swift
@@ -1,5 +1,29 @@
 import Ably.Private
 
+private class TestProxyTransportWebSocket: ARTSRWebSocket {
+    weak var testProxyTransport: TestProxyTransport?
+
+    override func open() {
+        guard let testProxyTransport else {
+            preconditionFailure("Tried to fetch testProxyTransport but it's already been deallocated")
+        }
+        if !testProxyTransport.handleWebSocketOpen() {
+            super.open()
+        }
+    }
+}
+
+private class TestProxyTransportWebSocketFactory: WebSocketFactory {
+    weak var testProxyTransport: TestProxyTransport?
+
+    func createWebSocket(with request: URLRequest, logger: InternalLog?) -> ARTSRWebSocket {
+        let webSocket = TestProxyTransportWebSocket(urlRequest: request, logger: logger)
+        webSocket.testProxyTransport = testProxyTransport
+
+        return webSocket
+    }
+}
+
 class TestProxyTransportFactory: RealtimeTransportFactory {
     // This value will be used by all TestProxyTransportFactory instances created by this factory (including those created before this property is updated).
     var fakeNetworkResponse: FakeNetworkResponse?
@@ -8,9 +32,9 @@ class TestProxyTransportFactory: RealtimeTransportFactory {
     var networkConnectEvent: ((ARTRealtimeTransport, URL) -> Void)?
 
     func transport(withRest rest: ARTRestInternal, options: ARTClientOptions, resumeKey: String?, connectionSerial: NSNumber?, logger: InternalLog) -> ARTRealtimeTransport {
-        let webSocketFactory = DefaultWebSocketFactory()
+        let webSocketFactory = TestProxyTransportWebSocketFactory()
 
-        return TestProxyTransport(
+        let testProxyTransport = TestProxyTransport(
             factory: self,
             rest: rest,
             options: options,
@@ -19,5 +43,9 @@ class TestProxyTransportFactory: RealtimeTransportFactory {
             logger: logger,
             webSocketFactory: webSocketFactory
         )
+
+        webSocketFactory.testProxyTransport = testProxyTransport
+
+        return testProxyTransport
     }
 }
