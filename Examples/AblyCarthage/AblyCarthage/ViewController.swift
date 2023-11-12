98309d8fd16a91709ca2a128d597ae265e261a71
Marat Al <m.alekperov@gmail.com>
2023-11-13 00:04:40 +0100

    Added Carthage installation sample.

---
+++ Examples/AblyCarthage/AblyCarthage/ViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+import Ably
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
