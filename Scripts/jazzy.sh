b647e97a3201da8fde898132f3a2304188eb7369
Marat Al <m.alekperov@gmail.com>
2024-05-21 14:32:32 +0200

    Bump version to 1.2.32.

--- Scripts/jazzy.sh
+++ Scripts/jazzy.sh
@@ -7,7 +7,7 @@ jazzy \
   --objc \
   --clean \
   --author Ably \
-  --module-version 1.2.31 \
+  --module-version 1.2.32 \
   --umbrella-header Source/include/Ably/Ably.h \
   --framework-root Source \
   --module Ably \
