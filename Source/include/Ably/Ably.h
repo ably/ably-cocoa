c23d0fa42cdae7d5fec0045b834307ed93c96753
Lawrence Forooghian <lawrence@forooghian.com>
2025-01-30 11:37:33 -0300

    Add mechanism for excluding public API from Jazzy docs
    Will use this for #2020 (wrapper SDK proxy), which will introduce some
    new API that is only meant to be used by other Ably-authored SDKs.
    
    (I'm not sure if the implementation here is the best possible one; tried
    using Jazzy's `--exclude` but it didn't seem to do anything.)

--- Source/include/Ably/Ably.h
+++ Source/include/Ably/Ably.h
@@ -12,52 +12,5 @@ FOUNDATION_EXPORT double ablyVersionNumber;
  */
 FOUNDATION_EXPORT const unsigned char ablyVersionString[];
 
-#import <Ably/ARTLog.h>
-#import <Ably/ARTTypes.h>
-
-#import <Ably/ARTAuth.h>
-#import <Ably/ARTAuthDetails.h>
-#import <Ably/ARTConnection.h>
-#import <Ably/ARTConnectionDetails.h>
-#import <Ably/ARTBaseMessage.h>
-#import <Ably/ARTRestChannels.h>
-#import <Ably/ARTChannelOptions.h>
-#import <Ably/ARTTokenDetails.h>
-#import <Ably/ARTTokenRequest.h>
-#import <Ably/ARTTokenParams.h>
-#import <Ably/ARTAuthOptions.h>
-#import <Ably/ARTClientOptions.h>
-#import <Ably/ARTCrypto.h>
-#import <Ably/ARTDefault.h>
-#import <Ably/ARTEventEmitter.h>
-#import <Ably/ARTFallback.h>
-#import <Ably/ARTMessage.h>
-#import <Ably/ARTPresence.h>
-#import <Ably/ARTPresenceMessage.h>
-#import <Ably/ARTProtocolMessage.h>
-#import <Ably/ARTQueuedMessage.h>
-#import <Ably/ARTRest.h>
-#import <Ably/ARTRestChannel.h>
-#import <Ably/ARTRestPresence.h>
-#import <Ably/ARTRealtime.h>
-#import <Ably/ARTRealtimeChannel.h>
-#import <Ably/ARTRealtimeChannelOptions.h>
-#import <Ably/ARTRealtimePresence.h>
-#import <Ably/ARTStats.h>
-#import <Ably/ARTEncoder.h>
-#import <Ably/ARTPaginatedResult.h>
-#import <Ably/ARTHTTPPaginatedResponse.h>
-#import <Ably/ARTPush.h>
-#import <Ably/ARTPushChannel.h>
-#import <Ably/ARTPushChannelSubscription.h>
-#import <Ably/ARTPushAdmin.h>
-#import <Ably/ARTPushChannelSubscriptions.h>
-#import <Ably/ARTPushDeviceRegistrations.h>
-#import <Ably/ARTDeviceDetails.h>
-#import <Ably/ARTDevicePushDetails.h>
-#import <Ably/ARTDeviceIdentityTokenDetails.h>
-#import <Ably/ARTDeviceStorage.h>
-#import <Ably/ARTLocalDevice.h>
-#import <Ably/ARTPendingMessage.h>
-#import <Ably/ARTStringifiable.h>
-#import <Ably/ARTClientInformation.h>
+#import <Ably/AblyPublic.h>
+#import <Ably/AblyInternal.h>
---
+++ Source/include/Ably/AblyInternal.h
@@ -0,0 +1,1 @@
+// This header contains imports of API that, whilst public, is intended for use only by Ably-authored SDKs and thus should be excluded from the Jazzy-generated documentation.
