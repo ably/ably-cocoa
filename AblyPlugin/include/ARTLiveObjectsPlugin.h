05489b76e8fbf296ca6d7d042d99dc3ce8b31db7
Lawrence Forooghian <lawrence@forooghian.com>
2025-02-24 11:00:49 -0300

    what if it's not a symlink, does that help?

--- AblyPlugin/include/ARTLiveObjectsPlugin.h
+++ AblyPlugin/include/ARTLiveObjectsPlugin.h
@@ -1,1 +1,13 @@
-../../Source/PrivateHeaders/Ably/ARTLiveObjectsPlugin.h
+#import <Foundation/Foundation.h>
+
+@class ARTRealtimeChannel;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol ARTLiveObjectsPlugin <NSObject>
+
++ (void)prepareChannel:(ARTRealtimeChannel *)channel;
+
+@end
+
+NS_ASSUME_NONNULL_END
