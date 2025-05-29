6ab19f4695de5785417c71328c36750a669b96fc
Marat Al <m.alekperov@gmail.com>
2025-05-29 21:35:14 +0200

    Moved `ARTPresenceQuery` to appropriate file.

--- Source/PrivateHeaders/Ably/ARTPresence+Private.h
+++ Source/PrivateHeaders/Ably/ARTPresence+Private.h
@@ -1,12 +1,6 @@
 #import <Ably/ARTPresence.h>
 #import <Ably/ARTChannel.h>
 
-@interface ARTPresenceQuery ()
-
-- (NSMutableArray<NSURLQueryItem *> *)asQueryItems;
-
-@end
-
 @interface ARTPresence ()
 
 @property (readonly, getter=getChannel) ARTChannel *channel;
