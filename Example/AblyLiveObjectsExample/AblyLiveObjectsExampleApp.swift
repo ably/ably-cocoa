54cbfa2de0623936def479894c6ffb74d02d3593
Marat Al <m.alekperov@gmail.com>
2025-08-22 14:39:16 +0200

    Example app based on https://ably.com/examples/liveobjects-live-counter?lang=javascript

--- Example/AblyLiveObjectsExample/AblyLiveObjectsExampleApp.swift
+++ Example/AblyLiveObjectsExample/AblyLiveObjectsExampleApp.swift
@@ -4,16 +4,23 @@ import SwiftUI
 
 @main
 struct AblyLiveObjectsExampleApp: App {
-    @State private var realtime = {
+    private func getRealtime() -> ARTRealtime {
         let clientOptions = ARTClientOptions(key: Secrets.ablyAPIKey)
         clientOptions.plugins = [.liveObjects: AblyLiveObjects.Plugin.self]
-
         return ARTRealtime(options: clientOptions)
-    }()
+    }
 
     var body: some Scene {
         WindowGroup {
-            ContentView(realtime: realtime)
+            #if os(macOS)
+                ContentView(realtime1: getRealtime(), realtime2: getRealtime())
+                    .frame(width: 400, height: 700, alignment: .center)
+            #else
+                ContentView(realtime1: getRealtime(), realtime2: getRealtime())
+            #endif
         }
+        #if os(macOS)
+        .windowResizability(.contentSize)
+        #endif
     }
 }
