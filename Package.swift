403d1c0ee7c49700bd22d7abdb021906a0346f31
umair <umair.hussain@ably.com>
2025-08-28 12:38:33 +0100

    unpins ably-cocoa version. now points to the min required version with liveobjects support

--- Package.swift
+++ Package.swift
@@ -20,8 +20,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/ably/ably-cocoa",
-            // TODO: Unpin before launch (https://github.com/ably/ably-liveobjects-swift-plugin/issues/75)
-            revision: "b0c2ecf9da993736297b6986be5610b375440a3f",
+            from: "1.2.44",
         ),
         .package(
             url: "https://github.com/ably/ably-cocoa-plugin-support",
