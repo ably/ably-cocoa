393c18f0aa5e01b764b123e158866691f2dccba6
Marat Al <m.alekperov@gmail.com>
2025-09-13 22:49:37 +0200

    Fix address for apps config.

--- Test/Test Utilities/TestUtilities.swift
+++ Test/Test Utilities/TestUtilities.swift
@@ -116,7 +116,7 @@ class AblyTests {
         if let testApplication {
             app = testApplication
         } else {
-            let request = NSMutableURLRequest(url: URL(string: "https://\(options.primaryDomain):\(options.tlsPort)/apps")!)
+            let request = NSMutableURLRequest(url: URL(string: "https://sandbox.realtime.ably-nonprod.net/apps")!) // G2
             request.httpMethod = "POST"
             request.httpBody = try JSONUtility.encode(appSetupModel.postApps)
 
