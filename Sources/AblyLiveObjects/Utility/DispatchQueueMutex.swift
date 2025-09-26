ably/ably-liveobjects-swift-plugin

Finalise and document threading approach
Isolate the plugin's mutable state to the ARTRealtime instance's
internal queue, for consistency with ably-cocoa.

Will unpin ably-cocoa and ably-cocoa-plugin-support before our next
release.

Resolves #3.

Sources/AblyLiveObjects/Utility/DispatchQueueMutex.swift
@@ -1,0 +1,48 @@
+import Foundation
+
+/// A class that provides mutually exclusive access to a value using a serial dispatch queue.
+///
+/// In order to access or mutate the mutex's value, it is expected that you know whether or not you are already executing on the mutex's queue. If you are, then use ``withoutLock(_:)``, which simply performs a runtime check that the current queue is correct. If not, then use ``withLock(_:)``, which synchronously dispatches to the queue. ``withLock(_:)`` must not be called from the queue, as doing so would cause a deadlock (there is a runtime check which terminates execution in this case).
+///
+/// This class is styled on Swift's built-in `Mutex` type.
+internal final class DispatchQueueMutex<T>: Sendable {
+    /// The queue that this mutex uses to synchronise access to the wrapped value.
+    internal let dispatchQueue: DispatchQueue
+
+    private nonisolated(unsafe) var value: T
+
+    internal init(dispatchQueue: DispatchQueue, initialValue: T) {
+        self.dispatchQueue = dispatchQueue
+        value = initialValue
+    }
+
+    /// Provides access to the wrapped value by dispatching synchronously to the dispatch queue.
+    ///
+    /// - Parameters:
+    ///   - body: The action to perform. It can read and/or mutate the wrapped value.
+    ///
+    /// - Warning: This must only be called when not already on the dispatch queue. Violating this precondition will result in a runtime error.
+    internal func withLock<R, E>(_ body: (inout T) throws(E) -> R) throws(E) -> R {
+        let result: Result<R, E> = dispatchQueue.ably_syncNoDeadlock {
+            do throws(E) {
+                return try .success(body(&value))
+            } catch {
+                return .failure(error)
+            }
+        }
+
+        return try result.get()
+    }
+
+    /// Provides access to the wrapped value without dispatching to the dispatch queue.
+    ///
+    /// - Parameters:
+    ///   - body: The action to perform. It can read and/or mutate the wrapped value.
+    ///
+    /// - Warning: This must only be called when already on the dispatch queue. Violating this precondition will result in a runtime error.
+    internal func withoutLock<R, E>(_ body: (inout T) throws(E) -> R) throws(E) -> R {
+        dispatchPrecondition(condition: .onQueue(dispatchQueue))
+
+        return try body(&value)
+    }
+}

Tests/AblyLiveObjectsTests/LiveObjectMutableStateTests.swift
@@ -124,10 +124,13 @@ struct LiveObjectMutableStateTests {
             var mutableState = LiveObjectMutableState<String>(objectID: "foo")
             let queue = DispatchQueue.main
             let subscriber = Subscriber<String, SubscribeResponse>(callbackQueue: queue)
-            let coreSDK = MockCoreSDK(channelState: channelState)
+            let internalQueue = TestFactories.createInternalQueue()
+            let coreSDK = MockCoreSDK(channelState: channelState, internalQueue: internalQueue)
 
             #expect {
-                try mutableState.subscribe(listener: subscriber.createListener(), coreSDK: coreSDK, updateSelfLater: { _ in fatalError("Not expected") })
+                try internalQueue.ably_syncNoDeadlock {
+                    try mutableState.nosync_subscribe(listener: subscriber.createListener(), coreSDK: coreSDK, updateSelfLater: { _ in fatalError("Not expected") })
+                }
             } throws: { error in
                 guard let errorInfo = error as? ARTErrorInfo else {
                     return false
@@ -146,8 +149,11 @@ struct LiveObjectMutableStateTests {
                 var mutableState = LiveObjectMutableState<String>(objectID: "foo")
                 let queue = DispatchQueue.main
                 let subscriber = Subscriber<String, SubscribeResponse>(callbackQueue: queue)
-                let coreSDK = MockCoreSDK(channelState: .attached)
-                try mutableState.subscribe(listener: subscriber.createListener(), coreSDK: coreSDK, updateSelfLater: { _ in fatalError("Not expected") })
+                let internalQueue = TestFactories.createInternalQueue()
+                let coreSDK = MockCoreSDK(channelState: .attached, internalQueue: internalQueue)
+                try internalQueue.ably_syncNoDeadlock {
+                    _ = try mutableState.nosync_subscribe(listener: subscriber.createListener(), coreSDK: coreSDK, updateSelfLater: { _ in fatalError("Not expected") })
+                }
 
                 // When
                 mutableState.emit(.noop, on: queue)
@@ -165,8 +171,11 @@ struct LiveObjectMutableStateTests {
                 var mutableState = LiveObjectMutableState<String>(objectID: "foo")
                 let queue = DispatchQueue.main
                 let subscriber = Subscriber<String, SubscribeResponse>(callbackQueue: queue)
-                let coreSDK = MockCoreSDK(channelState: .attached)
-                try mutableState.subscribe(listener: subscriber.createListener(), coreSDK: coreSDK, updateSelfLater: { _ in fatalError("Not expected") })
+                let internalQueue = TestFactories.createInternalQueue()
+                let coreSDK = MockCoreSDK(channelState: .attached, internalQueue: internalQueue)
+                try internalQueue.ably_syncNoDeadlock {
+                    _ = try mutableState.nosync_subscribe(listener: subscriber.createListener(), coreSDK: coreSDK, updateSelfLater: { _ in fatalError("Not expected") })
+                }
 
                 // When
                 mutableState.emit(.update("bar"), on: queue)
@@ -179,22 +188,21 @@ struct LiveObjectMutableStateTests {
 
         struct UnsubscribeTests {
             final class MutableStateStore<Update: Sendable>: Sendable {
-                private let mutex = NSLock()
-                private nonisolated(unsafe) var stored: LiveObjectMutableState<Update>
+                private let mutex: DispatchQueueMutex<LiveObjectMutableState<Update>>
 
-                init(stored: LiveObjectMutableState<Update>) {
-                    self.stored = stored
+                init(stored: LiveObjectMutableState<Update>, internalQueue: DispatchQueue) {
+                    mutex = .init(dispatchQueue: internalQueue, initialValue: stored)
                 }
 
                 @discardableResult
                 func subscribe(listener: @escaping LiveObjectUpdateCallback<Update>, coreSDK: CoreSDK) throws(ARTErrorInfo) -> SubscribeResponse {
-                    try mutex.ablyLiveObjects_withLockWithTypedThrow { () throws(ARTErrorInfo) in
-                        try stored.subscribe(listener: listener, coreSDK: coreSDK, updateSelfLater: { [weak self] action in
+                    try mutex.withLock { stored throws(ARTErrorInfo) in
+                        try stored.nosync_subscribe(listener: listener, coreSDK: coreSDK, updateSelfLater: { [weak self] action in
                             guard let self else {
                                 return
                             }
 
-                            mutex.withLock {
+                            mutex.withLock { stored in
                                 action(&stored)
                             }
                         })
@@ -202,13 +210,13 @@ struct LiveObjectMutableStateTests {
                 }
 
                 func emit(_ update: LiveObjectUpdate<Update>, on queue: DispatchQueue) {
-                    mutex.withLock {
+                    mutex.withLock { stored in
                         stored.emit(update, on: queue)
                     }
                 }
 
                 func unsubscribeAll() {
-                    mutex.withLock {
+                    mutex.withLock { stored in
                         stored.unsubscribeAll()
                     }
                 }
@@ -219,10 +227,11 @@ struct LiveObjectMutableStateTests {
             @Test
             func unsubscribeFromReturnValue() async throws {
                 // Given
-                let store = MutableStateStore<String>(stored: .init(objectID: "foo"))
                 let queue = DispatchQueue.main
+                let internalQueue = TestFactories.createInternalQueue()
+                let store = MutableStateStore<String>(stored: .init(objectID: "foo"), internalQueue: internalQueue)
                 let subscriber = Subscriber<String, SubscribeResponse>(callbackQueue: queue)
-                let coreSDK = MockCoreSDK(channelState: .attached)
+                let coreSDK = MockCoreSDK(channelState: .attached, internalQueue: internalQueue)
                 let subscription = try store.subscribe(listener: subscriber.createListener(), coreSDK: coreSDK)
 
                 // When
@@ -240,10 +249,11 @@ struct LiveObjectMutableStateTests {
             @Test(.disabled("This doesn't currently work and I don't think it's a priority, nor do I want to dwell on it right now or rush trying to fix it; see https://github.com/ably/ably-liveobjects-swift-plugin/issues/28"))
             func unsubscribeInsideCallback_backToBackUpdates() async throws {
                 // Given
-                let store = MutableStateStore<String>(stored: .init(objectID: "foo"))
                 let queue = DispatchQueue.main
+                let internalQueue = TestFactories.createInternalQueue()
+                let store = MutableStateStore<String>(stored: .init(objectID: "foo"), internalQueue: internalQueue)
                 let subscriber = Subscriber<String, SubscribeResponse>(callbackQueue: queue)
-                let coreSDK = MockCoreSDK(channelState: .attached)
+                let coreSDK = MockCoreSDK(channelState: .attached, internalQueue: internalQueue)
                 // Create a listener that calls `unsubscribe` on the `response` that's passed to the listener
                 let listener = subscriber.createListener { _, response in
                     response.unsubscribe()
@@ -265,10 +275,11 @@ struct LiveObjectMutableStateTests {
             @Test
             func unsubscribeInsideCallback_nonBackToBackUpdates() async throws {
                 // Given
-                let store = MutableStateStore<String>(stored: .init(objectID: "foo"))
                 let queue = DispatchQueue.main
+                let internalQueue = TestFactories.createInternalQueue()
+                let store = MutableStateStore<String>(stored: .init(objectID: "foo"), internalQueue: internalQueue)
                 let subscriber = Subscriber<String, SubscribeResponse>(callbackQueue: queue)
-                let coreSDK = MockCoreSDK(channelState: .attached)
+                let coreSDK = MockCoreSDK(channelState: .attached, internalQueue: internalQueue)
                 // Create a listener that calls `unsubscribe` on the `response` that's passed to the listener
                 let listener = subscriber.createListener { _, response in
                     response.unsubscribe()
@@ -291,9 +302,10 @@ struct LiveObjectMutableStateTests {
             @Test
             func unsubscribeAll() async throws {
                 // Given
-                let store = MutableStateStore<String>(stored: .init(objectID: "foo"))
                 let queue = DispatchQueue.main
-                let coreSDK = MockCoreSDK(channelState: .attached)
+                let internalQueue = TestFactories.createInternalQueue()
+                let store = MutableStateStore<String>(stored: .init(objectID: "foo"), internalQueue: internalQueue)
+                let coreSDK = MockCoreSDK(channelState: .attached, internalQueue: internalQueue)
                 let subscribers: [Subscriber<String, SubscribeResponse>] = [
                     .init(callbackQueue: queue),
                     .init(callbackQueue: queue),

Tests/AblyLiveObjectsTests/Mocks/MockCoreSDK.swift
@@ -3,14 +3,14 @@ import Ably
 @testable import AblyLiveObjects
 
 final class MockCoreSDK: CoreSDK {
-    /// Synchronizes access to all of this instance's mutable state.
+    /// Synchronizes access to `_publishHandler`.
     private let mutex = NSLock()
-
-    private nonisolated(unsafe) var _channelState: _AblyPluginSupportPrivate.RealtimeChannelState
     private nonisolated(unsafe) var _publishHandler: (([OutboundObjectMessage]) async throws(InternalError) -> Void)?
 
-    init(channelState: _AblyPluginSupportPrivate.RealtimeChannelState) {
-        _channelState = channelState
+    private let channelStateMutex: DispatchQueueMutex<_AblyPluginSupportPrivate.RealtimeChannelState>
+
+    init(channelState: _AblyPluginSupportPrivate.RealtimeChannelState, internalQueue: DispatchQueue) {
+        channelStateMutex = DispatchQueueMutex(dispatchQueue: internalQueue, initialValue: channelState)
     }
 
     func publish(objectMessages: [OutboundObjectMessage]) async throws(InternalError) {
@@ -25,17 +25,8 @@ final class MockCoreSDK: CoreSDK {
         protocolRequirementNotImplemented()
     }
 
-    var channelState: _AblyPluginSupportPrivate.RealtimeChannelState {
-        get {
-            mutex.withLock {
-                _channelState
-            }
-        }
-        set {
-            mutex.withLock {
-                _channelState = newValue
-            }
-        }
+    var nosync_channelState: _AblyPluginSupportPrivate.RealtimeChannelState {
+        channelStateMutex.withoutLock { $0 }
     }
 
     /// Sets a custom publish handler for testing

Tests/AblyLiveObjectsTests/Mocks/MockLiveMapObjectPoolDelegate.swift
@@ -3,23 +3,22 @@ import Foundation
 
 /// A mock delegate that can return predefined objects
 final class MockLiveMapObjectPoolDelegate: LiveMapObjectPoolDelegate {
-    private let mutex = NSLock()
-    private nonisolated(unsafe) var _objects: [String: ObjectsPool.Entry] = [:]
+    private let objectsMutex: DispatchQueueMutex<[String: ObjectsPool.Entry]>
+
+    init(internalQueue: DispatchQueue) {
+        objectsMutex = DispatchQueueMutex(dispatchQueue: internalQueue, initialValue: [:])
+    }
+
     var objects: [String: ObjectsPool.Entry] {
         get {
-            mutex.withLock {
-                _objects
-            }
+            objectsMutex.withLock { $0 }
         }
-
         set {
-            mutex.withLock {
-                _objects = newValue
-            }
+            objectsMutex.withLock { $0 = newValue }
         }
     }
 
-    func getObjectFromPool(id: String) -> ObjectsPool.Entry? {
-        objects[id]
+    func nosync_getObjectFromPool(id: String) -> ObjectsPool.Entry? {
+        objectsMutex.withoutLock { $0[id] }
     }
 }

ably/ably-cocoa

Sources/AblyLiveObjects/Utility/DispatchQueueMutex.swift
@@ -1,0 +1,48 @@
+import Foundation
+
+/// A class that provides mutually exclusive access to a value using a serial dispatch queue.
+///
+/// In order to access or mutate the mutex's value, it is expected that you know whether or not you are already executing on the mutex's queue. If you are, then use ``withoutSync(_:)``, which simply performs a runtime check that the current queue is correct. If not, then use ``withSync(_:)``, which synchronously dispatches to the queue. ``withSync(_:)`` must not be called from the queue, as doing so would cause a deadlock (there is a runtime check which terminates execution in this case).
+///
+/// This class is styled on Swift's built-in `Mutex` type.
+internal final class DispatchQueueMutex<T>: Sendable {
+    /// The queue that this mutex uses to synchronise access to the wrapped value.
+    internal let dispatchQueue: DispatchQueue
+
+    private nonisolated(unsafe) var value: T
+
+    internal init(dispatchQueue: DispatchQueue, initialValue: T) {
+        self.dispatchQueue = dispatchQueue
+        value = initialValue
+    }
+
+    /// Provides access to the wrapped value by dispatching synchronously to the dispatch queue.
+    ///
+    /// - Parameters:
+    ///   - body: The action to perform. It can read and/or mutate the wrapped value.
+    ///
+    /// - Warning: This must only be called when not already on the dispatch queue. Violating this precondition will result in a runtime error.
+    internal func withSync<R, E>(_ body: (inout T) throws(E) -> R) throws(E) -> R {
+        let result: Result<R, E> = dispatchQueue.ably_syncNoDeadlock {
+            do throws(E) {
+                return try .success(body(&value))
+            } catch {
+                return .failure(error)
+            }
+        }
+
+        return try result.get()
+    }
+
+    /// Provides access to the wrapped value without dispatching to the dispatch queue.
+    ///
+    /// - Parameters:
+    ///   - body: The action to perform. It can read and/or mutate the wrapped value.
+    ///
+    /// - Warning: This must only be called when already on the dispatch queue. Violating this precondition will result in a runtime error.
+    internal func withoutSync<R, E>(_ body: (inout T) throws(E) -> R) throws(E) -> R {
+        dispatchPrecondition(condition: .onQueue(dispatchQueue))
+
+        return try body(&value)
+    }
+}

Tests/AblyLiveObjectsTests/LiveObjectMutableStateTests.swift
@@ -124,10 +124,13 @@ struct LiveObjectMutableStateTests {
             var mutableState = LiveObjectMutableState<String>(objectID: "foo")
             let queue = DispatchQueue.main
             let subscriber = Subscriber<String, SubscribeResponse>(callbackQueue: queue)
-            let coreSDK = MockCoreSDK(channelState: channelState)
+            let internalQueue = TestFactories.createInternalQueue()
+            let coreSDK = MockCoreSDK(channelState: channelState, internalQueue: internalQueue)
 
             #expect {
-                try mutableState.subscribe(listener: subscriber.createListener(), coreSDK: coreSDK, updateSelfLater: { _ in fatalError("Not expected") })
+                try internalQueue.ably_syncNoDeadlock {
+                    try mutableState.nosync_subscribe(listener: subscriber.createListener(), coreSDK: coreSDK, updateSelfLater: { _ in fatalError("Not expected") })
+                }
             } throws: { error in
                 guard let errorInfo = error as? ARTErrorInfo else {
                     return false
@@ -146,8 +149,11 @@ struct LiveObjectMutableStateTests {
                 var mutableState = LiveObjectMutableState<String>(objectID: "foo")
                 let queue = DispatchQueue.main
                 let subscriber = Subscriber<String, SubscribeResponse>(callbackQueue: queue)
-                let coreSDK = MockCoreSDK(channelState: .attached)
-                try mutableState.subscribe(listener: subscriber.createListener(), coreSDK: coreSDK, updateSelfLater: { _ in fatalError("Not expected") })
+                let internalQueue = TestFactories.createInternalQueue()
+                let coreSDK = MockCoreSDK(channelState: .attached, internalQueue: internalQueue)
+                try internalQueue.ably_syncNoDeadlock {
+                    _ = try mutableState.nosync_subscribe(listener: subscriber.createListener(), coreSDK: coreSDK, updateSelfLater: { _ in fatalError("Not expected") })
+                }
 
                 // When
                 mutableState.emit(.noop, on: queue)
@@ -165,8 +171,11 @@ struct LiveObjectMutableStateTests {
                 var mutableState = LiveObjectMutableState<String>(objectID: "foo")
                 let queue = DispatchQueue.main
                 let subscriber = Subscriber<String, SubscribeResponse>(callbackQueue: queue)
-                let coreSDK = MockCoreSDK(channelState: .attached)
-                try mutableState.subscribe(listener: subscriber.createListener(), coreSDK: coreSDK, updateSelfLater: { _ in fatalError("Not expected") })
+                let internalQueue = TestFactories.createInternalQueue()
+                let coreSDK = MockCoreSDK(channelState: .attached, internalQueue: internalQueue)
+                try internalQueue.ably_syncNoDeadlock {
+                    _ = try mutableState.nosync_subscribe(listener: subscriber.createListener(), coreSDK: coreSDK, updateSelfLater: { _ in fatalError("Not expected") })
+                }
 
                 // When
                 mutableState.emit(.update("bar"), on: queue)
@@ -179,22 +188,21 @@ struct LiveObjectMutableStateTests {
 
         struct UnsubscribeTests {
             final class MutableStateStore<Update: Sendable>: Sendable {
-                private let mutex = NSLock()
-                private nonisolated(unsafe) var stored: LiveObjectMutableState<Update>
+                private let mutex: DispatchQueueMutex<LiveObjectMutableState<Update>>
 
-                init(stored: LiveObjectMutableState<Update>) {
-                    self.stored = stored
+                init(stored: LiveObjectMutableState<Update>, internalQueue: DispatchQueue) {
+                    mutex = .init(dispatchQueue: internalQueue, initialValue: stored)
                 }
 
                 @discardableResult
                 func subscribe(listener: @escaping LiveObjectUpdateCallback<Update>, coreSDK: CoreSDK) throws(ARTErrorInfo) -> SubscribeResponse {
-                    try mutex.ablyLiveObjects_withLockWithTypedThrow { () throws(ARTErrorInfo) in
-                        try stored.subscribe(listener: listener, coreSDK: coreSDK, updateSelfLater: { [weak self] action in
+                    try mutex.withSync { stored throws(ARTErrorInfo) in
+                        try stored.nosync_subscribe(listener: listener, coreSDK: coreSDK, updateSelfLater: { [weak self] action in
                             guard let self else {
                                 return
                             }
 
-                            mutex.withLock {
+                            mutex.withSync { stored in
                                 action(&stored)
                             }
                         })
@@ -202,13 +210,13 @@ struct LiveObjectMutableStateTests {
                 }
 
                 func emit(_ update: LiveObjectUpdate<Update>, on queue: DispatchQueue) {
-                    mutex.withLock {
+                    mutex.withSync { stored in
                         stored.emit(update, on: queue)
                     }
                 }
 
                 func unsubscribeAll() {
-                    mutex.withLock {
+                    mutex.withSync { stored in
                         stored.unsubscribeAll()
                     }
                 }
@@ -219,10 +227,11 @@ struct LiveObjectMutableStateTests {
             @Test
             func unsubscribeFromReturnValue() async throws {
                 // Given
-                let store = MutableStateStore<String>(stored: .init(objectID: "foo"))
                 let queue = DispatchQueue.main
+                let internalQueue = TestFactories.createInternalQueue()
+                let store = MutableStateStore<String>(stored: .init(objectID: "foo"), internalQueue: internalQueue)
                 let subscriber = Subscriber<String, SubscribeResponse>(callbackQueue: queue)
-                let coreSDK = MockCoreSDK(channelState: .attached)
+                let coreSDK = MockCoreSDK(channelState: .attached, internalQueue: internalQueue)
                 let subscription = try store.subscribe(listener: subscriber.createListener(), coreSDK: coreSDK)
 
                 // When
@@ -240,10 +249,11 @@ struct LiveObjectMutableStateTests {
             @Test(.disabled("This doesn't currently work and I don't think it's a priority, nor do I want to dwell on it right now or rush trying to fix it; see https://github.com/ably/ably-liveobjects-swift-plugin/issues/28"))
             func unsubscribeInsideCallback_backToBackUpdates() async throws {
                 // Given
-                let store = MutableStateStore<String>(stored: .init(objectID: "foo"))
                 let queue = DispatchQueue.main
+                let internalQueue = TestFactories.createInternalQueue()
+                let store = MutableStateStore<String>(stored: .init(objectID: "foo"), internalQueue: internalQueue)
                 let subscriber = Subscriber<String, SubscribeResponse>(callbackQueue: queue)
-                let coreSDK = MockCoreSDK(channelState: .attached)
+                let coreSDK = MockCoreSDK(channelState: .attached, internalQueue: internalQueue)
                 // Create a listener that calls `unsubscribe` on the `response` that's passed to the listener
                 let listener = subscriber.createListener { _, response in
                     response.unsubscribe()
@@ -265,10 +275,11 @@ struct LiveObjectMutableStateTests {
             @Test
             func unsubscribeInsideCallback_nonBackToBackUpdates() async throws {
                 // Given
-                let store = MutableStateStore<String>(stored: .init(objectID: "foo"))
                 let queue = DispatchQueue.main
+                let internalQueue = TestFactories.createInternalQueue()
+                let store = MutableStateStore<String>(stored: .init(objectID: "foo"), internalQueue: internalQueue)
                 let subscriber = Subscriber<String, SubscribeResponse>(callbackQueue: queue)
-                let coreSDK = MockCoreSDK(channelState: .attached)
+                let coreSDK = MockCoreSDK(channelState: .attached, internalQueue: internalQueue)
                 // Create a listener that calls `unsubscribe` on the `response` that's passed to the listener
                 let listener = subscriber.createListener { _, response in
                     response.unsubscribe()
@@ -291,9 +302,10 @@ struct LiveObjectMutableStateTests {
             @Test
             func unsubscribeAll() async throws {
                 // Given
-                let store = MutableStateStore<String>(stored: .init(objectID: "foo"))
                 let queue = DispatchQueue.main
-                let coreSDK = MockCoreSDK(channelState: .attached)
+                let internalQueue = TestFactories.createInternalQueue()
+                let store = MutableStateStore<String>(stored: .init(objectID: "foo"), internalQueue: internalQueue)
+                let coreSDK = MockCoreSDK(channelState: .attached, internalQueue: internalQueue)
                 let subscribers: [Subscriber<String, SubscribeResponse>] = [
                     .init(callbackQueue: queue),
                     .init(callbackQueue: queue),

Tests/AblyLiveObjectsTests/Mocks/MockCoreSDK.swift
@@ -3,14 +3,14 @@ import Ably
 @testable import AblyLiveObjects
 
 final class MockCoreSDK: CoreSDK {
-    /// Synchronizes access to all of this instance's mutable state.
+    /// Synchronizes access to `_publishHandler`.
     private let mutex = NSLock()
-
-    private nonisolated(unsafe) var _channelState: _AblyPluginSupportPrivate.RealtimeChannelState
     private nonisolated(unsafe) var _publishHandler: (([OutboundObjectMessage]) async throws(InternalError) -> Void)?
 
-    init(channelState: _AblyPluginSupportPrivate.RealtimeChannelState) {
-        _channelState = channelState
+    private let channelStateMutex: DispatchQueueMutex<_AblyPluginSupportPrivate.RealtimeChannelState>
+
+    init(channelState: _AblyPluginSupportPrivate.RealtimeChannelState, internalQueue: DispatchQueue) {
+        channelStateMutex = DispatchQueueMutex(dispatchQueue: internalQueue, initialValue: channelState)
     }
 
     func publish(objectMessages: [OutboundObjectMessage]) async throws(InternalError) {
@@ -25,17 +25,8 @@ final class MockCoreSDK: CoreSDK {
         protocolRequirementNotImplemented()
     }
 
-    var channelState: _AblyPluginSupportPrivate.RealtimeChannelState {
-        get {
-            mutex.withLock {
-                _channelState
-            }
-        }
-        set {
-            mutex.withLock {
-                _channelState = newValue
-            }
-        }
+    var nosync_channelState: _AblyPluginSupportPrivate.RealtimeChannelState {
+        channelStateMutex.withoutSync { $0 }
     }
 
     /// Sets a custom publish handler for testing

Tests/AblyLiveObjectsTests/Mocks/MockLiveMapObjectPoolDelegate.swift
@@ -3,23 +3,22 @@ import Foundation
 
 /// A mock delegate that can return predefined objects
 final class MockLiveMapObjectPoolDelegate: LiveMapObjectPoolDelegate {
-    private let mutex = NSLock()
-    private nonisolated(unsafe) var _objects: [String: ObjectsPool.Entry] = [:]
+    private let objectsMutex: DispatchQueueMutex<[String: ObjectsPool.Entry]>
+
+    init(internalQueue: DispatchQueue) {
+        objectsMutex = DispatchQueueMutex(dispatchQueue: internalQueue, initialValue: [:])
+    }
+
     var objects: [String: ObjectsPool.Entry] {
         get {
-            mutex.withLock {
-                _objects
-            }
+            objectsMutex.withSync { $0 }
         }
-
         set {
-            mutex.withLock {
-                _objects = newValue
-            }
+            objectsMutex.withSync { $0 = newValue }
         }
     }
 
-    func getObjectFromPool(id: String) -> ObjectsPool.Entry? {
-        objects[id]
+    func nosync_getObjectFromPool(id: String) -> ObjectsPool.Entry? {
+        objectsMutex.withoutSync { $0[id] }
     }
 }