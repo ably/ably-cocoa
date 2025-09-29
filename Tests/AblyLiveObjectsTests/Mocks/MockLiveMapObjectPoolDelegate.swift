ably/ably-liveobjects-swift-plugin

Change LiveMap pool delegate to return snapshot of pool
This will subsequently allow us to make more use of value types
internally.

(The change in this commit, in isolation, actually makes things a bit
messier because now we have to mock a whole ObjectsPool, with its root
object that the tests don't even need. I suppose we could instead make
the delegate just return a dictionary containing the pool's entries, but
let's see if it actually becomes a problem.)

Tests/AblyLiveObjectsTests/Mocks/MockLiveMapObjectPoolDelegate.swift
@@ -3,22 +3,39 @@ import Foundation
 
 /// A mock delegate that can return predefined objects
 final class MockLiveMapObjectPoolDelegate: LiveMapObjectPoolDelegate {
-    private let objectsMutex: DispatchQueueMutex<[String: ObjectsPool.Entry]>
+    private let poolMutex: DispatchQueueMutex<ObjectsPool>
 
     init(internalQueue: DispatchQueue) {
-        objectsMutex = DispatchQueueMutex(dispatchQueue: internalQueue, initialValue: [:])
+        poolMutex = DispatchQueueMutex(
+            dispatchQueue: internalQueue,
+            initialValue: Self.createPool(
+                internalQueue: internalQueue,
+                otherEntries: [:],
+            ),
+        )
+    }
+
+    static func createPool(internalQueue: DispatchQueue, otherEntries: [String: ObjectsPool.Entry]) -> ObjectsPool {
+        .init(
+            // Only otherEntries matters; the others just control the creation of the object at the root key, which none of the tests that use this delegate care about
+            logger: TestLogger(),
+            internalQueue: internalQueue,
+            userCallbackQueue: .main,
+            clock: MockSimpleClock(),
+            testsOnly_otherEntries: otherEntries,
+        )
     }
 
     var objects: [String: ObjectsPool.Entry] {
         get {
-            objectsMutex.withLock { $0 }
+            poolMutex.withLock { $0.entries }
         }
         set {
-            objectsMutex.withLock { $0 = newValue }
+            poolMutex.withLock { $0 = Self.createPool(internalQueue: poolMutex.dispatchQueue, otherEntries: newValue) }
         }
     }
 
-    func nosync_getObjectFromPool(id: String) -> ObjectsPool.Entry? {
-        objectsMutex.withoutLock { $0[id] }
+    var nosync_objectsPool: ObjectsPool {
+        poolMutex.withoutLock { $0 }
     }
 }

ably/ably-cocoa

Tests/AblyLiveObjectsTests/Mocks/MockLiveMapObjectPoolDelegate.swift
@@ -3,22 +3,39 @@ import Foundation
 
 /// A mock delegate that can return predefined objects
 final class MockLiveMapObjectPoolDelegate: LiveMapObjectPoolDelegate {
-    private let objectsMutex: DispatchQueueMutex<[String: ObjectsPool.Entry]>
+    private let poolMutex: DispatchQueueMutex<ObjectsPool>
 
     init(internalQueue: DispatchQueue) {
-        objectsMutex = DispatchQueueMutex(dispatchQueue: internalQueue, initialValue: [:])
+        poolMutex = DispatchQueueMutex(
+            dispatchQueue: internalQueue,
+            initialValue: Self.createPool(
+                internalQueue: internalQueue,
+                otherEntries: [:],
+            ),
+        )
+    }
+
+    static func createPool(internalQueue: DispatchQueue, otherEntries: [String: ObjectsPool.Entry]) -> ObjectsPool {
+        .init(
+            // Only otherEntries matters; the others just control the creation of the object at the root key, which none of the tests that use this delegate care about
+            logger: TestLogger(),
+            internalQueue: internalQueue,
+            userCallbackQueue: .main,
+            clock: MockSimpleClock(),
+            testsOnly_otherEntries: otherEntries,
+        )
     }
 
     var objects: [String: ObjectsPool.Entry] {
         get {
-            objectsMutex.withSync { $0 }
+            poolMutex.withSync { $0.entries }
         }
         set {
-            objectsMutex.withSync { $0 = newValue }
+            poolMutex.withSync { $0 = Self.createPool(internalQueue: poolMutex.dispatchQueue, otherEntries: newValue) }
         }
     }
 
-    func nosync_getObjectFromPool(id: String) -> ObjectsPool.Entry? {
-        objectsMutex.withoutSync { $0[id] }
+    var nosync_objectsPool: ObjectsPool {
+        poolMutex.withoutSync { $0 }
     }
 }