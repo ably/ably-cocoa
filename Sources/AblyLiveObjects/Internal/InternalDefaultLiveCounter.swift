ably/ably-liveobjects-swift-plugin

Remove InternalError
It wasn't serving a useful purpose and was just another layer of
indirection. Throw ARTErrorInfo directly inside the codebase and let it
bubble up to the public API (but still keep the richer underlying errors
for checking in tests).

Resolves #48.

Sources/AblyLiveObjects/Internal/InternalDefaultLiveCounter.swift
@@ -104,42 +104,34 @@ internal final class InternalDefaultLiveCounter: Sendable {
     }
 
     internal func increment(amount: Double, coreSDK: CoreSDK) async throws(ARTErrorInfo) {
-        do throws(InternalError) {
-            let objectMessage = try mutableStateMutex.withLock { mutableState throws(InternalError) in
-                // RTLC12c
-                do throws(ARTErrorInfo) {
-                    try coreSDK.nosync_validateChannelState(
-                        notIn: [.detached, .failed, .suspended],
-                        operationDescription: "LiveCounter.increment",
-                    )
-                } catch {
-                    throw error.toInternalError()
-                }
+        let objectMessage = try mutableStateMutex.withLock { mutableState throws(ARTErrorInfo) in
+            // RTLC12c
+            try coreSDK.nosync_validateChannelState(
+                notIn: [.detached, .failed, .suspended],
+                operationDescription: "LiveCounter.increment",
+            )
 
-                // RTLC12e1
-                if !amount.isFinite {
-                    throw LiveObjectsError.counterIncrementAmountInvalid(amount: amount).toARTErrorInfo().toInternalError()
-                }
-
-                return OutboundObjectMessage(
-                    operation: .init(
-                        // RTLC12e2
-                        action: .known(.counterInc),
-                        // RTLC12e3
-                        objectId: mutableState.liveObjectMutableState.objectID,
-                        counterOp: .init(
-                            // RTLC12e4
-                            amount: .init(value: amount),
-                        ),
-                    ),
-                )
+            // RTLC12e1
+            if !amount.isFinite {
+                throw LiveObjectsError.counterIncrementAmountInvalid(amount: amount).toARTErrorInfo()
             }
 
-            // RTLC12f
-            try await coreSDK.publish(objectMessages: [objectMessage])
-        } catch {
-            throw error.toARTErrorInfo()
+            return OutboundObjectMessage(
+                operation: .init(
+                    // RTLC12e2
+                    action: .known(.counterInc),
+                    // RTLC12e3
+                    objectId: mutableState.liveObjectMutableState.objectID,
+                    counterOp: .init(
+                        // RTLC12e4
+                        amount: .init(value: amount),
+                    ),
+                ),
+            )
         }
+
+        // RTLC12f
+        try await coreSDK.publish(objectMessages: [objectMessage])
     }
 
     internal func decrement(amount: Double, coreSDK: CoreSDK) async throws(ARTErrorInfo) {

Sources/AblyLiveObjects/Internal/InternalDefaultLiveMap.swift
@@ -150,66 +150,50 @@ internal final class InternalDefaultLiveMap: Sendable {
     }
 
     internal func set(key: String, value: InternalLiveMapValue, coreSDK: CoreSDK) async throws(ARTErrorInfo) {
-        do throws(InternalError) {
-            let objectMessage = try mutableStateMutex.withLock { mutableState throws(InternalError) in
-                // RTLM20c
-                do throws(ARTErrorInfo) {
-                    try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "LiveMap.set")
-                } catch {
-                    throw error.toInternalError()
-                }
+        let objectMessage = try mutableStateMutex.withLock { mutableState throws(ARTErrorInfo) in
+            // RTLM20c
+            try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "LiveMap.set")
 
-                return OutboundObjectMessage(
-                    operation: .init(
-                        // RTLM20e2
-                        action: .known(.mapSet),
-                        // RTLM20e3
-                        objectId: mutableState.liveObjectMutableState.objectID,
-                        mapOp: .init(
-                            // RTLM20e4
-                            key: key,
-                            // RTLM20e5
-                            data: value.nosync_toObjectData,
-                        ),
+            return OutboundObjectMessage(
+                operation: .init(
+                    // RTLM20e2
+                    action: .known(.mapSet),
+                    // RTLM20e3
+                    objectId: mutableState.liveObjectMutableState.objectID,
+                    mapOp: .init(
+                        // RTLM20e4
+                        key: key,
+                        // RTLM20e5
+                        data: value.nosync_toObjectData,
                     ),
-                )
-            }
-
-            try await coreSDK.publish(objectMessages: [objectMessage])
-        } catch {
-            throw error.toARTErrorInfo()
+                ),
+            )
         }
+
+        try await coreSDK.publish(objectMessages: [objectMessage])
     }
 
     internal func remove(key: String, coreSDK: CoreSDK) async throws(ARTErrorInfo) {
-        do throws(InternalError) {
-            let objectMessage = try mutableStateMutex.withLock { mutableState throws(InternalError) in
-                // RTLM21c
-                do throws(ARTErrorInfo) {
-                    try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "LiveMap.remove")
-                } catch {
-                    throw error.toInternalError()
-                }
+        let objectMessage = try mutableStateMutex.withLock { mutableState throws(ARTErrorInfo) in
+            // RTLM21c
+            try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "LiveMap.remove")
 
-                return OutboundObjectMessage(
-                    operation: .init(
-                        // RTLM21e2
-                        action: .known(.mapRemove),
-                        // RTLM21e3
-                        objectId: mutableState.liveObjectMutableState.objectID,
-                        mapOp: .init(
-                            // RTLM21e4
-                            key: key,
-                        ),
+            return OutboundObjectMessage(
+                operation: .init(
+                    // RTLM21e2
+                    action: .known(.mapRemove),
+                    // RTLM21e3
+                    objectId: mutableState.liveObjectMutableState.objectID,
+                    mapOp: .init(
+                        // RTLM21e4
+                        key: key,
                     ),
-                )
-            }
-
-            // RTLM21f
-            try await coreSDK.publish(objectMessages: [objectMessage])
-        } catch {
-            throw error.toARTErrorInfo()
+                ),
+            )
         }
+
+        // RTLM21f
+        try await coreSDK.publish(objectMessages: [objectMessage])
     }
 
     @discardableResult

Sources/AblyLiveObjects/Internal/InternalDefaultRealtimeObjects.swift
@@ -172,39 +172,31 @@ internal final class InternalDefaultRealtimeObjects: Sendable, LiveMapObjectPool
     }
 
     internal func createMap(entries: [String: InternalLiveMapValue], coreSDK: CoreSDK) async throws(ARTErrorInfo) -> InternalDefaultLiveMap {
-        do throws(InternalError) {
-            let creationOperation = try mutableStateMutex.withLock { _ throws(InternalError) in
-                // RTO11d
-                do throws(ARTErrorInfo) {
-                    try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "RealtimeObjects.createMap")
-                } catch {
-                    throw error.toInternalError()
-                }
+        let creationOperation = try mutableStateMutex.withLock { _ throws(ARTErrorInfo) in
+            // RTO11d
+            try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "RealtimeObjects.createMap")
 
-                // RTO11f
-                // TODO: This is a stopgap; change to use server time per RTO11f5 (https://github.com/ably/ably-liveobjects-swift-plugin/issues/50)
-                let timestamp = clock.now
-                return ObjectCreationHelpers.nosync_creationOperationForLiveMap(
-                    entries: entries,
-                    timestamp: timestamp,
-                )
-            }
+            // RTO11f
+            // TODO: This is a stopgap; change to use server time per RTO11f5 (https://github.com/ably/ably-liveobjects-swift-plugin/issues/50)
+            let timestamp = clock.now
+            return ObjectCreationHelpers.nosync_creationOperationForLiveMap(
+                entries: entries,
+                timestamp: timestamp,
+            )
+        }
 
-            // RTO11g
-            try await coreSDK.publish(objectMessages: [creationOperation.objectMessage])
+        // RTO11g
+        try await coreSDK.publish(objectMessages: [creationOperation.objectMessage])
 
-            // RTO11h
-            return mutableStateMutex.withLock { mutableState in
-                mutableState.objectsPool.nosync_getOrCreateMap(
-                    creationOperation: creationOperation,
-                    logger: logger,
-                    internalQueue: mutableStateMutex.dispatchQueue,
-                    userCallbackQueue: userCallbackQueue,
-                    clock: clock,
-                )
-            }
-        } catch {
-            throw error.toARTErrorInfo()
+        // RTO11h
+        return mutableStateMutex.withLock { mutableState in
+            mutableState.objectsPool.nosync_getOrCreateMap(
+                creationOperation: creationOperation,
+                logger: logger,
+                internalQueue: mutableStateMutex.dispatchQueue,
+                userCallbackQueue: userCallbackQueue,
+                clock: clock,
+            )
         }
     }
 
@@ -214,45 +206,37 @@ internal final class InternalDefaultRealtimeObjects: Sendable, LiveMapObjectPool
     }
 
     internal func createCounter(count: Double, coreSDK: CoreSDK) async throws(ARTErrorInfo) -> InternalDefaultLiveCounter {
-        do throws(InternalError) {
-            // RTO12d
-            do throws(ARTErrorInfo) {
-                try mutableStateMutex.withLock { _ throws(ARTErrorInfo) in
-                    try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "RealtimeObjects.createCounter")
-                }
-            } catch {
-                throw error.toInternalError()
-            }
+        // RTO12d
+        try mutableStateMutex.withLock { _ throws(ARTErrorInfo) in
+            try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "RealtimeObjects.createCounter")
+        }
 
-            // RTO12f1
-            if !count.isFinite {
-                throw LiveObjectsError.counterInitialValueInvalid(value: count).toARTErrorInfo().toInternalError()
-            }
+        // RTO12f1
+        if !count.isFinite {
+            throw LiveObjectsError.counterInitialValueInvalid(value: count).toARTErrorInfo()
+        }
 
-            // RTO12f
+        // RTO12f
 
-            // TODO: This is a stopgap; change to use server time per RTO12f5 (https://github.com/ably/ably-liveobjects-swift-plugin/issues/50)
-            let timestamp = clock.now
-            let creationOperation = ObjectCreationHelpers.creationOperationForLiveCounter(
-                count: count,
-                timestamp: timestamp,
+        // TODO: This is a stopgap; change to use server time per RTO12f5 (https://github.com/ably/ably-liveobjects-swift-plugin/issues/50)
+        let timestamp = clock.now
+        let creationOperation = ObjectCreationHelpers.creationOperationForLiveCounter(
+            count: count,
+            timestamp: timestamp,
+        )
+
+        // RTO12g
+        try await coreSDK.publish(objectMessages: [creationOperation.objectMessage])
+
+        // RTO12h
+        return mutableStateMutex.withLock { mutableState in
+            mutableState.objectsPool.nosync_getOrCreateCounter(
+                creationOperation: creationOperation,
+                logger: logger,
+                internalQueue: mutableStateMutex.dispatchQueue,
+                userCallbackQueue: userCallbackQueue,
+                clock: clock,
             )
-
-            // RTO12g
-            try await coreSDK.publish(objectMessages: [creationOperation.objectMessage])
-
-            // RTO12h
-            return mutableStateMutex.withLock { mutableState in
-                mutableState.objectsPool.nosync_getOrCreateCounter(
-                    creationOperation: creationOperation,
-                    logger: logger,
-                    internalQueue: mutableStateMutex.dispatchQueue,
-                    userCallbackQueue: userCallbackQueue,
-                    clock: clock,
-                )
-            }
-        } catch {
-            throw error.toARTErrorInfo()
         }
     }
 
@@ -356,7 +340,7 @@ internal final class InternalDefaultRealtimeObjects: Sendable, LiveMapObjectPool
     // MARK: - Sending `OBJECT` ProtocolMessage
 
     // This is currently exposed so that we can try calling it from the tests in the early days of the SDK to check that we can send an OBJECT ProtocolMessage. We'll probably make it private later on.
-    internal func testsOnly_publish(objectMessages: [OutboundObjectMessage], coreSDK: CoreSDK) async throws(InternalError) {
+    internal func testsOnly_publish(objectMessages: [OutboundObjectMessage], coreSDK: CoreSDK) async throws(ARTErrorInfo) {
         try await coreSDK.publish(objectMessages: objectMessages)
     }
 

ably/ably-cocoa

Sources/AblyLiveObjects/Internal/InternalDefaultLiveCounter.swift
@@ -104,42 +104,34 @@ internal final class InternalDefaultLiveCounter: Sendable {
     }
 
     internal func increment(amount: Double, coreSDK: CoreSDK) async throws(ARTErrorInfo) {
-        do throws(InternalError) {
-            let objectMessage = try mutableStateMutex.withSync { mutableState throws(InternalError) in
-                // RTLC12c
-                do throws(ARTErrorInfo) {
-                    try coreSDK.nosync_validateChannelState(
-                        notIn: [.detached, .failed, .suspended],
-                        operationDescription: "LiveCounter.increment",
-                    )
-                } catch {
-                    throw error.toInternalError()
-                }
+        let objectMessage = try mutableStateMutex.withSync { mutableState throws(ARTErrorInfo) in
+            // RTLC12c
+            try coreSDK.nosync_validateChannelState(
+                notIn: [.detached, .failed, .suspended],
+                operationDescription: "LiveCounter.increment",
+            )
 
-                // RTLC12e1
-                if !amount.isFinite {
-                    throw LiveObjectsError.counterIncrementAmountInvalid(amount: amount).toARTErrorInfo().toInternalError()
-                }
-
-                return OutboundObjectMessage(
-                    operation: .init(
-                        // RTLC12e2
-                        action: .known(.counterInc),
-                        // RTLC12e3
-                        objectId: mutableState.liveObjectMutableState.objectID,
-                        counterOp: .init(
-                            // RTLC12e4
-                            amount: .init(value: amount),
-                        ),
-                    ),
-                )
+            // RTLC12e1
+            if !amount.isFinite {
+                throw LiveObjectsError.counterIncrementAmountInvalid(amount: amount).toARTErrorInfo()
             }
 
-            // RTLC12f
-            try await coreSDK.publish(objectMessages: [objectMessage])
-        } catch {
-            throw error.toARTErrorInfo()
+            return OutboundObjectMessage(
+                operation: .init(
+                    // RTLC12e2
+                    action: .known(.counterInc),
+                    // RTLC12e3
+                    objectId: mutableState.liveObjectMutableState.objectID,
+                    counterOp: .init(
+                        // RTLC12e4
+                        amount: .init(value: amount),
+                    ),
+                ),
+            )
         }
+
+        // RTLC12f
+        try await coreSDK.publish(objectMessages: [objectMessage])
     }
 
     internal func decrement(amount: Double, coreSDK: CoreSDK) async throws(ARTErrorInfo) {

Sources/AblyLiveObjects/Internal/InternalDefaultLiveMap.swift
@@ -150,66 +150,50 @@ internal final class InternalDefaultLiveMap: Sendable {
     }
 
     internal func set(key: String, value: InternalLiveMapValue, coreSDK: CoreSDK) async throws(ARTErrorInfo) {
-        do throws(InternalError) {
-            let objectMessage = try mutableStateMutex.withSync { mutableState throws(InternalError) in
-                // RTLM20c
-                do throws(ARTErrorInfo) {
-                    try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "LiveMap.set")
-                } catch {
-                    throw error.toInternalError()
-                }
+        let objectMessage = try mutableStateMutex.withSync { mutableState throws(ARTErrorInfo) in
+            // RTLM20c
+            try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "LiveMap.set")
 
-                return OutboundObjectMessage(
-                    operation: .init(
-                        // RTLM20e2
-                        action: .known(.mapSet),
-                        // RTLM20e3
-                        objectId: mutableState.liveObjectMutableState.objectID,
-                        mapOp: .init(
-                            // RTLM20e4
-                            key: key,
-                            // RTLM20e5
-                            data: value.nosync_toObjectData,
-                        ),
+            return OutboundObjectMessage(
+                operation: .init(
+                    // RTLM20e2
+                    action: .known(.mapSet),
+                    // RTLM20e3
+                    objectId: mutableState.liveObjectMutableState.objectID,
+                    mapOp: .init(
+                        // RTLM20e4
+                        key: key,
+                        // RTLM20e5
+                        data: value.nosync_toObjectData,
                     ),
-                )
-            }
-
-            try await coreSDK.publish(objectMessages: [objectMessage])
-        } catch {
-            throw error.toARTErrorInfo()
+                ),
+            )
         }
+
+        try await coreSDK.publish(objectMessages: [objectMessage])
     }
 
     internal func remove(key: String, coreSDK: CoreSDK) async throws(ARTErrorInfo) {
-        do throws(InternalError) {
-            let objectMessage = try mutableStateMutex.withSync { mutableState throws(InternalError) in
-                // RTLM21c
-                do throws(ARTErrorInfo) {
-                    try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "LiveMap.remove")
-                } catch {
-                    throw error.toInternalError()
-                }
+        let objectMessage = try mutableStateMutex.withSync { mutableState throws(ARTErrorInfo) in
+            // RTLM21c
+            try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "LiveMap.remove")
 
-                return OutboundObjectMessage(
-                    operation: .init(
-                        // RTLM21e2
-                        action: .known(.mapRemove),
-                        // RTLM21e3
-                        objectId: mutableState.liveObjectMutableState.objectID,
-                        mapOp: .init(
-                            // RTLM21e4
-                            key: key,
-                        ),
+            return OutboundObjectMessage(
+                operation: .init(
+                    // RTLM21e2
+                    action: .known(.mapRemove),
+                    // RTLM21e3
+                    objectId: mutableState.liveObjectMutableState.objectID,
+                    mapOp: .init(
+                        // RTLM21e4
+                        key: key,
                     ),
-                )
-            }
-
-            // RTLM21f
-            try await coreSDK.publish(objectMessages: [objectMessage])
-        } catch {
-            throw error.toARTErrorInfo()
+                ),
+            )
         }
+
+        // RTLM21f
+        try await coreSDK.publish(objectMessages: [objectMessage])
     }
 
     @discardableResult

Sources/AblyLiveObjects/Internal/InternalDefaultRealtimeObjects.swift
@@ -172,39 +172,31 @@ internal final class InternalDefaultRealtimeObjects: Sendable, LiveMapObjectPool
     }
 
     internal func createMap(entries: [String: InternalLiveMapValue], coreSDK: CoreSDK) async throws(ARTErrorInfo) -> InternalDefaultLiveMap {
-        do throws(InternalError) {
-            let creationOperation = try mutableStateMutex.withSync { _ throws(InternalError) in
-                // RTO11d
-                do throws(ARTErrorInfo) {
-                    try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "RealtimeObjects.createMap")
-                } catch {
-                    throw error.toInternalError()
-                }
+        let creationOperation = try mutableStateMutex.withSync { _ throws(ARTErrorInfo) in
+            // RTO11d
+            try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "RealtimeObjects.createMap")
 
-                // RTO11f
-                // TODO: This is a stopgap; change to use server time per RTO11f5 (https://github.com/ably/ably-liveobjects-swift-plugin/issues/50)
-                let timestamp = clock.now
-                return ObjectCreationHelpers.nosync_creationOperationForLiveMap(
-                    entries: entries,
-                    timestamp: timestamp,
-                )
-            }
+            // RTO11f
+            // TODO: This is a stopgap; change to use server time per RTO11f5 (https://github.com/ably/ably-liveobjects-swift-plugin/issues/50)
+            let timestamp = clock.now
+            return ObjectCreationHelpers.nosync_creationOperationForLiveMap(
+                entries: entries,
+                timestamp: timestamp,
+            )
+        }
 
-            // RTO11g
-            try await coreSDK.publish(objectMessages: [creationOperation.objectMessage])
+        // RTO11g
+        try await coreSDK.publish(objectMessages: [creationOperation.objectMessage])
 
-            // RTO11h
-            return mutableStateMutex.withSync { mutableState in
-                mutableState.objectsPool.nosync_getOrCreateMap(
-                    creationOperation: creationOperation,
-                    logger: logger,
-                    internalQueue: mutableStateMutex.dispatchQueue,
-                    userCallbackQueue: userCallbackQueue,
-                    clock: clock,
-                )
-            }
-        } catch {
-            throw error.toARTErrorInfo()
+        // RTO11h
+        return mutableStateMutex.withSync { mutableState in
+            mutableState.objectsPool.nosync_getOrCreateMap(
+                creationOperation: creationOperation,
+                logger: logger,
+                internalQueue: mutableStateMutex.dispatchQueue,
+                userCallbackQueue: userCallbackQueue,
+                clock: clock,
+            )
         }
     }
 
@@ -214,45 +206,37 @@ internal final class InternalDefaultRealtimeObjects: Sendable, LiveMapObjectPool
     }
 
     internal func createCounter(count: Double, coreSDK: CoreSDK) async throws(ARTErrorInfo) -> InternalDefaultLiveCounter {
-        do throws(InternalError) {
-            // RTO12d
-            do throws(ARTErrorInfo) {
-                try mutableStateMutex.withSync { _ throws(ARTErrorInfo) in
-                    try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "RealtimeObjects.createCounter")
-                }
-            } catch {
-                throw error.toInternalError()
-            }
+        // RTO12d
+        try mutableStateMutex.withSync { _ throws(ARTErrorInfo) in
+            try coreSDK.nosync_validateChannelState(notIn: [.detached, .failed, .suspended], operationDescription: "RealtimeObjects.createCounter")
+        }
 
-            // RTO12f1
-            if !count.isFinite {
-                throw LiveObjectsError.counterInitialValueInvalid(value: count).toARTErrorInfo().toInternalError()
-            }
+        // RTO12f1
+        if !count.isFinite {
+            throw LiveObjectsError.counterInitialValueInvalid(value: count).toARTErrorInfo()
+        }
 
-            // RTO12f
+        // RTO12f
 
-            // TODO: This is a stopgap; change to use server time per RTO12f5 (https://github.com/ably/ably-liveobjects-swift-plugin/issues/50)
-            let timestamp = clock.now
-            let creationOperation = ObjectCreationHelpers.creationOperationForLiveCounter(
-                count: count,
-                timestamp: timestamp,
+        // TODO: This is a stopgap; change to use server time per RTO12f5 (https://github.com/ably/ably-liveobjects-swift-plugin/issues/50)
+        let timestamp = clock.now
+        let creationOperation = ObjectCreationHelpers.creationOperationForLiveCounter(
+            count: count,
+            timestamp: timestamp,
+        )
+
+        // RTO12g
+        try await coreSDK.publish(objectMessages: [creationOperation.objectMessage])
+
+        // RTO12h
+        return mutableStateMutex.withSync { mutableState in
+            mutableState.objectsPool.nosync_getOrCreateCounter(
+                creationOperation: creationOperation,
+                logger: logger,
+                internalQueue: mutableStateMutex.dispatchQueue,
+                userCallbackQueue: userCallbackQueue,
+                clock: clock,
             )
-
-            // RTO12g
-            try await coreSDK.publish(objectMessages: [creationOperation.objectMessage])
-
-            // RTO12h
-            return mutableStateMutex.withSync { mutableState in
-                mutableState.objectsPool.nosync_getOrCreateCounter(
-                    creationOperation: creationOperation,
-                    logger: logger,
-                    internalQueue: mutableStateMutex.dispatchQueue,
-                    userCallbackQueue: userCallbackQueue,
-                    clock: clock,
-                )
-            }
-        } catch {
-            throw error.toARTErrorInfo()
         }
     }
 
@@ -356,7 +340,7 @@ internal final class InternalDefaultRealtimeObjects: Sendable, LiveMapObjectPool
     // MARK: - Sending `OBJECT` ProtocolMessage
 
     // This is currently exposed so that we can try calling it from the tests in the early days of the SDK to check that we can send an OBJECT ProtocolMessage. We'll probably make it private later on.
-    internal func testsOnly_publish(objectMessages: [OutboundObjectMessage], coreSDK: CoreSDK) async throws(InternalError) {
+    internal func testsOnly_publish(objectMessages: [OutboundObjectMessage], coreSDK: CoreSDK) async throws(ARTErrorInfo) {
         try await coreSDK.publish(objectMessages: objectMessages)
     }
 