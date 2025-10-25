308b10ee2e2991c85fa35f7389773f7294284f7b
Marat Al <m.alekperov@gmail.com>
2025-10-25 22:53:51 +0100

    Comment out contents of `setLocalDeviceClientId_nosync` (storage and state machine).

--- Source/ARTAuth.m
+++ Source/ARTAuth.m
@@ -735,7 +735,7 @@ dispatch_async(_queue, ^{
     ARTLogVerbose(self.logger, @"BEGIN setProtocolClientId");
     _protocolClientId = clientId;
     #if TARGET_OS_IOS
-//    [self setLocalDeviceClientId_nosync:_protocolClientId];
+    [self setLocalDeviceClientId_nosync:_protocolClientId];
     #endif
     ARTLogVerbose(self.logger, @"END setProtocolClientId");
 }
@@ -832,16 +832,16 @@ dispatch_sync(_queue, ^{
     }
     ARTLogVerbose(self.logger, @"DO 1 setLocalDeviceClientId_nosync");
     [_rest.device_nosync setClientId:clientId];
-    ARTLogVerbose(self.logger, @"DO 2 setLocalDeviceClientId_nosync");
-    [_rest.storage setObject:clientId forKey:ARTClientIdKey];
-    ARTLogVerbose(self.logger, @"DO 3 setLocalDeviceClientId_nosync");
-    [_rest.push getActivationMachine:^(ARTPushActivationStateMachine *stateMachine) {
-        ARTLogVerbose(self.logger, @"DO 4 setLocalDeviceClientId_nosync");
-        if (![stateMachine.current_nosync isKindOfClass:[ARTPushActivationStateNotActivated class]]) {
-            ARTLogVerbose(self.logger, @"DO 5 setLocalDeviceClientId_nosync");
-            [stateMachine sendEvent:[[ARTPushActivationEventGotPushDeviceDetails alloc] init]];
-        }
-    }];
+//    ARTLogVerbose(self.logger, @"DO 2 setLocalDeviceClientId_nosync");
+//    [_rest.storage setObject:clientId forKey:ARTClientIdKey];
+//    ARTLogVerbose(self.logger, @"DO 3 setLocalDeviceClientId_nosync");
+//    [_rest.push getActivationMachine:^(ARTPushActivationStateMachine *stateMachine) {
+//        ARTLogVerbose(self.logger, @"DO 4 setLocalDeviceClientId_nosync");
+//        if (![stateMachine.current_nosync isKindOfClass:[ARTPushActivationStateNotActivated class]]) {
+//            ARTLogVerbose(self.logger, @"DO 5 setLocalDeviceClientId_nosync");
+//            [stateMachine sendEvent:[[ARTPushActivationEventGotPushDeviceDetails alloc] init]];
+//        }
+//    }];
     ARTLogVerbose(self.logger, @"END setLocalDeviceClientId_nosync");
 }
 #endif
