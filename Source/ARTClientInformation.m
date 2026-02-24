54664215b6bda8431f345385693cb6a1a51ff3c3
Andy Ford <andy.ford@ably.com>
2026-02-24 19:19:32 +0000

    Bump version to 1.2.58.

--- Source/ARTClientInformation.m
+++ Source/ARTClientInformation.m
@@ -6,7 +6,7 @@
 #import <sys/utsname.h>
 
 NSString *const ARTClientInformationAgentNotVersioned = @"ARTClientInformationAgentNotVersioned";
-NSString *const ARTClientInformation_libraryVersion = @"1.2.57";
+NSString *const ARTClientInformation_libraryVersion = @"1.2.58";
 static NSString *const _libraryName = @"ably-cocoa";
 
 // NSOperatingSystemVersion has NSInteger as version components for some reason, so mitigate it here.
