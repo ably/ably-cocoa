058c420ac32aed935f5f32fe5af57d1b778dd77d
Lawrence Forooghian <lawrence@forooghian.com>
2026-04-15 16:55:07 -0300

    Add empty app for reproducing LocalDevice storage bug

---
+++ Examples/LocalDeviceStorageBugTest/LocalDeviceStorageBugTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalDeviceStorageBugTest
+//
+//  Created by Lawrence Forooghian on 15/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
